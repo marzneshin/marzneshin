3e4799037447d8c7ed31d49bf0aebe5ef6d19b4e
Krr0ptioN <mardin-pop@protonmail.com>
2024-01-25 16:56:44 +0300

    feat(sidebar/routing-layout): gird layout and mapping sidebar items route

--- app/dashboard/src/App.tsx
+++ app/dashboard/src/App.tsx
@@ -1,12 +1,48 @@
+import { Grid, GridItem } from '@chakra-ui/react';
+import { Footer } from 'components/Footer';
+import { Header } from 'components/Header';
+import { pages, PageRoute } from 'constants/Pages';
+import { Sidebar } from 'components/Sidebar';
+import Login from 'pages/Login';
 import 'react-datepicker/dist/react-datepicker.css';
 import 'react-loading-skeleton/dist/skeleton.css';
-import { RouterProvider } from 'react-router-dom';
-import { router } from './components/modules/Router';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <main className="p-8">
-      <RouterProvider router={router} />
+    <main >
+      <Routes>
+        <Route
+          path="/login"
+          element={<Login />}
+        />
+      </Routes>
+
+      <Grid
+        h="100%"
+        templateAreas={`"nav header"
+                  "nav main"
+                  "nav footer"`}
+        gridTemplateRows={'20px 1fr 20px'}
+        gridTemplateColumns={'15rem 1fr'}
+      >
+        <GridItem area={'header'} my="12px" mx="30px">
+          <Header />
+        </GridItem>
+        <GridItem area={'nav'}>
+          <Sidebar />
+        </GridItem>
+        <GridItem area={'main'}>
+          <Routes>
+            {pages.map((route: PageRoute) =>
+              <Route {...route} />
+            )}
+          </Routes>
+        </GridItem>
+        <GridItem area={'footer'}>
+          <Footer />
+        </GridItem>
+      </Grid>
     </main>
   );
 }
