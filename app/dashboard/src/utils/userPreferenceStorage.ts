84bdd78f5647310c6ede60b3949facf315cfae74
Krr0ptioN <mardin-pop@protonmail.com>
2024-02-02 17:37:20 +0300

    feat(services-mngt): table default services row per page limit

--- app/dashboard/src/utils/userPreferenceStorage.ts
+++ app/dashboard/src/utils/userPreferenceStorage.ts
@@ -1,5 +1,8 @@
 const NUM_USERS_PER_PAGE_LOCAL_STORAGE_KEY = 'marzban-num-users-per-page';
+const NUM_SERVICES_PER_PAGE_LOCAL_STORAGE_KEY = 'marzban-num-users-per-page';
 const NUM_USERS_PER_PAGE_DEFAULT = 10;
+const NUM_SERVICES_PER_PAGE_DEFAULT = 10;
+
 export const getUsersPerPageLimitSize = () => {
   const numUsersPerPage =
     localStorage.getItem(NUM_USERS_PER_PAGE_LOCAL_STORAGE_KEY) ||
@@ -9,4 +12,16 @@ export const getUsersPerPageLimitSize = () => {
 
 export const setUsersPerPageLimitSize = (value: string) => {
   return localStorage.setItem(NUM_USERS_PER_PAGE_LOCAL_STORAGE_KEY, value);
+};
+
+
+export const getServicesPerPageLimitSize = () => {
+  const numServicesPerPage =
+    localStorage.getItem(NUM_SERVICES_PER_PAGE_LOCAL_STORAGE_KEY) ||
+    NUM_SERVICES_PER_PAGE_DEFAULT.toString(); // this catches `null` values
+  return parseInt(numServicesPerPage) || NUM_SERVICES_PER_PAGE_DEFAULT; // this catches NaN values
+};
+
+export const setServicesPerPageLimitSize = (value: string) => {
+  return localStorage.setItem(NUM_SERVICES_PER_PAGE_LOCAL_STORAGE_KEY, value);
 };
