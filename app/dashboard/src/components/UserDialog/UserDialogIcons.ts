aef768970403ea659f55b3bd370d73ea80596a6c
Krr0ptioN <mardin-pop@protonmail.com>
2024-02-03 16:00:26 +0300

    refact: generalize List checkbox component to become apart of the design system
    - Generalizing the List checkbox component
    - Adapting services list field in user dialog form to the new component system for List checkbox

--- app/dashboard/src/components/UserDialog/UserDialogIcons.ts
+++ app/dashboard/src/components/UserDialog/UserDialogIcons.ts
@@ -2,24 +2,7 @@
 import { chakra } from '@chakra-ui/react';
 import {
   ChartPieIcon,
-  PencilIcon,
-  UserPlusIcon,
 } from '@heroicons/react/24/outline';
-
-export const AddUserIcon = chakra(UserPlusIcon, {
-  baseStyle: {
-    w: 5,
-    h: 5,
-  },
-});
-
-export const EditUserIcon = chakra(PencilIcon, {
-  baseStyle: {
-    w: 5,
-    h: 5,
-  },
-});
-
 export const UserUsageIcon = chakra(ChartPieIcon, {
   baseStyle: {
     w: 5,
