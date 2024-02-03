d50ac34937390e4415a61d7f56d0542b4dba2095
Krr0ptioN <mardin-pop@protonmail.com>
2024-02-03 19:05:58 +0300

    feat(services-page): services page which containe table for service modification

--- app/dashboard/src/pages/Services.tsx
+++ app/dashboard/src/pages/Services.tsx
@@ -1,6 +1,26 @@
 import { FC } from 'react';
 import { Text } from '@chakra-ui/react';
+import { FC, useEffect } from 'react';
+import { Box, VStack } from '@chakra-ui/react';
+import { ServicesFilters } from 'components/ServicesFilters';
+import { ServicesTable } from 'components/ServicesTable';
+import { useDashboard } from 'contexts/DashboardContext';
+import { pages } from 'constants/Pages';
+
 
 export const ServicesPage: FC = () => {
-  return <Text>Serviced</Text>;
+  useEffect(() => {
+    useDashboard.getState().activatePage(pages.findIndex((page) => page.path === '/services'));
+    useDashboard.getState().refetchServices();
+  }, []);
+  return (
+    <VStack justifyContent="space-between" maxH="100vh" p="2rem" rowGap={4}>
+      <Box w="full" >
+        <ServicesFilters />
+        <ServicesTable />
+        {/* <ServiceDialog /> */}
+        {/* <DeleteServiceModal /> */}
+      </Box>
+    </VStack>
+  )
 }
