0b6e0fe71ccd08ad2e91100fe826da8ef2783f47
Krr0ptioN <mardin-pop@protonmail.com>
2024-02-05 22:58:27 +0300

    fix: typo in url for github repo star button

--- app/dashboard/src/constants/Project.ts
+++ app/dashboard/src/constants/Project.ts
@@ -1,3 +1,3 @@
-export const REPO_URL = 'https://github.com/khodedawosh/Marzneshin';
+export const REPO_URL = 'https://github.com/khodedawsh/Marzneshin';
 export const ORGANIZATION_URL = 'https://github.com/khodedawosh';
 export const DONATION_URL = 'https://github.com/khodedawosh/Marzneshin#donation';
