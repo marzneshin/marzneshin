bda7c9c28c6261a7bc2d8c20ecc121a5d4bb4920
Dawsh <khodedawsh@tuta.io>
2024-02-07 05:23:05 +0330

    fix some grpc calls

--- app/marznode/__init__.py
+++ app/marznode/__init__.py
@@ -1,9 +1,9 @@
-"""app.marznode stores nodes and provides entities to communicate with the nodes"""
+"""stores nodes and provides entities to communicate with the nodes"""
 from typing import Dict
 
-import app.marznode.operations as operations
-from app.marznode.base import MarzNodeBase
-from app.marznode.grpc import MarzNodeGRPC
+from . import operations
+from .base import MarzNodeBase
+from .grpc import MarzNodeGRPC
 
 nodes: Dict[int, MarzNodeBase] = {}
 
--- app/marznode/grpc.py
+++ app/marznode/grpc.py
@@ -3,33 +3,46 @@ from grpclib.health.v1.health_grpc import HealthStub
 
 from .base import MarzNodeBase
 from .marznode_grpc import MarzServiceStub
-from .marznode_pb2 import UserUpdate, Empty, User
+from .marznode_pb2 import UserUpdate, Empty, User, Inbound
 
 
 class MarzNodeGRPC(MarzNodeBase):
     def __init__(self, address: str, port: int, ssl_key: str, ssl_cert: str):  # TODO: implement ssl
         self._address = address
         self._port = port
-        self._channel = Channel(self.address, self.port)
+        self._channel = Channel(self._address, self._port)
         self._stub = MarzServiceStub(self._channel)
         self._health = HealthStub(self._channel)
 
-    async def add_user(self, user, inbounds):
+    async def add_user(self, user, inbounds: list[str] | None = None) -> None:
+        if inbounds is None:
+            inbounds = []
+
         async with self._stub.AddUser.open() as stm:
-            await stm.send_message(UserUpdate(user=User(id=user.id, username=user.name, key=user.key),
-                                              inbound_additions=[]))
+            await stm.send_message(UserUpdate(user=User(id=user.id, username=user.username, key=user.key),
+                                              inbound_additions=[Inbound(tag=i) for i in inbounds]))
             await stm.recv_message()
 
-    async def remove_user(self, user, inbounds=[]):
+    async def remove_user(self, user, inbounds: list[str] | None = None) -> None:
+        if inbounds is None:
+            inbounds = []
         async with self._stub.RemoveUser.open() as stm:
-            await stm.send_message(UserUpdate(user=User(id=user.id, username=user.name, key=user.key),
-                                              inbound_reductions=[]))
+            await stm.send_message(UserUpdate(user=User(id=user.id, username=user.username, key=user.key),
+                                              inbound_reductions=[Inbound(tag=i) for i in inbounds]))
             await stm.recv_message()
 
-    async def update_user_inbounds(self):
-        async with self._stub.AddUser.open() as stm:
-            await stm.send_message(UserUpdate(user=User(id=user.id, username=user.name, key=user.key),
-                                              inbound_additions=[]))
+    async def update_user_inbounds(self, user,
+                                   inbound_additions: list[str] | None = None,
+                                   inbound_reductions: list[str] | None = None) -> None:
+        if inbound_reductions is None:
+            inbound_reductions = []
+        if inbound_additions is None:
+            inbound_additions = []
+
+        async with self._stub.UpdateUserInbounds.open() as stm:
+            await stm.send_message(UserUpdate(user=User(id=user.id, username=user.username, key=user.key),
+                                              inbound_additions=[Inbound(tag=i) for i in inbound_additions],
+                                              inbound_reductions=[Inbound(tag=i) for i in inbound_reductions]))
             await stm.recv_message()
 
     async def repopulate_users(self, users):
@@ -39,6 +52,6 @@ class MarzNodeGRPC(MarzNodeBase):
         raise NotImplementedError
 
     async def fetch_inbounds(self):
-        async with self._stub.AddUser.open() as stm:
+        async with self._stub.FetchInbounds.open() as stm:
             await stm.send_message(Empty)
             await stm.recv_message()
