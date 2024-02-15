64573780ae4eac47b181e7782c512643aa86d182
Dawsh <156741006+khodedawsh@users.noreply.github.com>
2024-02-15 10:10:20 +0330

    refactor: fix database structure, remove duplicate indexes, remove useless parameters in models.py (#57)

--- app/db/models.py
+++ app/db/models.py
@@ -18,13 +18,13 @@ from app.models.user import (ReminderType, UserDataLimitResetStrategy,
 class Admin(Base):
     __tablename__ = "admins"
 
-    id = Column(Integer, primary_key=True, index=True)
+    id = Column(Integer, primary_key=True)
     username = Column(String(32), unique=True, index=True)
     hashed_password = Column(String(128))
     users = relationship("User", back_populates="admin")
     created_at = Column(DateTime, default=datetime.utcnow)
     is_sudo = Column(Boolean, default=False)
-    password_reset_at = Column(DateTime, nullable=True)
+    password_reset_at = Column(DateTime)
 
 
 inbounds_services = Table(
@@ -44,9 +44,9 @@ users_services = Table(
 
 class Service(Base):
     __tablename__ = "services"
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String(64), index=True)
-    # modifications = Column(String(1024), nullable=True, default=None)
+    id = Column(Integer, primary_key=True)
+    name = Column(String(64))
+    # modifications = Column(String(1024))
     users = relationship("User", secondary=users_services, back_populates="services")
     inbounds = relationship("Inbound", secondary=inbounds_services, back_populates="services")
 
@@ -54,9 +54,9 @@ class Service(Base):
 class User(Base):
     __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True, index=True)
-    username = Column(String(32, collation='NOCASE'), unique=True, index=True)
-    key = Column(String(64), unique=True, index=True)
+    id = Column(Integer, primary_key=True)
+    username = Column(String(32), unique=True, index=True)
+    key = Column(String(64), unique=True)
     services = relationship("Service", secondary=users_services, back_populates="users", lazy="joined")
     inbounds = relationship(
         "Inbound",
@@ -71,26 +71,26 @@ class User(Base):
                                lazy="joined")
     notification_reminders = relationship("NotificationReminder", back_populates="user",
                                           cascade="all,delete,delete-orphan")
-    data_limit = Column(BigInteger, nullable=True)
+    data_limit = Column(BigInteger)
     data_limit_reset_strategy = Column(
         Enum(UserDataLimitResetStrategy),
         nullable=False,
         default=UserDataLimitResetStrategy.no_reset,
     )
     ip_limit = Column(Integer, nullable=False, default=-1)
-    settings = Column(String, nullable=True, default=None)
+    settings = Column(String)
     usage_logs = relationship("UserUsageResetLogs", back_populates="user", lazy="joined")
-    expire = Column(Integer, nullable=True)
+    expire = Column(Integer)
     admin_id = Column(Integer, ForeignKey("admins.id"))
     admin = relationship("Admin", back_populates="users")
-    sub_updated_at = Column(DateTime, nullable=True, default=None)
-    sub_last_user_agent = Column(String(512), nullable=True, default=None)
+    sub_updated_at = Column(DateTime)
+    sub_last_user_agent = Column(String(512))
     created_at = Column(DateTime, default=datetime.utcnow)
-    note = Column(String(500), nullable=True, default=None)
-    online_at = Column(DateTime, nullable=True, default=None)
-    on_hold_expire_duration = Column(BigInteger, nullable=True, default=None)
-    on_hold_timeout = Column(DateTime, nullable=True, default=None)
-    edit_at = Column(DateTime, nullable=True, default=None)
+    note = Column(String(500))
+    online_at = Column(DateTime)
+    on_hold_expire_duration = Column(BigInteger)
+    on_hold_timeout = Column(DateTime)
+    edit_at = Column(DateTime)
 
     @property
     def lifetime_used_traffic(self):
@@ -107,7 +107,7 @@ class User(Base):
 class UserUsageResetLogs(Base):
     __tablename__ = "user_usage_logs"
 
-    id = Column(Integer, primary_key=True, index=True)
+    id = Column(Integer, primary_key=True)
     user_id = Column(Integer, ForeignKey("users.id"))
     user = relationship("User", back_populates="usage_logs")
     used_traffic_at_reset = Column(BigInteger, nullable=False)
@@ -116,10 +116,13 @@ class UserUsageResetLogs(Base):
 
 class Inbound(Base):
     __tablename__ = "inbounds"
+    __table_args__ = (
+        UniqueConstraint("node_id", "tag"),
+    )
 
     id = Column(Integer, primary_key=True)
     protocol = Column(Enum(ProxyTypes))
-    tag = Column(String(256), nullable=False, index=True)
+    tag = Column(String(256), nullable=False)
     config = Column(String(), nullable=False)
     node_id = Column(Integer, ForeignKey("nodes.id"), index=True)
     node = relationship("Node", back_populates="inbounds")
@@ -133,28 +136,25 @@ class InboundHost(Base):
     __tablename__ = "hosts"
 
     id = Column(Integer, primary_key=True)
-    remark = Column(String(256), unique=False, nullable=False)
-    address = Column(String(256), unique=False, nullable=False)
-    port = Column(Integer, nullable=True)
-    path = Column(String(256), unique=False, nullable=True)
-    sni = Column(String(256), unique=False, nullable=True)
-    host = Column(String(256), unique=False, nullable=True)
+    remark = Column(String(256), nullable=False)
+    address = Column(String(256), nullable=False)
+    port = Column(Integer)
+    path = Column(String(256))
+    sni = Column(String(256))
+    host = Column(String(256))
     security = Column(
         Enum(InboundHostSecurity),
-        unique=False,
         nullable=False,
         default=InboundHostSecurity.inbound_default,
     )
     alpn = Column(
         Enum(InboundHostALPN),
-        unique=False,
         nullable=False,
         default=InboundHostSecurity.none,
         server_default=InboundHostSecurity.none.name
     )
     fingerprint = Column(
         Enum(InboundHostFingerprint),
-        unique=False,
         nullable=False,
         default=InboundHostSecurity.none,
         server_default=InboundHostSecurity.none.name
@@ -162,14 +162,14 @@ class InboundHost(Base):
 
     inbound_id = Column(Integer, ForeignKey("inbounds.id"), nullable=False)
     inbound = relationship("Inbound", back_populates="hosts")
-    allowinsecure = Column(Boolean, nullable=True)
-    is_disabled = Column(Boolean, nullable=True, default=False)
+    allowinsecure = Column(Boolean, default=False)
+    is_disabled = Column(Boolean, default=False)
 
 
 class System(Base):
     __tablename__ = "system"
 
-    id = Column(Integer, primary_key=True, index=True)
+    id = Column(Integer, primary_key=True)
     uplink = Column(BigInteger, default=0)
     downlink = Column(BigInteger, default=0)
 
@@ -193,15 +193,18 @@ class TLS(Base):
 
 class Node(Base):
     __tablename__ = "nodes"
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String(256, collation='NOCASE'), unique=True)
-    address = Column(String(256), unique=False, nullable=True)
-    port = Column(Integer, unique=False, nullable=True)
-    xray_version = Column(String(32), nullable=True)
-    inbounds = relationship("Inbound", back_populates="node")
+    __table_args__ = (
+        UniqueConstraint("address", "port"),
+    )
+    id = Column(Integer, primary_key=True)
+    name = Column(String(256), unique=True)
+    address = Column(String(256))
+    port = Column(Integer)
+    xray_version = Column(String(32))
+    inbounds = relationship("Inbound", back_populates="node", cascade="all, delete")
     status = Column(Enum(NodeStatus), nullable=False, default=NodeStatus.unhealthy)
     last_status_change = Column(DateTime, default=datetime.utcnow)
-    message = Column(String(1024), nullable=True)
+    message = Column(String(1024))
     created_at = Column(DateTime, default=datetime.utcnow)
     uplink = Column(BigInteger, default=0)
     downlink = Column(BigInteger, default=0)
@@ -216,8 +219,8 @@ class NodeUserUsage(Base):
         UniqueConstraint('created_at', 'user_id', 'node_id'),
     )
 
-    id = Column(Integer, primary_key=True, index=True)
-    created_at = Column(DateTime, unique=False, nullable=False)  # one hour per record
+    id = Column(Integer, primary_key=True)
+    created_at = Column(DateTime, nullable=False)  # one hour per record
     user_id = Column(Integer, ForeignKey("users.id"))
     user = relationship("User", back_populates="node_usages")
     node_id = Column(Integer, ForeignKey("nodes.id"))
@@ -231,8 +234,8 @@ class NodeUsage(Base):
         UniqueConstraint('created_at', 'node_id'),
     )
 
-    id = Column(Integer, primary_key=True, index=True)
-    created_at = Column(DateTime, unique=False, nullable=False)  # one hour per record
+    id = Column(Integer, primary_key=True)
+    created_at = Column(DateTime, nullable=False)  # one hour per record
     node_id = Column(Integer, ForeignKey("nodes.id"))
     node = relationship("Node", back_populates="usages")
     uplink = Column(BigInteger, default=0)
@@ -242,9 +245,9 @@ class NodeUsage(Base):
 class NotificationReminder(Base):
     __tablename__ = "notification_reminders"
 
-    id = Column(Integer, primary_key=True, index=True)
+    id = Column(Integer, primary_key=True)
     user_id = Column(Integer, ForeignKey("users.id"))
     user = relationship("User", back_populates="notification_reminders")
     type = Column(Enum(ReminderType), nullable=False)
-    expires_at = Column(DateTime, nullable=True)
+    expires_at = Column(DateTime)
     created_at = Column(DateTime, default=datetime.utcnow)
