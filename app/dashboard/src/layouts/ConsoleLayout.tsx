261deec306344ff84aa7f70d6e0da9df1b32e739
Krr0ptioN <mardin-pop@protonmail.com>
2024-02-17 01:46:16 +0300

    refact: layout for dashboard

---
+++ app/dashboard/src/layouts/ConsoleLayout.tsx
@@ -0,0 +1,33 @@
+import { Grid, GridItem } from '@chakra-ui/react';
+import { Sidebar } from 'components/Sidebar';
+import { Suspense } from 'react';
+import { Outlet } from 'react-router-dom';
+
+export const ConsoleLayout = () => {
+  return (
+    <Grid
+      templateColumns="repeat(24, 1fr)"
+      display={{
+        base: 'flex',
+        md: 'grid',
+      }}
+      flexDirection="column"
+      gap={{
+        base: 0,
+        md: 4,
+      }}
+      minH="100vh"
+    >
+      <GridItem colSpan={{ md: 5 }} minW={{ md: '300px' }}>
+        <Sidebar />
+      </GridItem>
+      <GridItem colSpan={{ md: 19 }} flexGrow={1}>
+        <Suspense>
+          <Outlet />
+        </Suspense>
+      </GridItem>
+    </Grid>
+  );
+};
+
+export default ConsoleLayout;
