07b6b42a7c28199e1c442bccd4f961ef82cadd46
Krr0ptioN <mardin-pop@protonmail.com>
2024-02-17 01:49:02 +0300

    refact: no neead for element

--- app/dashboard/src/constants/Pages.tsx
+++ app/dashboard/src/constants/Pages.tsx
@@ -2,11 +2,6 @@ import { PageType } from 'types/Page';
 import { chakra } from '@chakra-ui/react';
 import { CubeIcon, HomeIcon, ServerIcon, ServerStackIcon, UsersIcon } from '@heroicons/react/24/outline';
 import { IconBaseStyle } from 'constants/IconBaseStyle';
-import { UsersPage } from 'pages/Users';
-import { NodesPage } from 'pages/Nodes';
-import { ServicesPage } from 'pages/Services';
-import { HostPage } from 'pages/Host';
-import { Dashboard } from 'pages/Dashboard';
 
 const UsersSidebarItemIcon = chakra(UsersIcon, IconBaseStyle);
 const ServicesSidebarItemIcon = chakra(ServerStackIcon, IconBaseStyle);
@@ -19,34 +14,25 @@ export const pages: PageType[] = [
     name: 'pageHeaderTitle.Home',
     path: '/',
     itemIcon: HomeSidebarItemIcon,
-    element: <Dashboard />,
   },
   {
     name: 'pageHeaderTitle.Users',
     path: '/users',
     itemIcon: UsersSidebarItemIcon,
-    element: <UsersPage />,
   },
   {
     name: 'pageHeaderTitle.Services',
     path: '/services',
     itemIcon: ServicesSidebarItemIcon,
-    element: <ServicesPage />,
   },
   {
     name: 'pageHeaderTitle.Nodes',
     path: '/nodes',
     itemIcon: NodesSidebarItemIcon,
-    element: <NodesPage />,
   },
   {
     name: 'pageHeaderTitle.Host',
     path: '/host',
     itemIcon: HostSidebarItemIcon,
-    element: <HostPage />,
   },
 ];
-
-export type PageRoute = Pick<PageType,
-  | 'path'
-  | 'element'>
