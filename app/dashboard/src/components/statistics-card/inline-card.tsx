848ff102c3f5f70b5a82d7d8f530137bc305e9e5
Krr0ptioN <mardin-pop@protonmail.com>
2024-02-18 01:34:46 +0300

    feat(statistics-widget): reusable and and optional widgets for charts and graphs
    - Inline widget for small and single variable statistics
    - Wide widget for bigger and charts

---
+++ app/dashboard/src/components/statistics-card/inline-card.tsx
@@ -0,0 +1,19 @@
+import { StatisticsWidget } from './widget';
+import { FC, PropsWithChildren } from 'react';
+import { StatisticCardProps } from './statistics-props';
+
+export const StatisticsCardInLine: FC<PropsWithChildren<StatisticCardProps>> = ({
+    title,
+    content,
+    icon }) => {
+    return (
+        <StatisticsWidget
+            title={title}
+            icon={icon}
+            content={content}
+            display="flex"
+            justifyContent="space-between"
+            flexDirection="row"
+        />
+    );
+};
---
+++ app/dashboard/src/components/statistics-card/statistics-props.tsx
@@ -0,0 +1,7 @@
+import { ReactElement, ReactNode } from 'react';
+
+export type StatisticCardProps = {
+    title: string;
+    content: ReactNode;
+    icon: ReactElement;
+};
