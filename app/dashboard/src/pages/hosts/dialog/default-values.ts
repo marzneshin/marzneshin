5809be453adc674034088ecd982c3ac93c55f4e5
Krr0ptioN <mardin-pop@protonmail.com>
2024-02-21 17:07:05 +0300

    fix(ci): linting

--- app/dashboard/src/pages/hosts/dialog/default-values.ts
+++ app/dashboard/src/pages/hosts/dialog/default-values.ts
@@ -1,14 +1,14 @@
 import { HostSchema } from 'stores';
 export const getDefaultValues = (): HostSchema => {
-    return {
-        host: '',
-        sni: '',
-        port: null,
-        path: '',
-        address: '',
-        remark: '',
-        security: 'inbound_default',
-        alpn: '',
-        fingerprint: '',
-    }
+  return {
+    host: '',
+    sni: '',
+    port: null,
+    path: '',
+    address: '',
+    remark: '',
+    security: 'inbound_default',
+    alpn: '',
+    fingerprint: '',
+  }
 };
