7a0393d416bb2620486088cb397295390c25bcb5
Krr0ptioN <mardin-pop@protonmail.com>
2024-02-23 20:11:55 +0300

    chore: fix linting

--- app/dashboard/src/pages/nodes/table/action-buttons.tsx
+++ app/dashboard/src/pages/nodes/table/action-buttons.tsx
@@ -5,47 +5,47 @@ import { chakra, HStack, IconButton, Tooltip } from '@chakra-ui/react';
 import { FC } from 'react';
 
 const iconProps = {
-    baseStyle: {
-        w: 4,
-        h: 4,
-    },
+  baseStyle: {
+    w: 4,
+    h: 4,
+  },
 };
 const CoreSettingsIcon = chakra(Cog6ToothIcon, iconProps);
 type ActionButtonsProps = {};
 
 export const ActionButtons: FC<ActionButtonsProps> = () => {
-    return (
-        <HStack
-            justifyContent="flex-end"
-            onClick={(e) => {
-                e.preventDefault();
-                e.stopPropagation();
-            }}
+  return (
+    <HStack
+      justifyContent="flex-end"
+      onClick={(e) => {
+        e.preventDefault();
+        e.stopPropagation();
+      }}
+    >
+      <Tooltip
+        label={
+          t('nodesTable.coreSettings')
+        }
+        placement="top"
+      >
+        <IconButton
+          p="0 !important"
+          aria-label="core settings"
+          bg="transparent"
+          _dark={{
+            _hover: {
+              bg: 'gray.700',
+            },
+          }}
+          size={{
+            base: 'sm',
+            md: 'md',
+          }}
         >
-            <Tooltip
-                label={
-                    t('nodesTable.coreSettings')
-                }
-                placement="top"
-            >
-                <IconButton
-                    p="0 !important"
-                    aria-label="core settings"
-                    bg="transparent"
-                    _dark={{
-                        _hover: {
-                            bg: 'gray.700',
-                        },
-                    }}
-                    size={{
-                        base: 'sm',
-                        md: 'md',
-                    }}
-                >
-                    <CoreSettingsIcon />
-                </IconButton>
-            </Tooltip>
-        </HStack >
-    );
+          <CoreSettingsIcon />
+        </IconButton>
+      </Tooltip>
+    </HStack >
+  );
 };
 
