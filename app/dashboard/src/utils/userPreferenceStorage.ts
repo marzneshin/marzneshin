3034187c8521d76a208afbe827e96411d5b45af7
Krr0ptioN <mardin-pop@protonmail.com>
2024-02-24 13:03:15 +0300

    refact: user preference for handling row per page for table

--- app/dashboard/src/utils/userPreferenceStorage.ts
+++ app/dashboard/src/utils/userPreferenceStorage.ts
@@ -1,40 +1,37 @@
-const NUM_USERS_PER_PAGE_LOCAL_STORAGE_KEY = 'marzban-num-users-per-page';
-const NUM_SERVICES_PER_PAGE_LOCAL_STORAGE_KEY = 'marzban-num-services-per-page';
-const NUM_NODES_PER_PAGE_LOCAL_STORAGE_KEY = 'marzban-num-nodes-per-page';
-const NUM_USERS_PER_PAGE_DEFAULT = 10;
-const NUM_NODES_PER_PAGE_DEFAULT = 10;
-const NUM_SERVICES_PER_PAGE_DEFAULT = 10;
-
-export const getUsersPerPageLimitSize = () => {
-  const numUsersPerPage =
-    localStorage.getItem(NUM_USERS_PER_PAGE_LOCAL_STORAGE_KEY) ||
-    NUM_USERS_PER_PAGE_DEFAULT.toString(); // this catches `null` values
-  return parseInt(numUsersPerPage) || NUM_USERS_PER_PAGE_DEFAULT; // this catches NaN values
-};
-
-export const setUsersPerPageLimitSize = (value: string) => {
-  return localStorage.setItem(NUM_USERS_PER_PAGE_LOCAL_STORAGE_KEY, value);
-};
 
 
-export const getServicesPerPageLimitSize = () => {
-  const numServicesPerPage =
-    localStorage.getItem(NUM_SERVICES_PER_PAGE_LOCAL_STORAGE_KEY) ||
-    NUM_SERVICES_PER_PAGE_DEFAULT.toString(); // this catches `null` values
-  return parseInt(numServicesPerPage) || NUM_SERVICES_PER_PAGE_DEFAULT; // this catches NaN values
-};
+export class PageSizeManager {
+  private localStorageKey: string;
+  private defaultPageSize: number;
 
-export const setServicesPerPageLimitSize = (value: string) => {
-  return localStorage.setItem(NUM_SERVICES_PER_PAGE_LOCAL_STORAGE_KEY, value);
-};
+  constructor(localStorageKey: string, defaultPageSize: number) {
+    this.localStorageKey = `marzneshin-num-${localStorageKey}-per-page`;
+    this.defaultPageSize = defaultPageSize;
+  }
 
-export const getNodesPerPageLimitSize = () => {
-  const numNodesPerPage =
-    localStorage.getItem(NUM_NODES_PER_PAGE_LOCAL_STORAGE_KEY) ||
-    NUM_NODES_PER_PAGE_DEFAULT.toString(); // this catches `null` values
-  return parseInt(numNodesPerPage) || NUM_NODES_PER_PAGE_DEFAULT; // this catches NaN values
-};
+  private getStoredPageSize(): string | null {
+    return localStorage.getItem(this.localStorageKey);
+  }
 
-export const setNodesPerPageLimitSize = (value: string) => {
-  return localStorage.setItem(NUM_NODES_PER_PAGE_LOCAL_STORAGE_KEY, value);
-};
+  getPageSize(): number {
+    const storedPageSize = this.getStoredPageSize();
+    const parsedPageSize = parseInt(storedPageSize || '', 10);
+    return isNaN(parsedPageSize) ? this.defaultPageSize : parsedPageSize;
+  }
+
+  setPageSize(value: string): void {
+    localStorage.setItem(this.localStorageKey, value);
+  }
+}
+
+const usersPageSizeManager = new PageSizeManager('users', 10);
+const servicesPageSizeManager = new PageSizeManager('services', 10);
+const nodesPageSizeManager = new PageSizeManager('nodes', 10);
+const hostsPageSizeManager = new PageSizeManager('hosts', 10);
+
+export const pageSizeManagers = {
+  users: usersPageSizeManager,
+  nodes: nodesPageSizeManager,
+  hosts: hostsPageSizeManager,
+  services: servicesPageSizeManager,
+}
