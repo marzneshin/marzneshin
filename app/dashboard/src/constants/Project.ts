e1438cf7d0d718f4d6f751c5479d753719dbd242
Krr0ptioN <mardin-pop@protonmail.com>
2024-02-25 19:34:32 +0300

    chore: add co-contrib

--- app/dashboard/src/constants/Project.ts
+++ app/dashboard/src/constants/Project.ts
@@ -1,3 +1,4 @@
 export const REPO_URL = 'https://github.com/khodedawsh/Marzneshin';
 export const ORGANIZATION_URL = 'https://github.com/khodedawosh';
+export const CO_CONTRIBUTOR = 'https://mardin.cc/';
 export const DONATION_URL = 'https://github.com/khodedawosh/Marzneshin#donation';
