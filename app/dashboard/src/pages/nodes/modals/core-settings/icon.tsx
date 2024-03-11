45d70abc04d998d09048b2a6e48aaebbf2bf5a32
Krr0ptioN <mardin-pop@protonmail.com>
2024-03-12 02:04:12 +0300

    remove: restarting button temperory

--- app/dashboard/src/pages/nodes/modals/core-settings/icon.tsx
+++ app/dashboard/src/pages/nodes/modals/core-settings/icon.tsx
@@ -1,34 +1,34 @@
 import { chakra } from '@chakra-ui/react';
 import {
-    ArrowPathIcon,
-    ArrowsPointingInIcon,
-    ArrowsPointingOutIcon,
-    Cog6ToothIcon,
+  ArrowPathIcon,
+  ArrowsPointingInIcon,
+  ArrowsPointingOutIcon,
+  Cog6ToothIcon,
 } from '@heroicons/react/24/outline';
 
 export const UsageIcon = chakra(Cog6ToothIcon, {
-    baseStyle: {
-        w: 5,
-        h: 5,
-    },
+  baseStyle: {
+    w: 5,
+    h: 5,
+  },
 });
 
 export const ReloadIcon = chakra(ArrowPathIcon, {
-    baseStyle: {
-        w: 4,
-        h: 4,
-    },
+  baseStyle: {
+    w: 4,
+    h: 4,
+  },
 });
 
 export const FullScreenIcon = chakra(ArrowsPointingOutIcon, {
-    baseStyle: {
-        w: 4,
-        h: 4,
-    },
+  baseStyle: {
+    w: 4,
+    h: 4,
+  },
 });
 export const ExitFullScreenIcon = chakra(ArrowsPointingInIcon, {
-    baseStyle: {
-        w: 3,
-        h: 3,
-    },
+  baseStyle: {
+    w: 3,
+    h: 3,
+  },
 });
