219e7bb585c76a848f20831c119e4d3c88d1bfe8
Krr0ptioN <mardin-pop@protonmail.com>
2024-03-19 02:32:02 +0300

    feat(comp): theme switch

---
+++ dashboard/src/features/theme-switch/index.ts
@@ -0,0 +1,2 @@
+export * from './theme-provider';
+export * from './theme-toggle';
---
+++ dashboard/src/features/theme-switch/theme-toggle/index.tsx
@@ -0,0 +1,37 @@
+import { Moon, Sun } from "lucide-react"
+
+import {
+    Button,
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuTrigger,
+} from "@marzneshin/components"
+import { useTheme } from "../theme-provider"
+
+export function ThemeToggle() {
+    const { setTheme } = useTheme()
+
+    return (
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+                <Button variant="outline" size="icon">
+                    <Sun className="transition-all scale-100 rotate-0 dark:scale-0 dark:-rotate-90 h-[1.2rem] w-[1.2rem]" />
+                    <Moon className="absolute transition-all scale-0 rotate-90 dark:scale-100 dark:rotate-0 h-[1.2rem] w-[1.2rem]" />
+                    <span className="sr-only">Toggle theme</span>
+                </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+                <DropdownMenuItem onClick={() => setTheme("light")}>
+                    Light
+                </DropdownMenuItem>
+                <DropdownMenuItem onClick={() => setTheme("dark")}>
+                    Dark
+                </DropdownMenuItem>
+                <DropdownMenuItem onClick={() => setTheme("system")}>
+                    System
+                </DropdownMenuItem>
+            </DropdownMenuContent>
+        </DropdownMenu>
+    )
+}
