f506598d3dbe824a1c053ff4b26cc653e54b7b91
Krr0ptioN <mardin-pop@protonmail.com>
2024-03-19 12:43:43 +0300

    feat(login-form): login page

--- dashboard/src/routes/_auth/login.tsx
+++ dashboard/src/routes/_auth/login.tsx
@@ -1,5 +1,27 @@
+import { Card, CardContent, CardHeader, CardTitle } from '@marzneshin/components';
+import { LoginForm } from '@marzneshin/features/auth';
 import { createFileRoute } from '@tanstack/react-router'
+import { FC } from 'react'
+import { useTranslation } from 'react-i18next';
+
+const LoginPage: FC = () => {
+  const { t } = useTranslation();
+  return (
+    <div className='flex flex-row justify-center items-center p-4 w-full h-full'>
+      <Card className="w-1/2">
+        <CardHeader>
+          <CardTitle className="font-header text-primary">
+            {t('login')}
+          </CardTitle>
+        </CardHeader>
+        <CardContent>
+          <LoginForm />
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
 
 export const Route = createFileRoute('/_auth/login')({
-  component: () => <div>Hello /_auth/login!</div>
+  component: () => <LoginPage />
 })
