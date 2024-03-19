45ca10cec4517d66ddd408290085814e9b0a45b6
Krr0ptioN <mardin-pop@protonmail.com>
2024-03-19 03:03:16 +0300

    feat: internationalization

--- dashboard/src/app.tsx
+++ dashboard/src/app.tsx
@@ -1,6 +1,7 @@
 import React, { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider, createRouter } from '@tanstack/react-router'
+import '@marzneshin/features/i18n'
 import './globals.css'
 
 import { routeTree } from './routeTree.gen'
--- dashboard/src/routes/index.lazy.tsx
+++ dashboard/src/routes/index.lazy.tsx
@@ -1,16 +1,18 @@
 import { Button } from '@marzneshin/components'
 import { ThemeToggle } from '@marzneshin/features/theme-switch'
 import { createLazyFileRoute } from '@tanstack/react-router'
+import { useTranslation } from 'react-i18next';
 
 export const Route = createLazyFileRoute('/')({
     component: Index,
 })
 
 function Index() {
+    const { t } = useTranslation();
     return (
         <div className="p-2">
             <ThemeToggle />
-            <Button>Hello</Button>
+            <Button>{t('name')}</Button>
         </div>
     )
 }
