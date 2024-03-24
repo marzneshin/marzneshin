d86d2685cb335c9d82ea65ec9428536b61959c0f
Krr0ptioN <mardin-pop@protonmail.com>
2024-03-24 12:43:52 +0300

    improve(nodes): log container and status

--- dashboard/src/features/nodes/log/container.tsx
+++ dashboard/src/features/nodes/log/container.tsx
@@ -1,6 +1,7 @@
 import { Badge, Card, CardContent, CardHeader, ScrollArea } from "@marzneshin/components"
-import { NodeType, NodesStatus, NodesStatusBadge, useNodesLog } from ".."
+import { NodeType, useNodesLog } from ".."
 import { FC } from "react"
+import { useTranslation } from "react-i18next"
 
 interface LogContainerProps {
     node: NodeType
@@ -8,18 +9,16 @@ interface LogContainerProps {
 
 export const LogContainer: FC<LogContainerProps> = ({ node }) => {
     const { logs, status, logsDiv } = useNodesLog(node)
-    console.log(logs.length)
+    const { t } = useTranslation()
     return (
         <Card className="h-1/2">
             <CardContent className="flex flex-col p-1 h-full" >
                 <CardHeader className="flex flex-row justify-between items-center p-2">
-                    <p className="font-header">Marznode status logs</p>
+                    <p className="capitalize font-header">{t('page.nodes.settings.log-container.title')}</p>
                     <div className="flex flex-row gap-2 justify-center items-center">
-                        <NodesStatusBadge status={NodesStatus[node.status]} />
-                        <Badge className="flex justify-center items-center w-full h-6 capitalize">
+                        <Badge variant={status === "closed" ? "destructive" : "default"} className="flex justify-center items-center w-full h-6 capitalize">
                             {status}
                         </Badge>
-
                     </div>
                 </CardHeader>
                 <ScrollArea className="flex p-2 m-1 h-full rounded-b-md divide-dashed divide-accent bg-secondary text-primary" ref={logsDiv}>
