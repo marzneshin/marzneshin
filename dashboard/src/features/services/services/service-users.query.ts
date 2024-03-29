d122d28bb5c6016aaebeb9ca0e1e687d07b9bc4e
Krr0ptioN <mardin-pop@protonmail.com>
2024-03-29 23:35:49 +0300

    feat(servicese): users table for features

--- dashboard/src/features/services/services/service-users.query.ts
+++ dashboard/src/features/services/services/service-users.query.ts
@@ -3,22 +3,9 @@ import { fetch } from "@marzneshin/utils";
 import { ServiceType } from "../types";
 import { UserType } from "@marzneshin/features/users";
 
-interface ServiceResponse {
-    id: number
-    name: string
-    inbounds: any[]
-    user_ids: number[]
-    inbound_ids: number[]
-    users: UserType[]
-}
-
 export async function fetchUsersService({ queryKey }: { queryKey: [string, number] }): Promise<UserType[]> {
-    return fetch('/services').then((result) => {
-        const users: UserType[] =
-            result.items
-                .find((fetchedService: ServiceResponse) => fetchedService.id === queryKey[1])
-                .users
-        return users;
+    return fetch(`/services/${queryKey[1]}`).then((result) => {
+        return result.users;
     });
 }
 
--- dashboard/src/features/services/tables/users/columns.tsx
+++ dashboard/src/features/services/tables/users/columns.tsx
@@ -1,8 +1,9 @@
 import { ColumnDef } from "@tanstack/react-table"
-import { UserType } from '@marzneshin/features/users'
 import { DataTableColumnHeader } from "@marzneshin/components/data-table/column-header"
 import i18n from "@marzneshin/features/i18n"
-import { Badge } from "@marzneshin/components"
+import { UserType, UserStatusBadge, statusColors } from "@marzneshin/features/users"
+import { CircularProgress } from "@nextui-org/progress";
+import { format } from "date-fns";
 
 export const columns: ColumnDef<UserType>[] = [
     {
@@ -12,18 +13,36 @@ export const columns: ColumnDef<UserType>[] = [
     {
         accessorKey: 'status',
         header: ({ column }) => <DataTableColumnHeader title={i18n.t('status')} column={column} />,
-        cell: ({ row }) => (
-            <Badge className="py-1 px-2"> {row.original.status} </Badge>
-        )
+        cell: ({ row }) => <UserStatusBadge status={statusColors[row.original.status ? row.original.status : 'error']} />,
     },
     {
-        accessorKey: 'services',
-        header: ({ column }) => <DataTableColumnHeader title={i18n.t('services')} column={column} />,
-        cell: ({ row }) => row.original.services.length
+        accessorKey: "used_traffic",
+        header: ({ column }) => <DataTableColumnHeader title={i18n.t('page.users.used_traffic')} column={column} />,
+        cell: ({ row }) => {
+            if (row.original.data_limit) {
+                return (
+                    <CircularProgress
+                        size="sm"
+                        value={row.original.used_traffic / row.original.data_limit * 100}
+                    />
+                )
+            } else {
+                return <Badge>No Limit</Badge>
+            }
+        },
+        sortingFn: (rowA, rowB) => {
+            if (rowA.original.data_limit && rowB.original.data_limit) {
+                const rowAUsedTraffic = (rowA.original.used_traffic / rowA.original.data_limit) * 100
+                const rowBUsedTraffic = (rowA.original.used_traffic / rowA.original.data_limit) * 100
+                return rowAUsedTraffic > rowBUsedTraffic ? 1 : rowBUsedTraffic > rowAUsedTraffic ? -1 : 0
+            } else {
+                return 0
+            }
+        }
     },
     {
         accessorKey: 'expire',
         header: ({ column }) => <DataTableColumnHeader title={i18n.t('expire')} column={column} />,
-        cell: ({ row }) => row.original.expire?.toString
+        cell: ({ row }) => format(row.original.expire, "PP")
     }
 ];
