84b3ed9a0c83c0b4e4dd153a35f00ea6bc2d7276
Krr0ptioN <mardin-pop@protonmail.com>
2024-03-30 01:13:59 +0300

    chore: add next-ui circular-progress comp package

--- dashboard/tailwind.config.js
+++ dashboard/tailwind.config.js
@@ -2,6 +2,7 @@ import tailwindcssAnimate from 'tailwindcss-animate';
 import tailwindcssForm from '@tailwindcss/forms';
 import tailwindcssAspectRatio from '@tailwindcss/aspect-ratio';
 import tailwindcssTypography from '@tailwindcss/typography';
+import { nextui } from '@nextui-org/react'
 
 /** @type {import('tailwindcss').Config} */
 const config = {
@@ -11,6 +12,7 @@ const config = {
     './components/**/*.{ts,tsx}',
     './app/**/*.{ts,tsx}',
     './src/**/*.{ts,tsx}',
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   prefix: "",
   theme: {
@@ -90,6 +92,7 @@ const config = {
     },
   },
   plugins: [
+    nextui(),
     tailwindcssAnimate,
     tailwindcssForm,
     tailwindcssAspectRatio,
