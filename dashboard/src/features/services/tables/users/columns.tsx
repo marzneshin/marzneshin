70554da34c8fbf6debedc5787dc127e723fd846a
Krr0ptioN <mardin-pop@protonmail.com>
2024-03-31 01:54:53 +0300

    chore: temporaryly disabled until users page implementation

--- dashboard/src/features/services/tables/users/columns.tsx
+++ dashboard/src/features/services/tables/users/columns.tsx
@@ -1,20 +1,21 @@
 import { ColumnDef } from "@tanstack/react-table"
 import { DataTableColumnHeader } from "@marzneshin/components/data-table/column-header"
 import i18n from "@marzneshin/features/i18n"
-import { UserType, UserStatusBadge, statusColors } from "@marzneshin/features/users"
+import { UserType/* , UserStatusBadge, statusColors */ } from "@marzneshin/features/users"
 import { CircularProgress } from "@nextui-org/progress";
 import { format } from "date-fns";
+import { Badge } from "@marzneshin/components";
 
 export const columns: ColumnDef<UserType>[] = [
     {
         accessorKey: "username",
         header: ({ column }) => <DataTableColumnHeader title={i18n.t('username')} column={column} />,
     },
-    {
-        accessorKey: 'status',
-        header: ({ column }) => <DataTableColumnHeader title={i18n.t('status')} column={column} />,
-        cell: ({ row }) => <UserStatusBadge status={statusColors[row.original.status ? row.original.status : 'error']} />,
-    },
+    // {
+    //     accessorKey: 'status',
+    //     header: ({ column }) => <DataTableColumnHeader title={i18n.t('status')} column={column} />,
+    //     cell: ({ row }) => <UserStatusBadge status={statusColors[row.original.status ? row.original.status : 'error']} />,
+    // },
     {
         accessorKey: "used_traffic",
         header: ({ column }) => <DataTableColumnHeader title={i18n.t('page.users.used_traffic')} column={column} />,
