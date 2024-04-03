704a71c141e9815b33ab288ec52a0deb09e92637
Krr0ptioN <mardin-pop@protonmail.com>
2024-04-03 19:40:23 +0300

    refact(services): mutation dialog uses the mutation dialog hook

--- dashboard/src/features/services/services/create-service.mutate.ts
+++ dashboard/src/features/services/services/create-service.mutate.ts
@@ -1,16 +1,16 @@
-import { ServiceCreateType, ServicesQueryFetchKey } from "@marzneshin/features/services";
+import { ServiceMutationType, ServicesQueryFetchKey } from "@marzneshin/features/services";
 import { useMutation } from "@tanstack/react-query";
 import { fetch, queryClient } from "@marzneshin/utils";
 import { toast } from "sonner";
 import i18n from "@marzneshin/features/i18n";
 
-export async function fetchCreateService(service: ServiceCreateType): Promise<ServiceCreateType> {
+export async function fetchCreateService(service: ServiceMutationType): Promise<ServiceMutationType> {
     return fetch('/services', { method: 'post', body: service }).then((service) => {
         return service;
     });
 }
 
-const handleError = (error: Error, value: ServiceCreateType) => {
+const handleError = (error: Error, value: ServiceMutationType) => {
     toast.error(
         i18n.t('events.create.error', { name: value.name }),
         {
@@ -18,7 +18,7 @@ const handleError = (error: Error, value: ServiceCreateType) => {
         })
 }
 
-const handleSuccess = (value: ServiceCreateType) => {
+const handleSuccess = (value: ServiceMutationType) => {
     toast.success(
         i18n.t('events.create.success.title', { name: value.name }),
         {
--- dashboard/src/features/services/types/services-create.type.ts
+++
@@ -1,8 +0,0 @@
-import { z } from "zod";
-
-export const ServiceCreateSchema = z.object({
-    inbounds: z.array(z.number()),
-    name: z.string().trim().min(1)
-})
-
-export type ServiceCreateType = z.infer<typeof ServiceCreateSchema>
