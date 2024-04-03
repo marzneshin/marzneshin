52e47fe7eae7e4c8db77a3da80c84a40c91a78f0
Krr0ptioN <mardin-pop@protonmail.com>
2024-04-03 18:55:37 +0300

    refact: reducing boilerplate code of user info table

--- dashboard/src/features/users/dialogs/settings/user-info/table.tsx
+++ dashboard/src/features/users/dialogs/settings/user-info/table.tsx
@@ -1,90 +1,70 @@
-
-import {
-    Card,
-    CardContent,
-    CardHeader,
-    CardTitle,
-    Table,
-    TableBody,
-    TableCell,
-    TableHead,
-    TableRow,
-} from "@marzneshin/components"
-import { FC } from 'react'
+import { Card, CardContent, CardHeader, CardTitle, Table, TableBody, TableCell, TableRow } from "@marzneshin/components";
+import { FC } from 'react';
 import { UserType, UsersStatus, UsersStatusBadge } from '@marzneshin/features/users';
 import { CircularProgress } from "@nextui-org/progress";
 import { useTranslation } from "react-i18next";
+import { format, isDate, isValid } from "date-fns";
 
 interface UserInfoTableProps {
-    entity: UserType
+    entity: UserType;
 }
 
-export const UserInfoTable: FC<UserInfoTableProps> = (
-    { entity }
-) => {
-    const { t } = useTranslation()
+const TableRowWithCell: FC<{ label: string; value: string | number | JSX.Element }> = ({ label, value }) => (
+    <TableRow>
+        <TableCell>{label}</TableCell>
+        <TableCell>{value}</TableCell>
+    </TableRow>
+);
+
+const DateTableRow: FC<{ label: string; date: Date | string }> = ({ label, date }) => {
+    let formattedDate = '';
+    if (date && isValid(date)) {
+        formattedDate = format(date, "PPP");
+    }
+    return (
+        <TableRowWithCell label={label} value={formattedDate} />
+    );
+};
+
+const CircularProgressBarRow: FC<{ label: string; value: number; limit: number }> = ({ label, value, limit }) => (
+    <TableRow>
+        <TableCell>{label}</TableCell>
+        <TableCell><CircularProgress size="sm" value={value / limit * 100} /></TableCell>
+    </TableRow>
+);
+
+export const UserInfoTable: FC<UserInfoTableProps> = ({ entity }) => {
+    const { t } = useTranslation();
+
+    const expireDate = entity.expire ? (isDate(entity.expire) ? entity.expire : new Date(entity.expire)) : null;
     return (
         <Card>
             <CardHeader>
-                <CardTitle>
-                    {t('user_info')}
-                </CardTitle>
+                <CardTitle>{t('user_info')}</CardTitle>
             </CardHeader>
             <CardContent>
                 <Table>
                     <TableBody>
-                        <TableRow>
-                            <TableHead>
-                                {t('username')}
-                            </TableHead>
-                            <TableCell>
-                                {entity.username}
-                            </TableCell>
-                        </TableRow>
-                        <TableRow>
-                            <TableHead>
-                                {t('status')}
-                            </TableHead>
-                            <TableCell>
-                                <UsersStatusBadge status={UsersStatus[entity.status]} />
-                            </TableCell>
-                        </TableRow>
-                        <TableRow>
-                            <TableHead>
-                                {t('page.users.expire_date')}
-                            </TableHead>
-                            <TableCell>
-                            </TableCell>
-                        </TableRow>
+                        <TableRowWithCell label={t('username')} value={entity.username} />
+                        <TableRowWithCell label={t('status')} value={<UsersStatusBadge status={UsersStatus[entity.status]} />} />
+                        {(expireDate && entity.on_hold_expire_duration === 0) ? (
+                            <DateTableRow label={t('page.users.expire_date')} date={expireDate} />
+                        ) : (
+                            <>
+                                <TableRowWithCell label={t('page.users.on_hold_expire_duration')} value={entity.on_hold_expire_duration} />
+                                {entity.on_hold_timeout &&
+                                    <DateTableRow label={t('page.users.expire_date')} date={entity.on_hold_timeout} />
+                                }
+                            </>
+                        )}
                         {entity.data_limit &&
-                            <TableRow>
-                                <TableHead>
-                                    {t('page.users.used_traffic')}
-                                </TableHead>
-                                <TableCell>
-                                    <CircularProgress size="sm" value={entity.used_traffic / entity.data_limit * 100} />
-                                </TableCell>
-                            </TableRow>
+                            <CircularProgressBarRow label={t('page.users.used_traffic')} value={entity.used_traffic} limit={entity.data_limit} />
                         }
-                        <TableRow>
-                            <TableHead>
-                                {t('subscription_link')}
-                            </TableHead>
-                            <TableCell>
-                                {entity.username}
-                            </TableCell>
-                        </TableRow>
-                        <TableRow>
-                            <TableHead>
-                                {t('note')}
-                            </TableHead>
-                            <TableCell>
-                                {entity.note}
-                            </TableCell>
-                        </TableRow>
+                        <DateTableRow label={t('page.users.online_at')} date={entity.online_at} />
+                        <TableRowWithCell label={t('note')} value={entity.note} />
                     </TableBody>
                 </Table>
             </CardContent>
         </Card>
     );
-}
+};
