46b022a6236c1fb38f7c0ee447dd658131877921
Krr0ptioN <mardin-pop@protonmail.com>
2024-04-05 02:11:39 +0300

    docs(page): for future changes

--- dashboard/src/components/page/index.tsx
+++ dashboard/src/components/page/index.tsx
@@ -3,6 +3,11 @@ import { ScrollArea } from '@marzneshin/components'
 
 interface PageProps extends PropsWithChildren { }
 
+/* WARN: The current page height which effect the scroll
+*  area activness, is calculated based on height of header 
+*  and margin.
+*/
+
 export const Page: FC<PageProps> = ({ children }) => {
     return (
         <ScrollArea style={{ height: `calc(100vh - 4.5rem)` }} className="m-3">
