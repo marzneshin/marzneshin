7848cbcc2c6578b2ce9cbf3681671bdaf315f320
Krr0ptioN <mardin-pop@protonmail.com>
2024-04-07 16:19:15 +0300

    test(entity-table): render correctly

---
+++ dashboard/src/components/entity-table/tests/entity-table.test.tsx
@@ -0,0 +1,43 @@
+import { afterEach, describe, expect, it } from 'vitest'
+import { act, cleanup, render, screen } from '@testing-library/react'
+import user from "@testing-library/user-event"
+import { EntityTable } from '..'
+import { fetchMockApi } from './fetch-entity.mock'
+import { mockColumns } from './columns.mock'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { queryClient } from '@marzneshin/utils'
+import '@testing-library/jest-dom'
+import {
+    MockDeleteConfirmationDialog,
+    MockDialog,
+    MockSettingsDialog
+} from './dialogs.mock'
+
+
+
+describe("Entity Table", () => {
+    afterEach(() => {
+        cleanup()
+    })
+
+    it('Renders Successfuly', () => {
+        const entityTable = render(
+            <QueryClientProvider client={queryClient}>
+                <EntityTable
+                    fetchEntity={fetchMockApi}
+                    MutationDialog={MockDialog}
+                    DeleteConfirmationDialog={MockDeleteConfirmationDialog}
+                    SettingsDialog={MockSettingsDialog}
+                    columnsFn={mockColumns}
+                    filteredColumn="name"
+                    entityKey="users"
+                />
+            </QueryClientProvider>
+        )
+
+        expect(entityTable).toBeTruthy()
+    })
+    describe('Entity Table Pagination', () => {
+        it('Paginate entities', () => { })
+    })
+})
