e691957561036dd868616e6e625ee20dead351a0
Krr0ptioN <mardin-pop@protonmail.com>
2024-04-09 22:37:56 +0300

    docs: logic whether the user is online or not

--- dashboard/src/utils/is-online.ts
+++ dashboard/src/utils/is-online.ts
@@ -1,5 +1,11 @@
 import { UserType } from "@marzneshin/features/users";
 
+/**
+ * Check whether the user has been online or not
+ *
+ * @param user - User entity
+ * @returns Whether the user has be online or not
+ */
 export const isUserOnline = (user: UserType): boolean => {
     const dateObject = new Date(user.online_at);
     const currentTime = new Date();
