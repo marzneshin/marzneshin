a7a142736a07d244fd806b20c41b8e79a6884d16
Krr0ptioN <mardin-pop@protonmail.com>
2024-04-09 22:47:24 +0300

    fix: conver the current time to UTC to match the server timezone

--- dashboard/src/utils/is-online.ts
+++ dashboard/src/utils/is-online.ts
@@ -9,6 +9,7 @@ import { UserType } from "@marzneshin/features/users";
 export const isUserOnline = (user: UserType): boolean => {
     const dateObject = new Date(user.online_at);
     const currentTime = new Date();
-    const timeDifference = currentTime.getTime() - dateObject.getTime();
+    const utcCurrentTime = new Date(currentTime.toISOString());
+    const timeDifference = utcCurrentTime.getTime() - dateObject.getTime();
     return timeDifference < 30000;
 };
