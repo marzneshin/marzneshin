bb04973f6d97f70756c6417113e01322fe96c208
Krr0ptioN <mardin-pop@protonmail.com>
2024-04-09 22:37:14 +0300

    fix: increasing the time limit to known as online

--- dashboard/src/utils/is-online.ts
+++ dashboard/src/utils/is-online.ts
@@ -4,5 +4,5 @@ export const isUserOnline = (user: UserType): boolean => {
     const dateObject = new Date(user.online_at);
     const currentTime = new Date();
     const timeDifference = currentTime.getTime() - dateObject.getTime();
-    return timeDifference < 3000;
+    return timeDifference < 30000;
 };
