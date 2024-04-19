47fe596a0915a8ddc400da4d642077b2337d4d5b
Krr0ptioN <mardin-pop@protonmail.com>
2024-04-19 12:27:09 +0300

    fix: entity table scroll area

--- dashboard/src/components/entity-table/table.tsx
+++ dashboard/src/components/entity-table/table.tsx
@@ -2,10 +2,7 @@ import {
     ColumnDef,
     flexRender,
 } from "@tanstack/react-table"
-
 import {
-    ScrollArea,
-    ScrollBar,
     Table,
     TableBody,
     TableCell,
@@ -31,59 +28,56 @@ export function EntityDataTable<TData, TValue>({
     const { table, isLoading } = useEntityTableContext()
     return (
         <Table className="w-full">
-            <ScrollArea>
-                <TableHeader>
-                    {table.getHeaderGroups().map((headerGroup) => (
-                        <TableRow key={headerGroup.id}>
-                            {headerGroup.headers.map((header) => {
-                                return (
-                                    <TableHead key={header.id}>
-                                        {header.isPlaceholder
-                                            ? null
-                                            : flexRender(
-                                                header.column.columnDef.header,
-                                                header.getContext()
-                                            )}
-                                    </TableHead>
-                                )
-                            })}
-                        </TableRow>
-                    ))}
-                </TableHeader>
-                <TableBody>
-                    {isLoading ? (
-                        <TableRow className="w-full">
+            <TableHeader>
+                {table.getHeaderGroups().map((headerGroup) => (
+                    <TableRow key={headerGroup.id}>
+                        {headerGroup.headers.map((header) => {
+                            return (
+                                <TableHead key={header.id}>
+                                    {header.isPlaceholder
+                                        ? null
+                                        : flexRender(
+                                            header.column.columnDef.header,
+                                            header.getContext()
+                                        )}
+                                </TableHead>
+                            )
+                        })}
+                    </TableRow>
+                ))}
+            </TableHeader>
+            <TableBody>
+                {isLoading ? (
+                    <TableRow className="w-full">
+                        <TableCell colSpan={columns.length} className="h-24 text-center">
+                            <LoaderCircle className="w-5 h-5 animate-spin text-primary" />
+                        </TableCell>
+                    </TableRow>
+                ) : (
+                    table.getRowModel().rows?.length ? (
+                        table.getRowModel().rows.map((row) => (
+                            <TableRow
+                                key={row.id}
+                                data-state={row.getIsSelected() && "selected"}
+                                data-testid={"entity-table-row"}
+                                onClick={() => onRowClick && onRowClick(row.original)}
+                            >
+                                {row.getVisibleCells().map((cell) => (
+                                    <TableCell key={cell.id}>
+                                        {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                                    </TableCell>
+                                ))}
+                            </TableRow>
+                        ))
+                    ) : (
+                        <TableRow>
                             <TableCell colSpan={columns.length} className="h-24 text-center">
-                                <LoaderCircle className="w-5 h-5 animate-spin text-primary" />
+                                {t('table.no-result')}
                             </TableCell>
                         </TableRow>
-                    ) : (
-                        table.getRowModel().rows?.length ? (
-                            table.getRowModel().rows.map((row) => (
-                                <TableRow
-                                    key={row.id}
-                                    data-state={row.getIsSelected() && "selected"}
-                                    data-testid={"entity-table-row"}
-                                    onClick={() => onRowClick && onRowClick(row.original)}
-                                >
-                                    {row.getVisibleCells().map((cell) => (
-                                        <TableCell key={cell.id}>
-                                            {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                                        </TableCell>
-                                    ))}
-                                </TableRow>
-                            ))
-                        ) : (
-                            <TableRow>
-                                <TableCell colSpan={columns.length} className="h-24 text-center">
-                                    {t('table.no-result')}
-                                </TableCell>
-                            </TableRow>
-                        )
-                    )}
-                </TableBody>
-                <ScrollBar orientation="horizontal" />
-            </ScrollArea>
+                    )
+                )}
+            </TableBody>
         </Table>
     )
 }
