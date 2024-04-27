190d5c5352523f48fbafc8a8a5b150ffc94d9649
Krr0ptioN <mardin-pop@protonmail.com>
2024-04-27 13:23:31 +0300

    feat(users): server side sorted users fetching

--- dashboard/src/features/users/services/users.query.ts
+++ dashboard/src/features/users/services/users.query.ts
@@ -1,15 +1,32 @@
 import { useQuery } from "@tanstack/react-query";
 import { fetch } from "@marzneshin/utils";
 import { UserType } from "../types";
-import { EntityQueryKeyType, FetchEntityReturn, UseEntityQueryProps } from "@marzneshin/components";
+import {
+    FetchEntityReturn,
+    UseEntityQueryProps,
+    SortableEntitySortQueryProps,
+    QueryKeyType,
+    SortableQueryKey,
+    QueryKey
+} from "@marzneshin/components";
+
+export type SortUserBy = "username" | "used_traffic" | "data_limit" | "expire" | "created_at"
 
 interface UserResponse extends UserType {
     service_ids: number[]
     services: any[]
 }
 
-export async function fetchUsers({ queryKey }: EntityQueryKeyType): FetchEntityReturn<UserType> {
-    return fetch(`/users?page=${queryKey[1]}&size=${queryKey[2]}&username=${queryKey[3]}`).then((result) => {
+export async function fetchUsers({ queryKey }: QueryKeyType<SortableQueryKey | QueryKey>): FetchEntityReturn<UserType> {
+    return fetch(`/users`, {
+        query: {
+            page: queryKey[1],
+            size: queryKey[2],
+            username: queryKey[3],
+            descending: queryKey[5],
+            order_by: queryKey[4]
+        }
+    }).then((result) => {
         const users: UserType[] = result.items.map((fetchedUser: UserResponse) => {
             const user: UserType = fetchedUser;
             user.services = fetchedUser.service_ids
@@ -24,9 +41,9 @@ export async function fetchUsers({ queryKey }: EntityQueryKeyType): FetchEntityR
 
 export const UsersQueryFetchKey = "users";
 
-export const useUsersQuery = ({ page, size, search }: UseEntityQueryProps) => {
+export const useUsersQuery = ({ page, size, search = "", sortBy = "created_at", desc = false }: UseEntityQueryProps & SortableEntitySortQueryProps<SortUserBy>) => {
     return useQuery({
-        queryKey: [UsersQueryFetchKey, page, size, search ? search : ""],
+        queryKey: [UsersQueryFetchKey, page, size, search, sortBy, desc],
         queryFn: fetchUsers,
         initialData: { entity: [], pageCount: 0 }
     })
