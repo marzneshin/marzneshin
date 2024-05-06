c9f6cff6605999d0393faa793ad410d57e8741f3
Krr0ptioN <mardin-pop@protonmail.com>
2024-05-06 21:32:35 +0300

    fix: services get unregistered
    - linting

--- dashboard/src/features/users/dialogs/mutation/fields/services.tsx
+++ dashboard/src/features/users/dialogs/mutation/fields/services.tsx
@@ -1,11 +1,11 @@
 import {
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-  ScrollArea,
-  Toggle
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+    ScrollArea,
+    Toggle
 } from '@marzneshin/components';
 import { useServicesQuery } from '@marzneshin/features/services';
 import { ServiceCard } from '@marzneshin/features/users';
@@ -16,46 +16,46 @@ import { useTranslation } from 'react-i18next';
 interface ServicesFieldProps { }
 
 export const ServicesField: FC<ServicesFieldProps> = () => {
-  const form = useFormContext();
-  const { data } = useServicesQuery({ page: 1, size: 100 });
-  const { t } = useTranslation();
+    const form = useFormContext();
+    const { data } = useServicesQuery({ page: 1, size: 100 });
+    const { t } = useTranslation();
 
-  return (
-    <FormItem>
-      <FormLabel>{t('services')}</FormLabel>
-      <ScrollArea className="flex flex-col justify-start px-1 h-[22rem]">
-        {data.entity.map(service => (
-          <FormField
-            control={form.control}
-            name={`services`}
-            render={({ field }) => (
-              <FormItem>
-                <FormControl>
-                  <Toggle
-                    key={service.id}
-                    value={String(service.id)}
-                    pressed={field.value?.includes(service.id)}
-                    onPressedChange={(checked) => {
-                      return checked
-                        ? field.onChange([...field.value, service.id])
-                        : field.onChange(
-                          field.value?.filter(
-                            (value: number) => value !== service.id
-                          )
-                        )
-                    }}
-                    {...form.register(`services.${service.id}`, { shouldUnregister: true })}
-                    className="px-0 mb-1 w-full"
-                  >
-                    <ServiceCard service={service} />
-                  </Toggle>
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-        ))}
-      </ScrollArea>
-    </FormItem>
-  );
+    return (
+        <FormItem>
+            <FormLabel>{t('services')}</FormLabel>
+            <ScrollArea className="flex flex-col justify-start px-1 h-[22rem]">
+                {data.entity.map(service => (
+                    <FormField
+                        control={form.control}
+                        name={`services`}
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormControl>
+                                    <Toggle
+                                        key={service.id}
+                                        value={String(service.id)}
+                                        pressed={field.value?.includes(service.id)}
+                                        onPressedChange={(checked) => {
+                                            return checked
+                                                ? field.onChange([...field.value, service.id])
+                                                : field.onChange(
+                                                    field.value?.filter(
+                                                        (value: number) => value !== service.id
+                                                    )
+                                                )
+                                        }}
+                                        {...form.register(`services.${service.id}`)}
+                                        className="px-0 mb-1 w-full"
+                                    >
+                                        <ServiceCard service={service} />
+                                    </Toggle>
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                ))}
+            </ScrollArea>
+        </FormItem>
+    );
 };
