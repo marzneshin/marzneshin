e0e96e6a23ab78e531c4b0dfce454bb1d2dcbc99
Krr0ptioN <mardin-pop@protonmail.com>
2024-05-12 17:36:52 +0300

    chore(lint): remove unused imports

--- dashboard/src/routes/__root.tsx
+++ dashboard/src/routes/__root.tsx
@@ -3,7 +3,7 @@ import { ThemeProvider } from '@marzneshin/features/theme-switch'
 import { queryClient } from '@marzneshin/utils';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { createRootRoute, Outlet } from '@tanstack/react-router'
-import React, { Suspense } from 'react'
+import { Suspense } from 'react'
 
 export const Route = createRootRoute({
     component: () => (
