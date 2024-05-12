7728ec4f313a38b354ec8bd52c780f5f28227ec4
Mrdn <54506351+Krr0ptioN@users.noreply.github.com>
2024-05-12 19:00:20 +0300

    fix(nodes): enable scrolling for nodes settings dialog (#262)

--- dashboard/src/features/nodes/dialogs/settings.dialog.tsx
+++ dashboard/src/features/nodes/dialogs/settings.dialog.tsx
@@ -2,6 +2,7 @@ import {
     Button,
     Card,
     CardContent,
+    ScrollArea,
     Sheet,
     SheetContent,
     SheetHeader,
@@ -48,45 +49,47 @@ export const NodesSettingsDialog: FC<NodesSettingsDialogProps> = ({ onOpenChange
     if (entity) {
         return (
             <Sheet open={open} onOpenChange={onOpenChange}>
-                <SheetContent className="sm:min-w-full md:min-w-[600px]" >
-                    <SheetHeader>
-                        <SheetTitle>
-                            {t('page.nodes.settings.title')}
-                        </SheetTitle>
-                    </SheetHeader>
-                    <div className="my-4">
-                        <h1 className="font-medium font-header">{t('page.nodes.settings.detail')}</h1>
-                        <NodesDetailTable node={entity} />
-                    </div>
-                    <Tabs className="my-3 w-full h-full" defaultValue="logs">
-                        <TabsList className="w-full">
-                            <TabsTrigger className="w-full" value="logs">{t('logs')}</TabsTrigger>
-                            <TabsTrigger className="w-full" value="config">{t('config')}</TabsTrigger>
-                        </TabsList>
-                        <TabsContent value="logs" className="h-full">
-                            <LogContainer node={entity} />
-                        </TabsContent>
-                        <TabsContent value="config" className="h-full z-51">
-                            <Card>
-                                <CardContent className="p-2 h-1/2 flex flex-col gap-2">
-                                    <Editor
-                                        height="50vh"
-                                        className="rounded-sm border"
-                                        defaultLanguage="json"
-                                        theme={theme === "dark" ? "vs-dark" : "github"}
-                                        defaultValue={JSON.stringify(data, null, '\t')}
-                                        onChange={handleConfigChange}
-                                    />
-                                    <Button
-                                        className="w-full"
-                                        onClick={() => mutate.mutate({ node: entity, config })}
-                                    >
-                                        {t('save')}
-                                    </Button>
-                                </CardContent>
-                            </Card>
-                        </TabsContent>
-                    </Tabs>
+                <SheetContent className="sm:min-w-full md:min-w-[600px] " >
+                    <ScrollArea className="h-full">
+                        <SheetHeader>
+                            <SheetTitle>
+                                {t('page.nodes.settings.title')}
+                            </SheetTitle>
+                        </SheetHeader>
+                        <div className="my-4">
+                            <h1 className="font-medium font-header">{t('page.nodes.settings.detail')}</h1>
+                            <NodesDetailTable node={entity} />
+                        </div>
+                        <Tabs className="my-3 w-full h-full" defaultValue="logs">
+                            <TabsList className="w-full">
+                                <TabsTrigger className="w-full" value="logs">{t('logs')}</TabsTrigger>
+                                <TabsTrigger className="w-full" value="config">{t('config')}</TabsTrigger>
+                            </TabsList>
+                            <TabsContent value="logs" className="h-full">
+                                <LogContainer node={entity} />
+                            </TabsContent>
+                            <TabsContent value="config" className="h-full z-51">
+                                <Card>
+                                    <CardContent className="p-2 h-1/2 flex flex-col gap-2">
+                                        <Editor
+                                            height="50vh"
+                                            className="rounded-sm border"
+                                            defaultLanguage="json"
+                                            theme={theme === "dark" ? "vs-dark" : "github"}
+                                            defaultValue={JSON.stringify(data, null, '\t')}
+                                            onChange={handleConfigChange}
+                                        />
+                                        <Button
+                                            className="w-full"
+                                            onClick={() => mutate.mutate({ node: entity, config })}
+                                        >
+                                            {t('save')}
+                                        </Button>
+                                    </CardContent>
+                                </Card>
+                            </TabsContent>
+                        </Tabs>
+                    </ScrollArea>
                 </SheetContent>
             </Sheet>
         )
