4756c79e494c14f5fa1f91b4f5b534ab89f09e2e
dawsh <dawsh@duck.com>
2024-05-19 17:21:38 +0330

    fix: rename misplaced names and users response

--- app/routes/system.py
+++ app/routes/system.py
@@ -12,12 +12,12 @@ from app.models.user import UserStatus
 router = APIRouter(tags=["System"], prefix="/system")
 
 
-@router.get("/stats/nodes", response_model=NodesStats)
+@router.get("/stats/admins", response_model=AdminsStats)
 def get_admins_stats(db: DBDep, admin: SudoAdminDep):
     return AdminsStats(total=db.query(DBAdmin).count())
 
 
-@router.get("/stats/admins", response_model=AdminsStats)
+@router.get("/stats/nodes", response_model=NodesStats)
 def get_nodes_stats(db: DBDep, admin: SudoAdminDep):
     return NodesStats(
         total=db.query(Node).count(),
@@ -29,45 +29,33 @@ def get_nodes_stats(db: DBDep, admin: SudoAdminDep):
 @router.get("/stats/users", response_model=UsersStats)
 def get_users_stats(db: DBDep, admin: AdminDep):
     return UsersStats(
-        total=(crud.get_users_count(db, admin=admin if not admin.is_sudo else None),),
-        active=(
-            crud.get_users_count(
-                db, admin=admin if not admin.is_sudo else None, status=UserStatus.active
-            ),
+        total=crud.get_users_count(db, admin=admin if not admin.is_sudo else None),
+        active=crud.get_users_count(
+            db, admin=admin if not admin.is_sudo else None, status=UserStatus.active
         ),
-        on_hold=(
-            crud.get_users_count(
-                db,
-                admin=admin if not admin.is_sudo else None,
-                status=UserStatus.on_hold,
-            ),
+        on_hold=crud.get_users_count(
+            db,
+            admin=admin if not admin.is_sudo else None,
+            status=UserStatus.on_hold,
         ),
-        expired=(
-            crud.get_users_count(
-                db,
-                admin=admin if not admin.is_sudo else None,
-                status=UserStatus.expired,
-            ),
+        expired=crud.get_users_count(
+            db,
+            admin=admin if not admin.is_sudo else None,
+            status=UserStatus.expired,
         ),
-        limited=(
-            crud.get_users_count(
-                db,
-                admin=admin if not admin.is_sudo else None,
-                status=UserStatus.limited,
-            ),
+        limited=crud.get_users_count(
+            db,
+            admin=admin if not admin.is_sudo else None,
+            status=UserStatus.limited,
         ),
-        online=(
-            crud.get_users_count(
-                db, admin=admin if not admin.is_sudo else None, online=True
-            ),
+        online=crud.get_users_count(
+            db, admin=admin if not admin.is_sudo else None, online=True
         ),
-        recent_subscription_updates=(
-            list(
-                i[0]
-                for i in db.query(User.username)
-                .filter(User.sub_updated_at != None)
-                .order_by(User.sub_updated_at)
-                .limit(5)
-            ),
+        recent_subscription_updates=list(
+            i[0]
+            for i in db.query(User.username)
+            .filter(User.sub_updated_at != None)
+            .order_by(User.sub_updated_at)
+            .limit(5)
         ),
     )
