33d05439c1348c02f706f5986045d5869cf5e4bd
Krr0ptioN <mardin-pop@protonmail.com>
2024-05-21 01:58:58 +0300

    fix: no border for page card

--- dashboard/src/routes/_dashboard/nodes.tsx
+++ dashboard/src/routes/_dashboard/nodes.tsx
@@ -1,13 +1,13 @@
 import {
-  Alert,
-  AlertDescription,
-  AlertTitle,
-  Card,
-  CardContent,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-  Page
+    Alert,
+    AlertDescription,
+    AlertTitle,
+    Card,
+    CardContent,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+    Page
 } from '@marzneshin/components';
 import { NodesTable } from '@marzneshin/features/nodes';
 import { Link, createFileRoute } from '@tanstack/react-router'
@@ -16,33 +16,33 @@ import { FC } from 'react';
 import { useTranslation } from 'react-i18next';
 
 export const NodesPage: FC = () => {
-  const { t } = useTranslation();
-  return (
-    <Page>
-      <Card className="sm:w-screen md:w-full">
-        <CardHeader>
-          <CardTitle>
-            {t('nodes')}
-          </CardTitle>
-        </CardHeader>
-        <CardContent>
-          <NodesTable />
-        </CardContent>
-        <CardFooter>
-          <Alert>
-            <Info className="mr-2" />
-            <AlertTitle className="font-semibold text-primary">{t('page.nodes.certificate-alert.title')}</AlertTitle>
-            <AlertDescription>
-              {t('page.nodes.certificate-alert.desc')}
-              <Link className="m-1 font-semibold text-secondary-foreground" to="/settings">{t('page.nodes.certificate-alert.click')}</Link>
-            </AlertDescription>
-          </Alert>
-        </CardFooter>
-      </Card >
-    </Page>
-  )
+    const { t } = useTranslation();
+    return (
+        <Page>
+            <Card className="border-0 sm:w-screen md:w-full">
+                <CardHeader>
+                    <CardTitle>
+                        {t('nodes')}
+                    </CardTitle>
+                </CardHeader>
+                <CardContent>
+                    <NodesTable />
+                </CardContent>
+                <CardFooter>
+                    <Alert>
+                        <Info className="mr-2" />
+                        <AlertTitle className="font-semibold text-primary">{t('page.nodes.certificate-alert.title')}</AlertTitle>
+                        <AlertDescription>
+                            {t('page.nodes.certificate-alert.desc')}
+                            <Link className="m-1 font-semibold text-secondary-foreground" to="/settings">{t('page.nodes.certificate-alert.click')}</Link>
+                        </AlertDescription>
+                    </Alert>
+                </CardFooter>
+            </Card >
+        </Page>
+    )
 };
 
 export const Route = createFileRoute('/_dashboard/nodes')({
-  component: () => <NodesPage />
+    component: () => <NodesPage />
 })
