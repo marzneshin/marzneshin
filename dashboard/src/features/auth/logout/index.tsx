1c0d1b765524e7d01bf9e56f62061c9115c4e4f6
Dawsh <dawsh@duck.com>
2024-05-23 22:34:10 +0330

    improve: translations (#294)
    * style: fix whitespaces
    
    * chore: translate to persian
    
    * chore: improve english translations
    
    * add: cancel theme and system translation
    
    * fix: english translation
    
    * style(header): same color as others
    
    * fix: successfully :)
    
    * fix: successfully :)
    
    * chore: some fixes
    
    * fix: translation for notes field placeholder
    
    * chore: biome config
    
    ---------
    
    Co-authored-by: Krr0ptioN <mardin-pop@protonmail.com>

--- dashboard/src/features/auth/logout/index.tsx
+++ dashboard/src/features/auth/logout/index.tsx
@@ -1,22 +1,23 @@
-
-import { Button } from '@marzneshin/components';
-import { useNavigate } from '@tanstack/react-router';
-import { LogOut } from 'lucide-react';
-import { FC, useCallback } from 'react'
+import { Button } from "@marzneshin/components";
+import { useNavigate } from "@tanstack/react-router";
+import { LogOut } from "lucide-react";
+import { FC, useCallback } from "react";
 
 interface LogoutProps { }
 
 export const Logout: FC<LogoutProps> = () => {
-    const navigate = useNavigate({ from: '/' })
-    const handleLogout = useCallback(
-        () => {
-            navigate({ to: '/login' })
-        },
-        [navigate],
-    )
+    const navigate = useNavigate({ from: "/" });
+    const handleLogout = useCallback(() => {
+        navigate({ to: "/login" });
+    }, [navigate]);
     return (
-        <Button className="h-full" size="icon" onClick={handleLogout}>
+        <Button
+            className="h-full text-secondary bg-gray-800 dark:hover:bg-secondary-foreground dark:hover:text-secondary dark:text-secondary-foreground"
+            size="icon"
+            variant="outline"
+            onClick={handleLogout}
+        >
             <LogOut className="h-[1rem] w-[1rem]" />
         </Button>
     );
-}
+};
--- dashboard/src/features/language-switch/index.tsx
+++ dashboard/src/features/language-switch/index.tsx
@@ -1,43 +1,53 @@
-import { Button, DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@marzneshin/components";
+import {
+    Button,
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuTrigger,
+} from "@marzneshin/components";
 import { Languages } from "lucide-react";
 import { FC } from "react";
 import { useTranslation } from "react-i18next";
 
 export const LanguageSwitch: FC = () => {
-  const { i18n } = useTranslation();
+    const { i18n } = useTranslation();
 
-  const changeLanguage = (lang: string) => {
-    i18n.changeLanguage(lang);
-  };
+    const changeLanguage = (lang: string) => {
+        i18n.changeLanguage(lang);
+    };
 
-  return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="outline" className="bg-secondary-foreground text-primary-foreground " size="icon">
-          <Languages className="h-[1rem] w-[1rem]" />
-          <span className="sr-only">Toggle Language</span>
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
-        <DropdownMenuItem onClick={() => changeLanguage("en")}>
-          English
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => changeLanguage("kmr")}>
-          Kurdî Kurmancî
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => changeLanguage("kur")}>
-          Kurdî Nawendî (Latin)
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => changeLanguage("ckb")}>
-          کوردی ناوەندی
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => changeLanguage("fa")}>
-          فارسی
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => changeLanguage("fa")}>
-          Русский
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
-  );
+    return (
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+                <Button
+                    variant="outline"
+                    className="bg-gray-800 text-secondary dark:hover:bg-secondary-foreground dark:hover:text-secondary dark:text-secondary-foreground"
+                    size="icon"
+                >
+                    <Languages className="h-[1rem] w-[1rem]" />
+                    <span className="sr-only">Toggle Language</span>
+                </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+                <DropdownMenuItem onClick={() => changeLanguage("en")}>
+                    English
+                </DropdownMenuItem>
+                <DropdownMenuItem onClick={() => changeLanguage("kmr")}>
+                    Kurdî Kurmancî
+                </DropdownMenuItem>
+                <DropdownMenuItem onClick={() => changeLanguage("kur")}>
+                    Kurdî Soranî
+                </DropdownMenuItem>
+                <DropdownMenuItem onClick={() => changeLanguage("ckb")}>
+                    کوردی سورانی
+                </DropdownMenuItem>
+                <DropdownMenuItem onClick={() => changeLanguage("fa")}>
+                    فارسی
+                </DropdownMenuItem>
+                <DropdownMenuItem onClick={() => changeLanguage("ru")}>
+                    Русский
+                </DropdownMenuItem>
+            </DropdownMenuContent>
+        </DropdownMenu>
+    );
 };
