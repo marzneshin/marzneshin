db59d853fc92169110415494047ca23a6ca6a97e
Krr0ptioN <mardin-pop@protonmail.com>
2024-05-23 17:32:18 +0300

    fix: correct value for alpn h2 http/1.1

--- dashboard/src/features/hosts/dialogs/mutation/fields/alpn.tsx
+++ dashboard/src/features/hosts/dialogs/mutation/fields/alpn.tsx
@@ -1,44 +1,44 @@
 import {
-	FormControl,
-	FormField,
-	FormItem,
-	FormLabel,
-	FormMessage,
-	Select,
-	SelectContent,
-	SelectItem,
-	SelectTrigger,
-	SelectValue,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
 } from "@marzneshin/components";
 import { useFormContext } from "react-hook-form";
 import { useTranslation } from "react-i18next";
 
 export const AlpnField = () => {
-	const { t } = useTranslation();
-	const form = useFormContext();
-	return (
-		<FormField
-			control={form.control}
-			name="alpn"
-			render={({ field }) => (
-				<FormItem className="w-full">
-					<FormLabel>{t("alpn")}</FormLabel>
-					<Select onValueChange={field.onChange} defaultValue={field.value}>
-						<FormControl>
-							<SelectTrigger>
-								<SelectValue placeholder="Select ALPN" />
-							</SelectTrigger>
-						</FormControl>
-						<SelectContent>
-							<SelectItem value="none">None</SelectItem>
-							<SelectItem value="h2">h2</SelectItem>
-							<SelectItem value="http/1.1">http 1.1</SelectItem>
-							<SelectItem value="h2,http/1.1">h2 http 1.1</SelectItem>
-						</SelectContent>
-					</Select>
-					<FormMessage />
-				</FormItem>
-			)}
-		/>
-	);
+    const { t } = useTranslation();
+    const form = useFormContext();
+    return (
+        <FormField
+            control={form.control}
+            name="alpn"
+            render={({ field }) => (
+                <FormItem className="w-full">
+                    <FormLabel>{t("alpn")}</FormLabel>
+                    <Select onValueChange={field.onChange} defaultValue={field.value}>
+                        <FormControl>
+                            <SelectTrigger>
+                                <SelectValue placeholder="Select ALPN" />
+                            </SelectTrigger>
+                        </FormControl>
+                        <SelectContent>
+                            <SelectItem value="none">None</SelectItem>
+                            <SelectItem value="h2">h2</SelectItem>
+                            <SelectItem value="http/1.1">http 1.1</SelectItem>
+                            <SelectItem value="h2,http/1.1">h2,http/1.1</SelectItem>
+                        </SelectContent>
+                    </Select>
+                    <FormMessage />
+                </FormItem>
+            )}
+        />
+    );
 };
