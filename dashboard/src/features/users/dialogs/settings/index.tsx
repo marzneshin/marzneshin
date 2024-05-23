6feb8fb67c1cbbe9aeca4c9969ec3c07b44088ad
Krr0ptioN <mardin-pop@protonmail.com>
2024-05-24 02:19:18 +0300

    fix: responsiveness in exteremly small and narrow screen

--- dashboard/src/features/users/dialogs/settings/index.tsx
+++ dashboard/src/features/users/dialogs/settings/index.tsx
@@ -8,39 +8,53 @@ import {
     TabsContent,
     TabsList,
     TabsTrigger,
-} from "@marzneshin/components"
-import { FC } from "react"
-import { useTranslation } from "react-i18next"
-import { UserServicesTable, UserType } from "@marzneshin/features/users"
-import { UserInfoTable, QRCodeSection } from "./user-info"
+} from "@marzneshin/components";
+import { UserServicesTable } from "@marzneshin/features/users";
+import type { UserType } from "@marzneshin/features/users";
+import type { FC } from "react";
+import { useTranslation } from "react-i18next";
+import { QRCodeSection, UserInfoTable } from "./user-info";
+import { SubscriptionActions } from "./user-info/subscription-actions";
 
 interface UsersSettingsDialogProps {
-    onOpenChange: (state: boolean) => void
-    open: boolean
-    entity: UserType | null
+    onOpenChange: (state: boolean) => void;
+    open: boolean;
+    entity: UserType | null;
 }
 
-export const UsersSettingsDialog: FC<UsersSettingsDialogProps> = ({ onOpenChange, open, entity }) => {
+export const UsersSettingsDialog: FC<UsersSettingsDialogProps> = ({
+    onOpenChange,
+    open,
+    entity,
+}) => {
     const { t } = useTranslation();
 
     if (entity) {
         return (
             <Sheet open={open} onOpenChange={onOpenChange}>
-                <SheetContent className="sm:min-w-full md:min-w-[700px]" >
-                    <SheetHeader>
-                        <SheetTitle>
-                            {t('settings')}
-                        </SheetTitle>
-                    </SheetHeader>
+                <SheetContent className="sm:min-w-full md:min-w-[700px]">
                     <ScrollArea className="flex flex-col gap-4 m-3 h-full">
+                        <SheetHeader className="my-1">
+                            <SheetTitle>{t("settings")}</SheetTitle>
+                        </SheetHeader>
                         <Tabs defaultValue="info" className="w-full h-full">
                             <TabsList className="w-full bg-accent">
-                                <TabsTrigger className="w-full" value="info"> {t('user_info')} </TabsTrigger>
-                                <TabsTrigger className="w-full" value="services"> {t('services')} </TabsTrigger>
+                                <TabsTrigger className="w-full" value="info">
+                                    {" "}
+                                    {t("user_info")}{" "}
+                                </TabsTrigger>
+                                <TabsTrigger className="w-full" value="services">
+                                    {" "}
+                                    {t("services")}{" "}
+                                </TabsTrigger>
                             </TabsList>
-                            <TabsContent value="info" className="flex flex-col gap-4">
+                            <TabsContent
+                                value="info"
+                                className="flex flex-col gap-2 w-full h-full"
+                            >
                                 <UserInfoTable entity={entity} />
                                 <QRCodeSection entity={entity} />
+                                <SubscriptionActions entity={entity} />
                             </TabsContent>
                             <TabsContent value="services">
                                 <UserServicesTable user={entity} />
@@ -49,6 +63,6 @@ export const UsersSettingsDialog: FC<UsersSettingsDialogProps> = ({ onOpenChange
                     </ScrollArea>
                 </SheetContent>
             </Sheet>
-        )
+        );
     }
-}
+};
