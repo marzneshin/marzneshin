cf826c26e1c230f2c45dba9ee799972ddfe71837
Krr0ptioN <mardin-pop@protonmail.com>
2024-05-23 21:19:36 +0300

    fix: translation for notes field placeholder

--- dashboard/src/features/users/dialogs/mutation/fields/note.tsx
+++ dashboard/src/features/users/dialogs/mutation/fields/note.tsx
@@ -4,28 +4,27 @@ import {
     FormItem,
     Textarea,
     FormLabel,
-    FormMessage
-} from '@marzneshin/components';
-import { FC } from 'react'
-import { useFormContext } from 'react-hook-form';
-import { useTranslation } from 'react-i18next';
-
+    FormMessage,
+} from "@marzneshin/components";
+import { FC } from "react";
+import { useFormContext } from "react-hook-form";
+import { useTranslation } from "react-i18next";
 
 interface NoteFieldProps { }
 
 export const NoteField: FC<NoteFieldProps> = () => {
-    const { t } = useTranslation()
-    const form = useFormContext()
+    const { t } = useTranslation();
+    const form = useFormContext();
     return (
         <FormField
             control={form.control}
             name="note"
             render={({ field }) => (
                 <FormItem>
-                    <FormLabel>{t('note')}</FormLabel>
+                    <FormLabel>{t("note")}</FormLabel>
                     <FormControl>
                         <Textarea
-                            placeholder="Additional information about data"
+                            placeholder={t("page.users.note-placeholder")}
                             className="resize-none"
                             {...field}
                         />
@@ -35,4 +34,4 @@ export const NoteField: FC<NoteFieldProps> = () => {
             )}
         />
     );
-}
+};
