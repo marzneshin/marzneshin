1d782e1b65b5d649bb67672c731d1289afe018ad
Mrdn <54506351+Krr0ptioN@users.noreply.github.com>
2024-05-24 13:25:09 +0300

    fix(nodes-log): node settings messes up when xray logs are too long (#297)
    * improve: separator between logs
    
    * chore: lint
    
    * style(nodes-log): correcting oversization and log separation + hover effect
    
    * style(nodes-log): hover-effect for light theme

--- dashboard/src/features/nodes/dialogs/settings/index.tsx
+++ dashboard/src/features/nodes/dialogs/settings/index.tsx
@@ -10,42 +10,50 @@ import {
     TabsContent,
     TabsList,
     TabsTrigger,
-} from "@marzneshin/components"
-import { FC } from "react"
-import { useTranslation } from "react-i18next"
-import { NodeType } from "../.."
+} from "@marzneshin/components";
+import type { FC } from "react";
+import { useTranslation } from "react-i18next";
+import type { NodeType } from "../..";
 import { LogContainer } from "../../log";
 import { NodesDetailTable } from "../../tables/detail-table";
 import { NodeConfigEditor } from "./node-config-editor";
 
 interface NodesSettingsDialogProps {
-    onOpenChange: (state: boolean) => void
-    open: boolean
-    entity: NodeType
+    onOpenChange: (state: boolean) => void;
+    open: boolean;
+    entity: NodeType;
 }
 
-export const NodesSettingsDialog: FC<NodesSettingsDialogProps> = ({ onOpenChange, open, entity }) => {
+export const NodesSettingsDialog: FC<NodesSettingsDialogProps> = ({
+    onOpenChange,
+    open,
+    entity,
+}) => {
     const { t } = useTranslation();
 
     return (
         <Sheet open={open} onOpenChange={onOpenChange}>
-            <SheetContent className="sm:min-w-full md:min-w-[600px] " >
-                <ScrollArea className="h-full">
+            <SheetContent className="sm:min-w-full md:min-w-[600px] ">
+                <ScrollArea className="h-full w-full">
                     <SheetHeader>
-                        <SheetTitle>
-                            {t('page.nodes.settings.title')}
-                        </SheetTitle>
+                        <SheetTitle>{t("page.nodes.settings.title")}</SheetTitle>
                     </SheetHeader>
                     <div className="my-4">
-                        <h1 className="font-medium font-header">{t('page.nodes.settings.detail')}</h1>
+                        <h1 className="font-medium font-header">
+                            {t("page.nodes.settings.detail")}
+                        </h1>
                         <NodesDetailTable node={entity} />
                     </div>
-                    <Tabs className="my-3 w-full h-full" defaultValue="logs">
+                    <Tabs className="my-3 max-w-full h-full" defaultValue="logs">
                         <TabsList className="w-full">
-                            <TabsTrigger className="w-full" value="logs">{t('logs')}</TabsTrigger>
-                            <TabsTrigger className="w-full" value="config">{t('config')}</TabsTrigger>
+                            <TabsTrigger className="w-full" value="logs">
+                                {t("logs")}
+                            </TabsTrigger>
+                            <TabsTrigger className="w-full" value="config">
+                                {t("config")}
+                            </TabsTrigger>
                         </TabsList>
-                        <TabsContent value="logs" className="h-full">
+                        <TabsContent value="logs" className="h-full w-full">
                             <LogContainer node={entity} />
                         </TabsContent>
                         <TabsContent value="config" className="h-full z-51">
@@ -59,5 +67,5 @@ export const NodesSettingsDialog: FC<NodesSettingsDialogProps> = ({ onOpenChange
                 </ScrollArea>
             </SheetContent>
         </Sheet>
-    )
-}
+    );
+};
