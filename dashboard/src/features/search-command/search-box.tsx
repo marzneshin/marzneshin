8be3172ba71a4e363ac0782d6120c7a01f894ce1
Krr0ptioN <mardin-pop@protonmail.com>
2024-05-26 00:24:05 +0300

    fix: html attributes for onclick

--- dashboard/src/features/search-command/search-box.tsx
+++ dashboard/src/features/search-command/search-box.tsx
@@ -1,19 +1,21 @@
-import type { FC } from "react";
+import type { FC, HTMLAttributes } from "react";
 import { SearchIcon } from "lucide-react";
 
-export const SearchBox: FC = ({ ...props }) => {
-    return (
-        <div
-            className="bg-gray-800 p-2 md:w-60 flex flex-row items-center rounded-md justify-between"
-            {...props}
-        >
-            <div className="flex flex-row items-center gap-1">
-                <SearchIcon className="size-4 text-muted dark:text-muted-foreground" />
-                <p className="text-sm text-muted dark:text-muted-foreground ">Search</p>
-            </div>
-            <kbd className="pointer-events-none md:inline-flex h-5 select-none items-center gap-1 rounded bg-muted bg-gray-600 px-1.5 font-mono text-[10px] font-medium text-muted dark:text-muted-foreground text-opacity-100 hidden">
-                <span className="text-xs">CTRL</span>K
-            </kbd>
-        </div>
-    );
+export const SearchBox: FC<HTMLAttributes<HTMLDivAttribute>> = ({
+  ...props
+}) => {
+  return (
+    <div
+      className="bg-gray-800 p-2 md:w-60 flex flex-row items-center rounded-md justify-between"
+      {...props}
+    >
+      <div className="flex flex-row items-center gap-1">
+        <SearchIcon className="size-4 text-muted dark:text-muted-foreground" />
+        <p className="text-sm text-muted dark:text-muted-foreground ">Search</p>
+      </div>
+      <kbd className="pointer-events-none md:inline-flex h-5 select-none items-center gap-1 rounded bg-muted bg-gray-600 px-1.5 font-mono text-[10px] font-medium text-muted dark:text-muted-foreground text-opacity-100 hidden">
+        <span className="text-xs">CTRL</span>K
+      </kbd>
+    </div>
+  );
 };
