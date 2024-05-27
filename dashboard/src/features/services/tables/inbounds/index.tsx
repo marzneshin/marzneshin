3072f607968ae1ee1a2c17d9c77aa103ee03c1a1
Mrdn <54506351+Krr0ptioN@users.noreply.github.com>
2024-05-27 13:00:33 +0300

    refact(dashboard): resolving code smells and unreliable code (#321)
    * refact: Remove this type without members
    * refact(services-mutation): dividing the mutation into more components
    * refact(services-mutation): not nest functions more than 4 levels deep
    * refact: prefer for loop over foreach
    * refact: lint, import types, separat card component for service
    * refact(users-service-table): prefer for-loops, type imports
    * fix: checkbox card type props
    * refact: simplifying forloop
    * chore: add issue number to the todo
    * refact: use memo

--- dashboard/src/features/services/tables/inbounds/index.tsx
+++ dashboard/src/features/services/tables/inbounds/index.tsx
@@ -1,58 +1,61 @@
-
-import { FC, useCallback, useEffect, useState } from "react";
-import {
-    useInboundsQuery,
-} from '@marzneshin/features/inbounds';
+import { type FC, useCallback, useEffect, useState } from "react";
+import { useInboundsQuery } from "@marzneshin/features/inbounds";
 import { Button, DataTable } from "@marzneshin/components";
 import { columns } from "./columns";
-import { ServiceType, useServicesUpdateMutation } from "../..";
+import { type ServiceType, useServicesUpdateMutation } from "../..";
 import { useTranslation } from "react-i18next";
-import { RowSelectionState } from "@tanstack/react-table";
+import type { RowSelectionState } from "@tanstack/react-table";
 
 interface ServiceInboundsTableProps {
-    service: ServiceType
+    service: ServiceType;
 }
 
-export const ServiceInboundsTable: FC<ServiceInboundsTableProps> = ({ service }) => {
+export const ServiceInboundsTable: FC<ServiceInboundsTableProps> = ({
+    service,
+}) => {
     const { mutate: updateService } = useServicesUpdateMutation();
     const { data } = useInboundsQuery();
-    const [selectedInbound, setSelectedInbound] = useState<{ [key: number]: boolean }>({})
-    const { t } = useTranslation()
+    const [selectedInbound, setSelectedInbound] = useState<{
+        [key: number]: boolean;
+    }>({});
+    const { t } = useTranslation();
 
     useEffect(() => {
         setSelectedInbound((prevSelected) => {
             const updatedSelected: RowSelectionState = { ...prevSelected };
-            service.inbounds.forEach((inboundId) => {
-                data.forEach((fetchedInbound, i) => {
+            for (const inboundId of service.inbounds) {
+                for (const [i, fetchedInbound] of data.entries()) {
                     if (fetchedInbound.id === inboundId) {
                         updatedSelected[i] = true;
                     }
-                });
-            });
+                }
+            }
             return updatedSelected;
         });
-    }, [data, service.inbounds])
+    }, [data, service.inbounds]);
 
     const handleApply = useCallback(() => {
         const selectedInboundIds = Object.keys(selectedInbound)
-            .filter(key => selectedInbound[parseInt(key)])
-            .map(key => data[parseInt(key)].id);
+            .filter((key) => selectedInbound[Number.parseInt(key)])
+            .map((key) => data[Number.parseInt(key)].id);
         updateService({ ...service, inbounds: selectedInboundIds });
     }, [data, selectedInbound, service, updateService]);
 
-    const disabled = (Object.keys(selectedInbound).length < 1) ? true : false
+    const disabled = Object.keys(selectedInbound).length < 1;
 
     return (
         <div className="flex flex-col gap-4">
             <DataTable
                 columns={columns}
                 data={data}
-                filteredColumn='tag'
+                filteredColumn="tag"
                 selectedRow={selectedInbound}
                 setSelectedRow={setSelectedInbound}
             />
 
-            <Button onClick={handleApply} disabled={disabled}>{t('apply')}</Button>
+            <Button onClick={handleApply} disabled={disabled}>
+                {t("apply")}
+            </Button>
         </div>
-    )
-}
+    );
+};
