d3484a5f973de0f7ab15e4bc848c44f017f30ec4
Krr0ptioN <mardin-pop@protonmail.com>
2024-05-27 13:27:54 +0300

    Revert "fix: use one for index when initally is 0"
    This reverts commit 878e3f95c6924aa09cdd4cc76ac764f27e3301e1.

--- dashboard/src/components/entity-table/hooks/use-entity-table.ts
+++ dashboard/src/components/entity-table/hooks/use-entity-table.ts
@@ -1,64 +1,64 @@
 import {
-  getCoreRowModel,
-  getPaginationRowModel,
-  getSortedRowModel,
-  useReactTable,
+    getCoreRowModel,
+    getPaginationRowModel,
+    getSortedRowModel,
+    useReactTable,
 } from "@tanstack/react-table";
 import type {
-  ColumnDef,
-  OnChangeFn,
-  PaginationState,
+    ColumnDef,
+    OnChangeFn,
+    PaginationState,
 } from "@tanstack/react-table";
 import type {
-  UseRowSelectionReturn,
-  UseSortingReturn,
-  UseVisibilityReturn,
+    UseRowSelectionReturn,
+    UseSortingReturn,
+    UseVisibilityReturn,
 } from ".";
 
 interface UseEntityTableProps<TData, TValue> {
-  columns: ColumnDef<TData, TValue>[];
-  data: {
-    entity: TData[];
-    pageCount: number;
-  };
-  sorting: UseSortingReturn;
-  visibility: UseVisibilityReturn;
-  rowSelection?: UseRowSelectionReturn;
-  manualSorting?: boolean;
-  pageIndex: number;
-  pageSize: number;
-  onPaginationChange: OnChangeFn<PaginationState>;
+    columns: ColumnDef<TData, TValue>[];
+    data: {
+        entity: TData[];
+        pageCount: number;
+    };
+    sorting: UseSortingReturn;
+    visibility: UseVisibilityReturn;
+    rowSelection?: UseRowSelectionReturn;
+    manualSorting?: boolean;
+    pageIndex: number;
+    pageSize: number;
+    onPaginationChange: OnChangeFn<PaginationState>;
 }
 
 export const useEntityTable = <TData, TValue>({
-  columns,
-  data,
-  sorting,
-  manualSorting = false,
-  visibility,
-  rowSelection,
-  pageIndex,
-  pageSize,
-  onPaginationChange,
-}: UseEntityTableProps<TData, TValue>) =>
-  useReactTable({
-    data: data.entity,
     columns,
-    manualPagination: true,
-    manualSorting,
-    pageCount: data.pageCount + 1,
-    autoResetPageIndex: false,
+    data,
+    sorting,
+    manualSorting = false,
+    visibility,
+    rowSelection,
+    pageIndex,
+    pageSize,
     onPaginationChange,
-    onSortingChange: sorting.setSorting,
-    getCoreRowModel: getCoreRowModel(),
-    getPaginationRowModel: getPaginationRowModel(),
-    getSortedRowModel: getSortedRowModel(),
-    onColumnVisibilityChange: visibility.setColumnVisibility,
-    onRowSelectionChange: rowSelection?.setSelectedRow,
-    state: {
-      sorting: sorting.sorting,
-      columnVisibility: visibility.columnVisibility,
-      pagination: { pageIndex: pageIndex === 0 ? 1 : pageIndex - 1, pageSize },
-      rowSelection: rowSelection ? rowSelection.selectedRow : {},
-    },
-  });
+}: UseEntityTableProps<TData, TValue>) =>
+    useReactTable({
+        data: data.entity,
+        columns,
+        manualPagination: true,
+        manualSorting,
+        pageCount: data.pageCount + 1,
+        autoResetPageIndex: false,
+        onPaginationChange,
+        onSortingChange: sorting.setSorting,
+        getCoreRowModel: getCoreRowModel(),
+        getPaginationRowModel: getPaginationRowModel(),
+        getSortedRowModel: getSortedRowModel(),
+        onColumnVisibilityChange: visibility.setColumnVisibility,
+        onRowSelectionChange: rowSelection?.setSelectedRow,
+        state: {
+            sorting: sorting.sorting,
+            columnVisibility: visibility.columnVisibility,
+            pagination: { pageIndex: pageIndex - 1, pageSize },
+            rowSelection: rowSelection ? rowSelection.selectedRow : {},
+        },
+    });
