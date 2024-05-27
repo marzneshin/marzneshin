4d9363df58bfa18b6a04f28e5e8740ad786582b1
Krr0ptioN <mardin-pop@protonmail.com>
2024-05-27 13:27:33 +0300

    Revert "exp: page index without computation"
    This reverts commit 711ed367f2264c0c2477f2528afd79687625608f.

--- dashboard/src/components/entity-table/hooks/use-entity-table.ts
+++ dashboard/src/components/entity-table/hooks/use-entity-table.ts
@@ -37,7 +37,7 @@ export const useEntityTable = <TData, TValue>({
   manualSorting = false,
   visibility,
   rowSelection,
-  pageIndex = 1,
+  pageIndex,
   pageSize,
   onPaginationChange,
 }: UseEntityTableProps<TData, TValue>) =>
@@ -58,7 +58,7 @@ export const useEntityTable = <TData, TValue>({
     state: {
       sorting: sorting.sorting,
       columnVisibility: visibility.columnVisibility,
-      pagination: { pageIndex, pageSize },
+      pagination: { pageIndex: pageIndex === 0 ? 1 : pageIndex - 1, pageSize },
       rowSelection: rowSelection ? rowSelection.selectedRow : {},
     },
   });
