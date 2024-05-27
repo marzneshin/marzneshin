9e3db7fcbe453a7788b52b65b15c10cbf8db9bab
Krr0ptioN <mardin-pop@protonmail.com>
2024-05-27 11:32:06 +0300

    refact: lint, import types, separat card component for service

--- dashboard/src/features/users/dialogs/mutation/fields/services.tsx
+++ dashboard/src/features/users/dialogs/mutation/fields/services.tsx
@@ -1,71 +1,86 @@
 import {
-  Checkbox,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-  ScrollArea,
+    Checkbox,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+    ScrollArea,
 } from "@marzneshin/components";
 import { useServicesQuery } from "@marzneshin/features/services";
 import { ServiceCard } from "@marzneshin/features/users";
 import { cn } from "@marzneshin/utils";
 import type { FC } from "react";
-import { useFormContext } from "react-hook-form";
+import {
+    useFormContext,
+    type FieldValues,
+    type ControllerRenderProps,
+} from "react-hook-form";
 import { useTranslation } from "react-i18next";
 
-export const ServicesField: FC = () => {
-  const form = useFormContext();
-  const { data } = useServicesQuery({ page: 1, size: 100 });
-  const { t } = useTranslation();
+const ServiceCheckboxCard: FC = ({
+    service,
+    field,
+}: {
+    service: ServiceType;
+    field: ControllerRenderProps<FieldValues, "services">;
+}) => {
+    const { t } = useTranslation();
+    return (
+        <div
+            className={cn(
+                "flex mb-2 flex-row gap-2 items-center p-3 rounded-md border",
+                { "bg-secondary": field.value?.includes(service.id) },
+            )}
+        >
+            <Checkbox
+                checked={field.value?.includes(service.id)}
+                onCheckedChange={(checked) => {
+                    return checked
+                        ? field.onChange([...field.value, service.id])
+                        : field.onChange(
+                            field.value?.filter((value: number) => value !== service.id),
+                        );
+                }}
+            />
+            <FormLabel className="flex flex-row justify-between items-center w-full">
+                <ServiceCard service={service} />
+            </FormLabel>
+        </div>
+    );
+};
 
-  return (
-    <FormField
-      control={form.control}
-      name="services"
-      render={() => (
-        <FormItem>
-          <FormLabel>{t("services")}</FormLabel>
-          <ScrollArea className="flex flex-col justify-start px-1 h-full max-h-[20rem]">
-            {data.entity.map((service) => (
-              <FormField
-                key={service.id}
-                control={form.control}
-                name="services"
-                render={({ field }) => (
-                  <FormItem key={service.id}>
-                    <FormControl>
-                      <div
-                        className={cn(
-                          "flex mb-2 flex-row gap-2 items-center p-3 rounded-md border",
-                          { "bg-secondary": field.value?.includes(service.id) },
-                        )}
-                      >
-                        <Checkbox
-                          checked={field.value?.includes(service.id)}
-                          onCheckedChange={(checked) => {
-                            return checked
-                              ? field.onChange([...field.value, service.id])
-                              : field.onChange(
-                                  field.value?.filter(
-                                    (value: number) => value !== service.id,
-                                  ),
-                                );
-                          }}
-                        />
-                        <FormLabel className="flex flex-row justify-between items-center w-full">
-                          <ServiceCard service={service} />
-                        </FormLabel>
-                      </div>
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-            ))}
-          </ScrollArea>
-        </FormItem>
-      )}
-    />
-  );
+export const ServicesField: FC = () => {
+    const form = useFormContext();
+    const { data } = useServicesQuery({ page: 1, size: 100 });
+    const { t } = useTranslation();
+
+    return (
+        <FormField
+            control={form.control}
+            name="services"
+            render={() => (
+                <FormItem>
+                    <FormLabel>{t("services")}</FormLabel>
+                    <ScrollArea className="flex flex-col justify-start px-1 h-full max-h-[20rem]">
+                        {data.entity.map((service) => (
+                            <FormField
+                                key={service.id}
+                                control={form.control}
+                                name="services"
+                                render={({ field }) => (
+                                    <FormItem key={service.id}>
+                                        <FormControl>
+                                            <ServiceCard service={service} field={field} />
+                                        </FormControl>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                        ))}
+                    </ScrollArea>
+                </FormItem>
+            )}
+        />
+    );
 };
