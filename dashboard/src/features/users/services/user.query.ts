f915d8cf76a1c3715f38ae543f0119146f3db8e0
Krr0ptioN <mardin-pop@protonmail.com>
2024-05-29 15:12:05 +0300

    feat: get user service query

---
+++ dashboard/src/features/users/services/user.query.ts
@@ -0,0 +1,21 @@
+import { useQuery } from "@tanstack/react-query";
+import { fetch } from "@marzneshin/utils";
+import type { UserType } from "../types";
+
+export async function fetchUser({
+    queryKey,
+}: { queryKey: [string, string] }): Promise<UserType> {
+    return await fetch(`/users/${queryKey[1]}`).then((result) => {
+        return result;
+    });
+}
+
+export const UserQueryFetchKey = "users";
+
+export const useUserQuery = ({ username }: { username: string }) => {
+    return useQuery({
+        queryKey: [UserQueryFetchKey, username],
+        queryFn: fetchUser,
+        initialData: undefined,
+    });
+};
