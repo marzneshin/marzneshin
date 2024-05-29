46b1861d2b791dbac38763b3d78aaefac5fecc4f
Krr0ptioN <mardin-pop@protonmail.com>
2024-05-29 17:47:23 +0300

    improve: provide on close trigger

--- dashboard/src/routes/_dashboard/services/create.tsx
+++ dashboard/src/routes/_dashboard/services/create.tsx
@@ -1,15 +1,17 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, useNavigate } from "@tanstack/react-router";
 import { MutationDialog } from "@marzneshin/features/services";
 import { useDialog } from "@marzneshin/hooks";
 
 export const Route = createFileRoute("/_dashboard/services/create")({
     component: () => {
         const [createDialogOpen, setCreateDialogOpen] = useDialog(true);
+        const navigate = useNavigate({ from: "/services/create" });
         return (
             <MutationDialog
                 open={createDialogOpen}
                 onOpenChange={setCreateDialogOpen}
                 entity={null}
+                onClose={() => navigate({ to: "/services" })}
             />
         );
     },
