1ffa3eeed974e2d5a5f37ee178c474037050eef8
Krr0ptioN <mardin-pop@protonmail.com>
2024-05-29 16:19:56 +0300

    improve(services): services page handle outlet components

--- dashboard/src/routes/_dashboard/services.tsx
+++ dashboard/src/routes/_dashboard/services.tsx
@@ -1,15 +1,14 @@
 import {
     Card,
     CardContent,
-    CardFooter,
     CardHeader,
     CardTitle,
-    Page
-} from '@marzneshin/components'
-import { ServicesTable } from '@marzneshin/features/services';
-import { createFileRoute } from '@tanstack/react-router'
-import { FC } from 'react';
-import { useTranslation } from 'react-i18next';
+    Page,
+} from "@marzneshin/components";
+import { ServicesTable } from "@marzneshin/features/services";
+import { createFileRoute, Outlet } from "@tanstack/react-router";
+import type { FC } from "react";
+import { useTranslation } from "react-i18next";
 
 export const ServicesPage: FC = () => {
     const { t } = useTranslation();
@@ -17,20 +16,17 @@ export const ServicesPage: FC = () => {
         <Page>
             <Card className="border-0 sm:w-screen md:w-full">
                 <CardHeader>
-                    <CardTitle>
-                        {t('services')}
-                    </CardTitle>
+                    <CardTitle>{t("services")}</CardTitle>
                 </CardHeader>
                 <CardContent>
                     <ServicesTable />
+                    <Outlet />
                 </CardContent>
-                <CardFooter>
-                </CardFooter>
-            </Card >
+            </Card>
         </Page>
-    )
+    );
 };
 
-export const Route = createFileRoute('/_dashboard/services')({
-    component: () => <ServicesPage />
-})
+export const Route = createFileRoute("/_dashboard/services")({
+    component: () => <ServicesPage />,
+});
