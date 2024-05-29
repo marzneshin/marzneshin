742b1eea8a2785aef6ca903aeba2512537eddf7e
Krr0ptioN <mardin-pop@protonmail.com>
2024-05-29 16:08:35 +0300

    fix:  provide on close

--- dashboard/src/routes/_dashboard/users/$userId/index.tsx
+++ dashboard/src/routes/_dashboard/users/$userId/index.tsx
@@ -1,4 +1,4 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, useNavigate } from "@tanstack/react-router";
 import { UsersSettingsDialog, useUserQuery } from "@marzneshin/features/users";
 import { AlertDialog, AlertDialogContent } from "@marzneshin/components";
 import { useDialog } from "@marzneshin/hooks";
@@ -7,12 +7,14 @@ const UserSetting = () => {
     const [settingsDialogOpen, setSettingsDialogOpen] = useDialog(true);
     const { userId } = Route.useParams();
     const { data: user } = useUserQuery({ username: userId });
+    const navigate = useNavigate({ from: "/users/$userId" });
 
     return user ? (
         <UsersSettingsDialog
             open={settingsDialogOpen}
             onOpenChange={setSettingsDialogOpen}
             entity={user}
+            onClose={() => navigate({ to: "/users" })}
         />
     ) : (
         <AlertDialog>
