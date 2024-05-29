52b795cc30dd29806590573269afc0e1da896c28
Krr0ptioN <mardin-pop@protonmail.com>
2024-05-29 16:06:21 +0300

    improve(users-settings): accept on close functionality

--- dashboard/src/features/users/dialogs/settings/index.tsx
+++ dashboard/src/features/users/dialogs/settings/index.tsx
@@ -11,7 +11,7 @@ import {
 } from "@marzneshin/components";
 import { UserServicesTable } from "@marzneshin/features/users";
 import type { UserType } from "@marzneshin/features/users";
-import type { FC } from "react";
+import { type FC, useEffect } from "react";
 import { useTranslation } from "react-i18next";
 import { QRCodeSection, UserInfoTable } from "./user-info";
 import { SubscriptionActions } from "./user-info/subscription-actions";
@@ -19,50 +19,54 @@ import { SubscriptionActions } from "./user-info/subscription-actions";
 interface UsersSettingsDialogProps {
     onOpenChange: (state: boolean) => void;
     open: boolean;
-    entity: UserType | null;
+    entity: UserType;
+    onClose: () => void;
 }
 
 export const UsersSettingsDialog: FC<UsersSettingsDialogProps> = ({
     onOpenChange,
     open,
     entity,
+    onClose,
 }) => {
     const { t } = useTranslation();
 
-    if (entity) {
-        return (
-            <Sheet open={open} onOpenChange={onOpenChange}>
-                <SheetContent className="sm:min-w-full md:min-w-[700px]">
-                    <ScrollArea className="flex flex-col gap-4 m-3 h-full">
-                        <SheetHeader className="my-1">
-                            <SheetTitle>{t("settings")}</SheetTitle>
-                        </SheetHeader>
-                        <Tabs defaultValue="info" className="w-full h-full">
-                            <TabsList className="w-full bg-accent">
-                                <TabsTrigger className="w-full" value="info">
-                                    {" "}
-                                    {t("user_info")}{" "}
-                                </TabsTrigger>
-                                <TabsTrigger className="w-full" value="services">
-                                    {" "}
-                                    {t("services")}{" "}
-                                </TabsTrigger>
-                            </TabsList>
-                            <TabsContent
-                                value="info"
-                                className="flex flex-col gap-2 w-full h-full"
-                            >
-                                <UserInfoTable entity={entity} />
-                                <QRCodeSection entity={entity} />
-                                <SubscriptionActions entity={entity} />
-                            </TabsContent>
-                            <TabsContent value="services">
-                                <UserServicesTable user={entity} />
-                            </TabsContent>
-                        </Tabs>
-                    </ScrollArea>
-                </SheetContent>
-            </Sheet>
-        );
-    }
+    useEffect(() => {
+        if (!open) onClose();
+    }, [open, onClose]);
+
+    return (
+        <Sheet open={open} onOpenChange={onOpenChange}>
+            <SheetContent className="sm:min-w-full md:min-w-[700px]">
+                <ScrollArea className="flex flex-col gap-4 m-3 h-full">
+                    <SheetHeader className="my-1">
+                        <SheetTitle>{t("settings")}</SheetTitle>
+                    </SheetHeader>
+                    <Tabs defaultValue="info" className="w-full h-full">
+                        <TabsList className="w-full bg-accent">
+                            <TabsTrigger className="w-full" value="info">
+                                {" "}
+                                {t("user_info")}{" "}
+                            </TabsTrigger>
+                            <TabsTrigger className="w-full" value="services">
+                                {" "}
+                                {t("services")}{" "}
+                            </TabsTrigger>
+                        </TabsList>
+                        <TabsContent
+                            value="info"
+                            className="flex flex-col gap-2 w-full h-full"
+                        >
+                            <UserInfoTable entity={entity} />
+                            <QRCodeSection entity={entity} />
+                            <SubscriptionActions entity={entity} />
+                        </TabsContent>
+                        <TabsContent value="services">
+                            <UserServicesTable user={entity} />
+                        </TabsContent>
+                    </Tabs>
+                </ScrollArea>
+            </SheetContent>
+        </Sheet>
+    );
 };
--- dashboard/src/features/users/services/user.query.ts
+++ dashboard/src/features/users/services/user.query.ts
@@ -3,19 +3,19 @@ import { fetch } from "@marzneshin/utils";
 import type { UserType } from "../types";
 
 export async function fetchUser({
-    queryKey,
+  queryKey,
 }: { queryKey: [string, string] }): Promise<UserType> {
-    return await fetch(`/users/${queryKey[1]}`).then((result) => {
-        return result;
-    });
+  return await fetch(`/users/${queryKey[1]}`).then((result) => {
+    return result;
+  });
 }
 
 export const UserQueryFetchKey = "users";
 
 export const useUserQuery = ({ username }: { username: string }) => {
-    return useQuery({
-        queryKey: [UserQueryFetchKey, username],
-        queryFn: fetchUser,
-        initialData: undefined,
-    });
+  return useQuery({
+    queryKey: [UserQueryFetchKey, username],
+    queryFn: fetchUser,
+    initialData: null,
+  });
 };
