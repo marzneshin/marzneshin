b49f2859cbd469ecb92ee6a6901f918f1bfd107a
Krr0ptioN <mardin-pop@protonmail.com>
2024-05-29 16:08:26 +0300

    fix: litning and provide on close

--- dashboard/src/routes/_dashboard/users/$userId/edit.tsx
+++ dashboard/src/routes/_dashboard/users/$userId/edit.tsx
@@ -1,26 +1,28 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, useNavigate } from "@tanstack/react-router";
 import { UsersMutationDialog, useUserQuery } from "@marzneshin/features/users";
 import { AlertDialog, AlertDialogContent } from "@marzneshin/components";
 import { useDialog } from "@marzneshin/hooks";
 
 const UserEdit = () => {
-  const [editDialogOpen, setEditDialogOpen] = useDialog(true);
-  const { userId } = Route.useParams();
-  const { data: user } = useUserQuery({ username: userId });
+    const [editDialogOpen, setEditDialogOpen] = useDialog(true);
+    const { userId } = Route.useParams();
+    const { data: user } = useUserQuery({ username: userId });
+    const navigate = useNavigate();
 
-  return user ? (
-    <UsersMutationDialog
-      open={editDialogOpen}
-      onOpenChange={setEditDialogOpen}
-      entity={user}
-    />
-  ) : (
-    <AlertDialog>
-      <AlertDialogContent>User not found</AlertDialogContent>
-    </AlertDialog>
-  );
+    return user ? (
+        <UsersMutationDialog
+            open={editDialogOpen}
+            onOpenChange={setEditDialogOpen}
+            entity={user}
+            onClose={() => navigate({ to: "/users" })}
+        />
+    ) : (
+        <AlertDialog>
+            <AlertDialogContent>User not found</AlertDialogContent>
+        </AlertDialog>
+    );
 };
 
 export const Route = createFileRoute("/_dashboard/users/$userId/edit")({
-  component: UserEdit,
+    component: UserEdit,
 });
