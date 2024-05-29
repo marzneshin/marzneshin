37ed10ec1095a0898ef660d2d5b5f2f66f86a14c
Krr0ptioN <mardin-pop@protonmail.com>
2024-05-29 16:09:52 +0300

    fix(users-create): provide on close

--- dashboard/src/routes/_dashboard/users/create.tsx
+++ dashboard/src/routes/_dashboard/users/create.tsx
@@ -1,16 +1,18 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, useNavigate } from "@tanstack/react-router";
 import { UsersMutationDialog } from "@marzneshin/features/users";
 import { useDialog } from "@marzneshin/hooks";
 
 export const Route = createFileRoute("/_dashboard/users/create")({
-  component: () => {
-    const [mutationDialogOpen, setMutationDialogOpen] = useDialog(true);
-    return (
-      <UsersMutationDialog
-        open={mutationDialogOpen}
-        onOpenChange={setMutationDialogOpen}
-        entity={null}
-      />
-    );
-  },
+    component: () => {
+        const [mutationDialogOpen, setMutationDialogOpen] = useDialog(true);
+        const navigate = useNavigate({ from: "/users/create" });
+        return (
+            <UsersMutationDialog
+                open={mutationDialogOpen}
+                onOpenChange={setMutationDialogOpen}
+                entity={null}
+                onClose={() => navigate({ to: "/users" })}
+            />
+        );
+    },
 });
