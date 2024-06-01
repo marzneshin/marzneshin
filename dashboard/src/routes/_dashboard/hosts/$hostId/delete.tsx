1fa7b3ff2deb6bfc9befee74e781c1a68dff05a7
Krr0ptioN <mardin-pop@protonmail.com>
2024-06-01 19:16:03 +0300

    feat(hosts-delete): routing based dialog

--- dashboard/src/routes/_dashboard/hosts/$hostId/delete.tsx
+++ dashboard/src/routes/_dashboard/hosts/$hostId/delete.tsx
@@ -1,5 +1,27 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, useNavigate } from "@tanstack/react-router";
+import { HostsDeleteConfirmationDialog, useHostQuery } from "@marzneshin/features/hosts";
+import { AlertDialog, AlertDialogContent } from "@marzneshin/components";
+import { useDialog } from "@marzneshin/hooks";
+
+const HostDelete = () => {
+    const [deleteDialogOpen, setDeleteDialogOpen] = useDialog(true);
+    const { hostId } = Route.useParams();
+    const { data: host } = useHostQuery({ hostId: Number.parseInt(hostId) });
+    const navigate = useNavigate({ from: "/hosts/$hostId/delete" });
+    return host ? (
+        <HostsDeleteConfirmationDialog
+            open={deleteDialogOpen}
+            onOpenChange={setDeleteDialogOpen}
+            entity={host}
+            onClose={() => navigate({ to: "/hosts" })}
+        />
+    ) : (
+        <AlertDialog>
+            <AlertDialogContent>Host not found</AlertDialogContent>
+        </AlertDialog>
+    );
+}
 
 export const Route = createFileRoute('/_dashboard/hosts/$hostId/delete')({
-  component: () => <div>Hello /_dashboard/hosts/$hostId/delete!</div>
+    component: HostDelete
 })
