92c295f1588afaa1cc6428554c48de77acf6d8a7
Krr0ptioN <mardin-pop@protonmail.com>
2024-06-01 19:15:54 +0300

    feat(hosts-settings): routing based dialog

--- dashboard/src/routes/_dashboard/hosts/$hostId/index.tsx
+++ dashboard/src/routes/_dashboard/hosts/$hostId/index.tsx
@@ -1,5 +1,28 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, useNavigate } from "@tanstack/react-router";
+import { HostSettingsDialog, useHostQuery } from "@marzneshin/features/hosts";
+import { AlertDialog, AlertDialogContent } from "@marzneshin/components";
+import { useDialog } from "@marzneshin/hooks";
+
+
+const HostOpen = () => {
+    const [settingsDialogOpen, setSettingsDialogOpen] = useDialog(true);
+    const { hostId } = Route.useParams();
+    const { data: host } = useHostQuery({ hostId: Number.parseInt(hostId) });
+    const navigate = useNavigate({ from: "/hosts/$hostId" });
+    return host ? (
+        <HostSettingsDialog
+            open={settingsDialogOpen}
+            onOpenChange={setSettingsDialogOpen}
+            entity={host}
+            onClose={() => navigate({ to: "/hosts" })}
+        />
+    ) : (
+        <AlertDialog>
+            <AlertDialogContent>Host not found</AlertDialogContent>
+        </AlertDialog>
+    );
+}
 
 export const Route = createFileRoute('/_dashboard/hosts/$hostId/')({
-  component: () => <div>Hello /_dashboard/hosts/$hostId/!</div>
+    component: HostOpen
 })
