acb0d0e2691f18a92d08d49f6a6ac2d7816ce010
Krr0ptioN <mardin-pop@protonmail.com>
2024-06-01 19:22:03 +0300

    feat: onclose for hosts dialogs

--- dashboard/src/features/hosts/dialogs/delete-confirmation/index.tsx
+++ dashboard/src/features/hosts/dialogs/delete-confirmation/index.tsx
@@ -1,22 +1,26 @@
 import { DeleteConfirmation } from '@marzneshin/components'
-import { FC } from 'react'
+import { type FC, useEffect } from 'react'
 import { HostType, useHostsDeletionMutation } from '@marzneshin/features/hosts'
 
 interface HostsDeleteConfirmationDialogProps {
     onOpenChange: (state: boolean) => void
+    onClose: () => void
     open: boolean
-    entity: HostType | null
+    entity: HostType
 }
 
-export const HostsDeleteConfirmationDialog: FC<HostsDeleteConfirmationDialogProps> = ({ onOpenChange, open, entity }) => {
+export const HostsDeleteConfirmationDialog: FC<HostsDeleteConfirmationDialogProps> = ({ onOpenChange, open, entity, onClose }) => {
     const deleteMutation = useHostsDeletionMutation();
-    if (entity !== null) {
-        return (
-            <DeleteConfirmation
-                open={open}
-                onOpenChange={onOpenChange}
-                action={() => deleteMutation.mutate(entity)}
-            />
-        )
-    }
+
+    useEffect(() => {
+        if (!open) onClose();
+    }, [open, onClose]);
+
+    return (
+        <DeleteConfirmation
+            open={open}
+            onOpenChange={onOpenChange}
+            action={() => deleteMutation.mutate(entity)}
+        />
+    )
 }
--- dashboard/src/features/hosts/dialogs/settings/index.tsx
+++ dashboard/src/features/hosts/dialogs/settings/index.tsx
@@ -1,4 +1,4 @@
-
+import { useEffect } from "react";
 import {
     Sheet,
     SheetContent,
@@ -16,85 +16,87 @@ import { HostType } from "../.."
 
 interface HostsSettingsDialogProps {
     onOpenChange: (state: boolean) => void
+    onClose: () => void
     open: boolean
-    entity: HostType | null
+    entity: HostType
 }
 
-export const HostSettingsDialog: FC<HostsSettingsDialogProps> = ({ onOpenChange, open, entity }) => {
-
+export const HostSettingsDialog: FC<HostsSettingsDialogProps> = ({ onOpenChange, open, entity, onClose }) => {
     const { t } = useTranslation();
 
-    if (entity) {
-        return (
-            <Sheet open={open} onOpenChange={onOpenChange}>
-                <SheetContent className="sm:min-w-full md:min-w-[700px]" >
-                    <SheetHeader>
-                        <SheetTitle>
-                            {t('page.services.settings.title')}
-                        </SheetTitle>
-                    </SheetHeader>
-                    <Table>
-                        <TableBody>
-                            <TableRow>
-                                <TableHead>
-                                    {t('remark')}
-                                </TableHead>
-                                <TableCell>
-                                    {entity.remark}
-                                </TableCell>
-                            </TableRow>
-                            <TableRow>
-                                <TableHead>
-                                    {t('address')}
-                                </TableHead>
-                                <TableCell>
-                                    {entity.address}
-                                </TableCell>
-                            </TableRow>
-                            <TableRow>
-                                <TableHead>
-                                    {t('port')}
-                                </TableHead>
-                                <TableCell>
-                                    {entity.port}
-                                </TableCell>
-                            </TableRow>
-                            <TableRow>
-                                <TableHead>
-                                    {t('host')}
-                                </TableHead>
-                                <TableCell>
-                                    {entity.host}
-                                </TableCell>
-                            </TableRow>
-                            <TableRow>
-                                <TableHead>
-                                    {t('path')}
-                                </TableHead>
-                                <TableCell>
-                                    {entity.path}
-                                </TableCell>
-                            </TableRow>
-                            <TableRow>
-                                <TableHead>
-                                    {t('sni')}
-                                </TableHead>
-                                <TableCell>
-                                    {entity.sni}
-                                </TableCell>
-                            </TableRow>
-                            <TableRow>
-                                <TableHead>
-                                    {t('alpn')}
-                                </TableHead>
-                                <TableCell>
-                                    {entity.alpn}
-                                </TableCell>
-                            </TableRow>
-                        </TableBody>
-                    </Table>
-                </SheetContent>
-            </Sheet>
-        )
-    }
+    useEffect(() => {
+        if (!open) onClose();
+    }, [open, onClose]);
+
+    return (
+        <Sheet open={open} onOpenChange={onOpenChange}>
+            <SheetContent className="sm:min-w-full md:min-w-[700px]" >
+                <SheetHeader>
+                    <SheetTitle>
+                        {t('page.services.settings.title')}
+                    </SheetTitle>
+                </SheetHeader>
+                <Table>
+                    <TableBody>
+                        <TableRow>
+                            <TableHead>
+                                {t('remark')}
+                            </TableHead>
+                            <TableCell>
+                                {entity.remark}
+                            </TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableHead>
+                                {t('address')}
+                            </TableHead>
+                            <TableCell>
+                                {entity.address}
+                            </TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableHead>
+                                {t('port')}
+                            </TableHead>
+                            <TableCell>
+                                {entity.port}
+                            </TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableHead>
+                                {t('host')}
+                            </TableHead>
+                            <TableCell>
+                                {entity.host}
+                            </TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableHead>
+                                {t('path')}
+                            </TableHead>
+                            <TableCell>
+                                {entity.path}
+                            </TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableHead>
+                                {t('sni')}
+                            </TableHead>
+                            <TableCell>
+                                {entity.sni}
+                            </TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableHead>
+                                {t('alpn')}
+                            </TableHead>
+                            <TableCell>
+                                {entity.alpn}
+                            </TableCell>
+                        </TableRow>
+                    </TableBody>
+                </Table>
+            </SheetContent>
+        </Sheet>
+    )
 }
