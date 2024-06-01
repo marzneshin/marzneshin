b8d9b680ea17df181fbfb0ecd187c0e96e75aaac
Krr0ptioN <mardin-pop@protonmail.com>
2024-06-01 19:29:36 +0300

    improve: show alert dialog when there is no inbound

--- dashboard/src/routes/_dashboard/hosts/$inboundId/create.tsx
+++ dashboard/src/routes/_dashboard/hosts/$inboundId/create.tsx
@@ -1,5 +1,6 @@
 import { createFileRoute, useNavigate } from "@tanstack/react-router";
 import { HostsMutationDialog, useInboundQuery } from "@marzneshin/features/hosts";
+import { AlertDialog, AlertDialogContent } from "@marzneshin/components";
 import { useDialog } from "@marzneshin/hooks";
 
 const HostCreate = () => {
@@ -7,13 +8,17 @@ const HostCreate = () => {
     const { inboundId } = Route.useParams();
     const { data: inbound } = useInboundQuery({ inboundId: Number.parseInt(inboundId) });
     const navigate = useNavigate({ from: "/hosts/$inboundId/create" });
-    return (
+    return inbound ? (
         <HostsMutationDialog
             open={createDialogOpen}
             onOpenChange={setCreateDialogOpen}
             inboundId={Number(inbound.id)}
             onClose={() => navigate({ to: "/hosts" })}
         />
+    ) : (
+        <AlertDialog>
+            <AlertDialogContent>Inbound not found</AlertDialogContent>
+        </AlertDialog>
     );
 }
 
