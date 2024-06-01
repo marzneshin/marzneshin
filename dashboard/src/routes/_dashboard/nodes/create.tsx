0b1599cc03ea87a2b299aef70a7ec2fb38a70b61
Krr0ptioN <mardin-pop@protonmail.com>
2024-06-01 19:14:32 +0300

    fix(lint): hooks werent recognized

--- dashboard/src/routes/_dashboard/nodes/create.tsx
+++ dashboard/src/routes/_dashboard/nodes/create.tsx
@@ -2,17 +2,19 @@ import { createFileRoute, useNavigate } from "@tanstack/react-router";
 import { MutationDialog } from "@marzneshin/features/nodes";
 import { useDialog } from "@marzneshin/hooks";
 
+const NodeCreate = () => {
+    const [createDialogOpen, setCreateDialogOpen] = useDialog(true);
+    const navigate = useNavigate({ from: "/nodes/create" });
+    return (
+        <MutationDialog
+            open={createDialogOpen}
+            onOpenChange={setCreateDialogOpen}
+            entity={null}
+            onClose={() => navigate({ to: "/nodes" })}
+        />
+    );
+}
+
 export const Route = createFileRoute("/_dashboard/nodes/create")({
-    component: () => {
-        const [createDialogOpen, setCreateDialogOpen] = useDialog(true);
-        const navigate = useNavigate({ from: "/nodes/create" });
-        return (
-            <MutationDialog
-                open={createDialogOpen}
-                onOpenChange={setCreateDialogOpen}
-                entity={null}
-                onClose={() => navigate({ to: "/nodes" })}
-            />
-        );
-    },
+    component: NodeCreate,
 });
--- dashboard/src/routes/_dashboard/services/create.tsx
+++ dashboard/src/routes/_dashboard/services/create.tsx
@@ -2,17 +2,19 @@ import { createFileRoute, useNavigate } from "@tanstack/react-router";
 import { MutationDialog } from "@marzneshin/features/services";
 import { useDialog } from "@marzneshin/hooks";
 
+const ServiceCreate = () => {
+    const [createDialogOpen, setCreateDialogOpen] = useDialog(true);
+    const navigate = useNavigate({ from: "/services/create" });
+    return (
+        <MutationDialog
+            open={createDialogOpen}
+            onOpenChange={setCreateDialogOpen}
+            entity={null}
+            onClose={() => navigate({ to: "/services" })}
+        />
+    );
+}
+
 export const Route = createFileRoute("/_dashboard/services/create")({
-    component: () => {
-        const [createDialogOpen, setCreateDialogOpen] = useDialog(true);
-        const navigate = useNavigate({ from: "/services/create" });
-        return (
-            <MutationDialog
-                open={createDialogOpen}
-                onOpenChange={setCreateDialogOpen}
-                entity={null}
-                onClose={() => navigate({ to: "/services" })}
-            />
-        );
-    },
+    component: ServiceCreate,
 });
