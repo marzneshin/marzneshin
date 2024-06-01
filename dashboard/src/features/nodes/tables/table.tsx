3bda24131814cd1ed9d31fb58fdbe05b2f44d8ed
Krr0ptioN <mardin-pop@protonmail.com>
2024-06-01 15:21:02 +0300

    feat(nodes-table): routing based handlers for actions

--- dashboard/src/features/nodes/tables/table.tsx
+++ dashboard/src/features/nodes/tables/table.tsx
@@ -1,23 +1,38 @@
 import { FC } from "react";
 import {
     columns,
-    NodesSettingsDialog,
     fetchNodes,
 } from '@marzneshin/features/nodes';
-import { MutationDialog } from "../dialogs/mutation.dialog";
-import { NodesDeleteConfirmationDialog } from "../dialogs/delete-confirmation.dialog";
 import { EntityTable } from "@marzneshin/components";
+import { useNavigate } from "@tanstack/react-router";
 
 export const NodesTable: FC = () => {
+    const navigate = useNavigate({ from: "/nodes" });
     return (
         <EntityTable
             fetchEntity={fetchNodes}
-            MutationDialog={MutationDialog}
-            DeleteConfirmationDialog={NodesDeleteConfirmationDialog}
-            SettingsDialog={NodesSettingsDialog}
             columnsFn={columns}
             filteredColumn="name"
             entityKey="nodes"
+            onCreate={() => navigate({ to: "/nodes/create" })}
+            onOpen={(entity) =>
+                navigate({
+                    to: "/nodes/$nodeId",
+                    params: { nodeId: String(entity.id) },
+                })
+            }
+            onEdit={(entity) =>
+                navigate({
+                    to: "/nodes/$nodeId/edit",
+                    params: { nodeId: String(entity.id) },
+                })
+            }
+            onDelete={(entity) =>
+                navigate({
+                    to: "/nodes/$nodeId/delete",
+                    params: { nodeId: String(entity.id) },
+                })
+            }
         />
     )
 }
