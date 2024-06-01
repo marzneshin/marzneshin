821e5528e643ddf50b861fa07db33436fe816a6e
Krr0ptioN <mardin-pop@protonmail.com>
2024-06-01 23:18:15 +0300

    fix: determined expiration method correctly

--- dashboard/src/features/users/dialogs/mutation/sections/use-expiration-method-tabs.ts
+++ dashboard/src/features/users/dialogs/mutation/sections/use-expiration-method-tabs.ts
@@ -7,7 +7,7 @@ type ExpirationMethodStrategy = "onhold" | "determined" | "unlimited";
 const getUserExpirationMethod = (entity: UserMutationType): ExpirationMethodStrategy => {
     if (entity.status === "on_hold")
         return 'onhold'
-    else if (entity.expire !== undefined)
+    else if (entity.expire !== undefined && entity.expire !== null)
         return 'determined'
     else
         return 'unlimited'
@@ -17,7 +17,8 @@ const getUserExpirationMethod = (entity: UserMutationType): ExpirationMethodStra
 export const useExpirationMethodTabs = ({ entity }: { entity: UserMutationType | null }) => {
     const form = useFormContext()
 
-    const defaultExpirationMethodTab = entity?.username ? getUserExpirationMethod(entity) : 'determined'
+    const defaultExpirationMethodTab =
+        (entity !== null) ? getUserExpirationMethod(entity) : 'determined'
     const [
         selectedExpirationMethodTab,
         setSelectedExpirationMethodTab
