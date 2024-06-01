bd620535513963ca9f01f9c752d64313a16a8dc1
Krr0ptioN <mardin-pop@protonmail.com>
2024-06-01 19:21:34 +0300

    feat(hosts-edit): routing based dialog

--- dashboard/src/routes/_dashboard/hosts/$hostId/edit.tsx
+++ dashboard/src/routes/_dashboard/hosts/$hostId/edit.tsx
@@ -1,5 +1,28 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, useNavigate } from "@tanstack/react-router";
+import { HostsMutationDialog, useHostQuery } from "@marzneshin/features/hosts";
+import { AlertDialog, AlertDialogContent } from "@marzneshin/components";
+import { useDialog } from "@marzneshin/hooks";
+
+
+const HostEdit = () => {
+    const [editDialogOpen, setEditDialogOpen] = useDialog(true);
+    const { hostId } = Route.useParams();
+    const { data: host } = useHostQuery({ hostId: Number.parseInt(hostId) });
+    const navigate = useNavigate({ from: "/hosts/$hostId/edit" });
+    return host ? (
+        <HostsMutationDialog
+            open={editDialogOpen}
+            onOpenChange={setEditDialogOpen}
+            entity={host}
+            onClose={() => navigate({ to: "/hosts" })}
+        />
+    ) : (
+        <AlertDialog>
+            <AlertDialogContent>Host not found</AlertDialogContent>
+        </AlertDialog>
+    );
+}
 
 export const Route = createFileRoute('/_dashboard/hosts/$hostId/edit')({
-  component: () => <div>Hello /_dashboard/hosts/$hostId/edit!</div>
+    component: HostEdit
 })
