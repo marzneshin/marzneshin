c1137530c1a1bf443d48ebac8a0de277396ca750
Krr0ptioN <mardin-pop@protonmail.com>
2024-06-04 18:19:04 +0300

    fix: use standard types for story definition

--- dashboard/src/components/ui/button/button.stories.tsx
+++ dashboard/src/components/ui/button/button.stories.tsx
@@ -1,81 +1,94 @@
 import { Button } from './index'
 import { action } from "@storybook/addon-actions";
+import type { Meta, StoryObj } from "@storybook/react";
 
-export default {
+
+const meta: Meta<typeof Button> = {
     title: "Components/UI/Button",
     component: Button,
 };
 
-const Template = args => <Button {...args} />
+export default meta;
+
+type Story = StoryObj<typeof Button>;
 
 
-export const Primary = Template.bind({})
-Primary.args = {
-    onMouseDown: action('clicked'),
-    children: 'Primary'
+export const Primary: Story = {
+    args: {
+        onMouseDown: action('clicked'),
+        children: 'Primary'
+    }
 }
 
-export const Secondary = Template.bind({})
-Secondary.args = {
-    onMouseDown: action('clicked'),
-    variant: 'secondary',
-    children: 'Secondary'
+export const Secondary: Story = {
+    args: {
+        onMouseDown: action('clicked'),
+        variant: 'secondary',
+        children: 'Secondary'
+    }
 }
 
-export const Destructive = Template.bind({})
-Destructive.args = {
-    onMouseDown: action('clicked'),
-    variant: 'destructive',
-    children: 'Destructive'
+export const Destructive: Story = {
+    args: {
+        onMouseDown: action('clicked'),
+        variant: 'destructive',
+        children: 'Destructive'
+    }
 }
 
-export const Success = Template.bind({})
-Success.args = {
-    onMouseDown: action('clicked'),
-    variant: 'success',
-    children: 'Success'
+export const Success: Story = {
+    args: {
+        onMouseDown: action('clicked'),
+        variant: 'success',
+        children: 'Success'
+    }
 }
 
-
-export const Ghost = Template.bind({})
-Ghost.args = {
+export const Ghost: Story = {
+    args : {
     onMouseDown: action('clicked'),
     variant: 'ghost',
     children: 'Ghost'
+    }
 }
 
-export const Link = Template.bind({})
-Link.args = {
+export const Link: Story = {
+    args : {
     onMouseDown: action('clicked'),
     variant: 'link',
     children: 'Ghost'
+    }
 }
 
-export const SmallSize = Template.bind({})
-SmallSize.args = {
+export const SmallSize: Story = {
+    args : {
     onMouseDown: action('clicked'),
     children: 'Small',
     size: 'sm'
+    }
 }
 
-export const DefaultSize = Template.bind({})
-DefaultSize.args = {
+export const DefaultSize: Story = {
+    args : {
     onMouseDown: action('clicked'),
     children: 'Default',
     size: 'sm'
+    }
 }
 
-export const LargeSize = Template.bind({})
-LargeSize.args = {
+export const LargeSize: Story = {
+    args : {
     onMouseDown: action('clicked'),
     children: 'Large',
     size: 'lg'
+    }
 }
 
-export const IconSize = Template.bind({})
-IconSize.args = {
+export const IconSize: Story = {
+    args : {
     onMouseDown: action('clicked'),
     children: 'I',
     size: 'icon'
+    }
 }
 
