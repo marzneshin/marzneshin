3f937bff0a7eda09fb6d45e72eca0388a8d4cfb5
Krr0ptioN <mardin-pop@protonmail.com>
2024-06-06 01:07:05 +0300

    docs(github-repo): story

---
+++ dashboard/src/features/github-repo/github-repo.stories.tsx
@@ -0,0 +1,22 @@
+import { GithubRepo } from './index'
+import type { Meta, StoryObj } from "@storybook/react";
+import { within, userEvent, expect } from '@storybook/test';
+
+const meta: Meta<typeof GithubRepo> = {
+    title: "Features/Github Repo Stats",
+    component: GithubRepo,
+    tags: ["autodocs"]
+};
+
+export default meta;
+
+type Story = StoryObj<typeof GithubRepo>;
+
+/**
+ * Github repo present repo fullname, description, and repo stars.
+ * */
+export const GithubRepoStats: Story = {
+    args: {}
+}
+
+
