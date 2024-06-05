b03ce4d1af32b137fc989c8e7f7313075f9a6bce
Krr0ptioN <mardin-pop@protonmail.com>
2024-06-06 01:05:31 +0300

    chore: project info data

---
+++ dashboard/src/utils/project-info.ts
@@ -0,0 +1,8 @@
+
+export const projectInfo = {
+    repo: "khodedawsh/marzneshin",
+    github: "https://github.com/khodedawsh/marzneshin",
+    donationLink: "https://github.com/khodedawsh/marzneshin#donation",
+    website: "marzneshin.com",
+    authors: ["Mardin", "Dawsh"]
+}
