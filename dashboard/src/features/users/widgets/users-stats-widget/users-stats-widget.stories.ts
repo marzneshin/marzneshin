b119f38aac6c6ccdeae1a48b16f3568c930a73e9
Krr0ptioN <mardin-pop@protonmail.com>
2024-06-06 21:35:47 +0300

    docs(users-stats): story

---
+++ dashboard/src/features/users/widgets/users-stats-widget/users-stats-widget.stories.ts
@@ -0,0 +1,29 @@
+import { UsersStatsWidget } from "./index"
+import type { Meta, StoryObj } from "@storybook/react";
+import { UsersStatsLegend } from "./legend"
+
+const meta: Meta<typeof UsersStatsWidget> = {
+    title: "Features/Users/Users Stats",
+    component: UsersStatsWidget,
+    tags: ["autodocs"]
+};
+
+export default meta;
+
+type WidgetStory = StoryObj<typeof UsersStatsWidget>;
+
+/**
+ * Users chart widget which shows the active, online, expired, on hold, and limited users.
+ * */
+export const UsersChart: WidgetStory = {
+    args: {
+        active: 50,
+        online: 30,
+        expired: 10,
+        on_hold: 5,
+        limited: 5,
+    }
+}
+
+type LegendStory = StoryObj<typeof UsersStatsLegend>;
+export const UsersLegend: LegendStory = {}
