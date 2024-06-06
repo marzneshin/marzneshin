24dccdafc922f9b6b66c734eb414efb50decc438
Krr0ptioN <mardin-pop@protonmail.com>
2024-06-06 14:08:44 +0300

    refact(theme-switch): adapted for menu header with sub menu items to select theme

--- dashboard/src/features/theme-switch/theme-toggle/index.tsx
+++ dashboard/src/features/theme-switch/theme-toggle/index.tsx
@@ -1,11 +1,10 @@
 import { Moon, Sun } from "lucide-react";
-
 import {
-    Button,
-    DropdownMenu,
-    DropdownMenuContent,
+    DropdownMenuSub,
+    DropdownMenuSubContent,
+    DropdownMenuSubTrigger,
     DropdownMenuItem,
-    DropdownMenuTrigger,
+    DropdownMenuPortal,
 } from "@marzneshin/components";
 import { useTheme } from "../theme-provider";
 import { useTranslation } from "react-i18next";
@@ -13,30 +12,29 @@ import { useTranslation } from "react-i18next";
 export function ThemeToggle() {
     const { setTheme } = useTheme();
     const { t } = useTranslation();
+
     return (
-        <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-                <Button
-                    variant="outline"
-                    className="bg-gray-800 text-secondary dark:hover:bg-secondary-foreground dark:hover:text-secondary dark:text-secondary-foreground"
-                    size="icon"
-                >
-                    <Sun className="transition-all scale-100 rotate-0 dark:scale-0 dark:-rotate-90 h-[1rem] w-[1rem]" />
-                    <Moon className="absolute transition-all scale-0 rotate-90 dark:scale-100 dark:rotate-0 h-[1rem] w-[1rem]" />
-                    <span className="sr-only">Toggle theme</span>
-                </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent align="end">
-                <DropdownMenuItem onClick={() => setTheme("light")}>
-                    {t("light")}
-                </DropdownMenuItem>
-                <DropdownMenuItem onClick={() => setTheme("dark")}>
-                    {t("dark")}
-                </DropdownMenuItem>
-                <DropdownMenuItem onClick={() => setTheme("system")}>
-                    {t("system")}
-                </DropdownMenuItem>
-            </DropdownMenuContent>
-        </DropdownMenu>
+        <DropdownMenuSub>
+            <DropdownMenuSubTrigger arrowDir="left" className="w-full flex">
+                <div className="flex items-center ml-auto">
+                    <Sun className="w-4 h-4 m-0 transition-all transform scale-100 rotate-0 dark:scale-0 dark:-rotate-90" />
+                    <Moon className="w-4 h-4 m-0 transition-all transform scale-0 rotate-90 dark:scale-100 dark:rotate-0" />
+                </div>
+                <span>Theme</span>
+            </DropdownMenuSubTrigger>
+            <DropdownMenuPortal>
+                <DropdownMenuSubContent>
+                    <DropdownMenuItem onClick={() => setTheme("light")}>
+                        {t("light")}
+                    </DropdownMenuItem>
+                    <DropdownMenuItem onClick={() => setTheme("dark")}>
+                        {t("dark")}
+                    </DropdownMenuItem>
+                    <DropdownMenuItem onClick={() => setTheme("system")}>
+                        {t("system")}
+                    </DropdownMenuItem>
+                </DropdownMenuSubContent>
+            </DropdownMenuPortal>
+        </DropdownMenuSub>
     );
 }
