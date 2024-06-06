e8a63c3c723cc14f82e7cb664a4fdb30758f056a
Krr0ptioN <mardin-pop@protonmail.com>
2024-06-06 14:07:14 +0300

    feat(github-repo): mini variant github repo

--- dashboard/src/features/github-repo/index.tsx
+++ dashboard/src/features/github-repo/index.tsx
@@ -8,20 +8,24 @@ import {
 import {
     useGithubRepoStatsQuery
 } from "./services";
+import { type FC } from "react";
 import {
     GithubIcon,
     StarIcon,
 } from "lucide-react";
 
+interface GithubRepoProps {
+    variant?: "full" | "mini"
+}
 
-export const GithubRepo = (props: {}) => {
+export const GithubRepo: FC<GithubRepoProps> = ({ variant = "full" }) => {
     const { data: stats } = useGithubRepoStatsQuery()
     return (
-        <Button className="bg-gray-800 text-secondary dark:hover:bg-secondary-foreground dark:hover:text-secondary dark:text-secondary-foreground p-2" asChild>
+        <Button variant="secondary" className="bg-gray-800 border-0 dark:text-primary text-secondary dark:hover:text-secondary dark:hover:bg-primary hover:bg-secondary hover:text-primary p-2" asChild>
             <Card>
                 <CardContent className="hstack size-fit p-0 gap-2 items-center">
                     <GithubIcon className="size-fit" />
-                    <div className="vstack items-start">
+                    {variant === "full" ? (<div className="vstack items-start">
                         <CardTitle className="font-mono text-xs hstack justify-between w-full">
                             {stats.full_name}
                             <div className="hstack gap-1 font-bold items-center text-xs">
@@ -33,6 +37,12 @@ export const GithubRepo = (props: {}) => {
                             {stats.description}
                         </CardDescription>
                     </div>
+                    ) : (
+                        <CardDescription className="hstack gap-1 font-bold items-center text-xs">
+                            <StarIcon className="size-3" />
+                            {stats.stargazers_count}
+                        </CardDescription>
+                    )}
                 </CardContent>
             </Card>
         </Button>
