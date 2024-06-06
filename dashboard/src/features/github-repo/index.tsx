dca551fb4cfa90d2102921580c9b573fdb6e3071
Krr0ptioN <mardin-pop@protonmail.com>
2024-06-06 15:17:45 +0300

    feat(github-repo): clicking on repo stats must open the github repo in a new tab

--- dashboard/src/features/github-repo/index.tsx
+++ dashboard/src/features/github-repo/index.tsx
@@ -13,6 +13,7 @@ import {
     GithubIcon,
     StarIcon,
 } from "lucide-react";
+import { projectInfo } from "@marzneshin/utils";
 
 interface GithubRepoProps {
     variant?: "full" | "mini"
@@ -23,27 +24,29 @@ export const GithubRepo: FC<GithubRepoProps> = ({ variant = "full" }) => {
     return (
         <Button variant="secondary" className="bg-gray-800 border-0 dark:text-primary text-secondary dark:hover:text-secondary dark:hover:bg-primary hover:bg-secondary hover:text-primary p-2" asChild>
             <Card>
-                <CardContent className="hstack size-fit p-0 gap-2 items-center">
-                    <GithubIcon className="size-fit" />
-                    {variant === "full" ? (<div className="vstack items-start">
-                        <CardTitle className="font-mono text-xs hstack justify-between w-full">
-                            {stats.full_name}
-                            <div className="hstack gap-1 font-bold items-center text-xs">
+                <a href={projectInfo.github} target="_blank">
+                    <CardContent className="hstack size-fit p-0 gap-2 items-center">
+                        <GithubIcon className="size-fit" />
+                        {variant === "full" ? (<div className="vstack items-start">
+                            <CardTitle className="font-mono text-xs hstack justify-between w-full">
+                                {stats.full_name}
+                                <div className="hstack gap-1 font-bold items-center text-xs">
+                                    <StarIcon className="size-3" />
+                                    {stats.stargazers_count}
+                                </div>
+                            </CardTitle>
+                            <CardDescription className="text-xs">
+                                {stats.description}
+                            </CardDescription>
+                        </div>
+                        ) : (
+                            <CardDescription className="hstack gap-1 font-bold items-center text-xs">
                                 <StarIcon className="size-3" />
                                 {stats.stargazers_count}
-                            </div>
-                        </CardTitle>
-                        <CardDescription className="text-xs">
-                            {stats.description}
-                        </CardDescription>
-                    </div>
-                    ) : (
-                        <CardDescription className="hstack gap-1 font-bold items-center text-xs">
-                            <StarIcon className="size-3" />
-                            {stats.stargazers_count}
-                        </CardDescription>
-                    )}
-                </CardContent>
+                            </CardDescription>
+                        )}
+                    </CardContent>
+                </a>
             </Card>
         </Button>
     )
