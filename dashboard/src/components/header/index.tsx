1c5750d129fd79d8f704eb07a77c4613285d9e54
Krr0ptioN <mardin-pop@protonmail.com>
2024-06-06 14:10:44 +0300

    style(header): mini github repo for mobile screen

--- dashboard/src/components/header/index.tsx
+++ dashboard/src/components/header/index.tsx
@@ -1,12 +1,13 @@
-import { LanguageSwitch } from "@marzneshin/features/language-switch";
-import { ThemeToggle } from "@marzneshin/features/theme-switch";
-import { Logout } from "@marzneshin/features/auth";
+import { HeaderMenu } from "./menu";
 import { GithubRepo } from "@marzneshin/features/github-repo";
 import { Link } from "@tanstack/react-router";
 import { CommandBox } from "@marzneshin/features/search-command";
 import type { FC, PropsWithChildren } from "react";
+import { useScreenBreakpoint } from "@marzneshin/hooks/use-screen-breakpoint";
 
 export const Header: FC<PropsWithChildren> = ({ children }) => {
+    const isDesktop = useScreenBreakpoint("md");
+
     return (
         <div className="flex flex-row justify-between items-center p-1 px-4 w-full h-full bg-primary text-primary-foreground dark:bg-primary-foreground">
             <div className="flex flex-row gap-4 justify-center items-center">
@@ -16,13 +17,11 @@ export const Header: FC<PropsWithChildren> = ({ children }) => {
                     </div>
                 </Link>
                 {children}
+                <CommandBox />
             </div>
-            <CommandBox />
             <div className="flex flex-row gap-2 h-10">
-                <GithubRepo />
-                <ThemeToggle />
-                <LanguageSwitch />
-                <Logout />
+                <GithubRepo variant={isDesktop ? "full" : "mini"} />
+                <HeaderMenu />
             </div>
         </div>
     );
