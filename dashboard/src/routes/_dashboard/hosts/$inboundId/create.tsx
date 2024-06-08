444163d85b160c74e5b6f0feda7715901403e108
Krr0ptioN <mardin-pop@protonmail.com>
2024-06-08 13:30:08 +0300

    perf(hosts-creation): removing the uncessary inbounds fetching since the id is what is required

--- dashboard/src/routes/_dashboard/hosts/$inboundId/create.tsx
+++ dashboard/src/routes/_dashboard/hosts/$inboundId/create.tsx
@@ -1,27 +1,28 @@
 import { createFileRoute, useNavigate } from "@tanstack/react-router";
-import { HostsMutationDialog, useInboundQuery } from "@marzneshin/features/hosts";
+import { HostsMutationDialog } from "@marzneshin/features/hosts";
 import { AlertDialog, AlertDialogContent } from "@marzneshin/components";
 import { useDialog } from "@marzneshin/hooks";
 
 const HostCreate = () => {
     const [createDialogOpen, setCreateDialogOpen] = useDialog(true);
     const { inboundId } = Route.useParams();
-    const { data: inbound } = useInboundQuery({ inboundId: Number.parseInt(inboundId) });
     const navigate = useNavigate({ from: "/hosts/$inboundId/create" });
-    return inbound ? (
+
+    return (
         <HostsMutationDialog
             open={createDialogOpen}
             onOpenChange={setCreateDialogOpen}
-            inboundId={Number(inbound.id)}
+            inboundId={Number(inboundId)}
             onClose={() => navigate({ to: "/hosts" })}
         />
-    ) : (
-        <AlertDialog>
-            <AlertDialogContent>Inbound not found</AlertDialogContent>
-        </AlertDialog>
     );
 }
 
 export const Route = createFileRoute('/_dashboard/hosts/$inboundId/create')({
+    errorComponent: () => (
+        <AlertDialog open={true}>
+            <AlertDialogContent>Host not found</AlertDialogContent>
+        </AlertDialog>
+    ),
     component: HostCreate,
 });
