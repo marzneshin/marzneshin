5c4d1ee518d441209f25d323228ea3e855087498
Krr0ptioN <mardin-pop@protonmail.com>
2024-06-12 00:10:49 +0300

    docs(header): header anatomy, header with menu, dashbaord header

---
+++ dashboard/src/components/header/header.stories.tsx
@@ -0,0 +1,42 @@
+import { HeaderMenu, Header } from ".";
+import { CommandBox } from "@marzneshin/features/search-command";
+import { HeaderLogo } from "@marzneshin/components";
+import type { Meta, StoryObj } from "@storybook/react";
+
+
+const meta: Meta<typeof Header> = {
+    title: "Components/Header",
+    component: Header,
+    tags: ["autodocs"],
+    subcomponents: { HeaderMenu }
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Header>;
+
+
+export const HeaderAnatomy: Story = {
+    args: {
+        start: <h1>Start section</h1>,
+        center: <h1>Center section</h1>,
+        end: <h1>End section</h1>
+    }
+}
+
+export const HeaderWithMenu: Story = {
+    args: {
+        start: <h1>Start section</h1>,
+        center: <h1>Center section</h1>,
+        end: <HeaderMenu />
+    }
+}
+
+
+export const DashoardHeader: Story = {
+    args: {
+        start: <HeaderLogo />,
+        center: <CommandBox />,
+        end: <HeaderMenu />
+    }
+}
