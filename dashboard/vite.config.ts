c9419feddd6ec176e32ec0267bb863b5d9c8f550
Mardin <54506351+Krr0ptioN@users.noreply.github.com>
2024-06-13 12:21:11 +0300

    improve(version): display full version for both dev and prod (#376)
    * improve: display full version for both dev and prod
    
    * fix(ci): supporting CI environment for setting git version to dashboard
    
    * fix(ci): fetch with depth 0
    
    * fix(security): add path to commends
    
    * fix(docker): git dependencies
    
    * fix: assign the latest version

--- dashboard/vite.config.ts
+++ dashboard/vite.config.ts
@@ -3,25 +3,43 @@ import react from '@vitejs/plugin-react-swc'
 import { TanStackRouterVite } from '@tanstack/router-vite-plugin';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import path from 'path';
+import { execSync } from 'child_process';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [
-        TanStackRouterVite(),
-        tsconfigPaths(),
-        react()
-    ],
-    root: '.',
-    build: {
-        assetsDir: 'static',
-        outDir: 'dist'
-    },
-    resolve: {
-        alias: [
-            {
-                find: '@marzneshin',
-                replacement: path.resolve(__dirname, './src/'),
-            },
+export default defineConfig(({ mode }) => {
+    const dev = mode === "development";
+
+    let latestVersion = process.env.VITE_LATEST_APP_VERSION;
+
+    if (!latestVersion) {
+        try {
+            latestVersion = dev ?
+                execSync("/usr/bin/git describe --tags").toString().trimEnd() :
+                execSync("/usr/bin/git describe --tags --abbrev=0").toString().trimEnd();
+        } catch (e) {
+            console.error("Git versioning failed:", e);
+            latestVersion = "unknown";
+        }
+        process.env.VITE_LATEST_APP_VERSION = latestVersion;
+    }
+
+    return {
+        plugins: [
+            TanStackRouterVite(),
+            tsconfigPaths(),
+            react()
         ],
-    },
+        root: '.',
+        build: {
+            assetsDir: 'static',
+            outDir: 'dist'
+        },
+        resolve: {
+            alias: [
+                {
+                    find: '@marzneshin',
+                    replacement: path.resolve(__dirname, './src/'),
+                },
+            ],
+        },
+    }
 })
