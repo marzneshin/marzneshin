e5e7d20dd366ab5484de548d53527643300ed0f7
Krr0ptioN <mardin-pop@protonmail.com>
2024-06-18 16:43:00 +0300

    feat(awaiting): incorperating skeleton and notfound component for component depends on query result

---
+++ dashboard/src/components/awaiting/index.tsx
@@ -0,0 +1,34 @@
+import { type FC } from "react";
+
+interface AwaitingProps {
+    isFetching: boolean;
+    isNotFound: boolean;
+    NotFound: JSX.Element;
+    Skeleton: JSX.Element;
+    Component: JSX.Element;
+}
+
+/**
+ * @param isFetching - returned from react-query
+ * which indicate the promise is still fetching.
+ * @example 
+ * const { data, isFetching} = useQuery();
+ *
+ * @param isNotFound - whether the data return is empty or not
+ * @param NotFound - Not found component
+ * @param Component - The actual component itself
+ * @param Skeleton - Skeleton structure which will be presented 
+ * when the data is still pending and fetching
+ *
+ */
+export const Awaiting: FC<AwaitingProps> = ({
+    isFetching, isNotFound, NotFound, Skeleton, Component
+}) => {
+    if (isFetching) {
+        return Skeleton
+    } else if (isNotFound) {
+        return NotFound
+    } else {
+        return Component
+    }
+}
