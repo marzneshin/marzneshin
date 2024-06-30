a7b6befe14b34514e5f93111476d7c4319022239
Krr0ptioN <mardin-pop@protonmail.com>
2024-06-30 03:07:43 +0300

    ci: addingg react compiler linter

--- dashboard/.eslintrc.cjs
+++ dashboard/.eslintrc.cjs
@@ -1,14 +1,15 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react-hooks/recommended', 'plugin:storybook/recommended'],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    '@typescript-eslint/unused-disable-directives': ['off'],
-    '@typescript-eslint/ban-ts-comment': ['off'],
-    '@typescript-eslint/no-explicit-any': ['off'],
-    'react-refresh/only-export-components': ['off'],
-  },
+    root: true,
+    env: { browser: true, es2020: true },
+    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react-hooks/recommended', 'plugin:storybook/recommended'],
+    ignorePatterns: ['dist', '.eslintrc.cjs'],
+    parser: '@typescript-eslint/parser',
+    plugins: ['eslint-plugin-react-compiler', 'react-refresh'],
+    rules: {
+        'react-compiler/react-compiler': "error",
+        '@typescript-eslint/unused-disable-directives': ['off'],
+        '@typescript-eslint/ban-ts-comment': ['off'],
+        '@typescript-eslint/no-explicit-any': ['off'],
+        'react-refresh/only-export-components': ['off'],
+    },
 };
