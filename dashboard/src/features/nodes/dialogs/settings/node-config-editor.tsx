1236ebc03bd3bc0fbe70e136334a96025d1fb102
Krr0ptioN <mardin-pop@protonmail.com>
2024-07-06 02:09:19 +0300

    refact(nodes-editor): changing nodes editor

--- dashboard/src/features/nodes/dialogs/settings/node-config-editor.tsx
+++ dashboard/src/features/nodes/dialogs/settings/node-config-editor.tsx
@@ -1,33 +1,19 @@
-
-import Editor from '@monaco-editor/react';
-import { useTheme } from "@marzneshin/features/theme-switch";
 import { useNodesSettings } from "./use-nodes-settings";
-import { Button } from '@marzneshin/components';
+import { Button, JsonEditor } from '@marzneshin/components';
 import { useTranslation } from 'react-i18next';
 import { NodeType } from '../..';
 
 export const NodeConfigEditor = ({ entity }: { entity: NodeType }) => {
     const { t } = useTranslation()
-    const { theme } = useTheme()
     const {
         payloadValidity,
         data,
         handleConfigSave,
         handleConfigChange,
-        handleEditorValidation
     } = useNodesSettings(entity)
 
     return (<>
-        <Editor
-            height="50vh"
-            className="rounded-sm border"
-            defaultLanguage="json"
-            theme={theme === "dark" ? "vs-dark" : "github"}
-            defaultValue={JSON.stringify(data, null, '\t')}
-            onChange={handleConfigChange}
-            onValidate={handleEditorValidation}
-
-        />
+        <JsonEditor json={data} onChange={handleConfigChange} />
         <Button
             className="w-full"
             variant={!payloadValidity ? "destructive" : "default"}
