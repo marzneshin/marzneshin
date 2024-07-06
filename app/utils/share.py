caf581b47a54b9ecb5040b81a652383c406d9323
dawsh <dawsh@duck.com>
2024-07-06 21:47:12 +0330

    fix(subscription): use the default shadowsocks method for now

--- app/utils/share.py
+++ app/utils/share.py
@@ -281,6 +281,7 @@ def generate_user_configs(
                 http_headers={
                     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.3"
                 },
+                shadowsocks_method="chacha20-ietf-poly1305",
             )
             if host.fragment:
                 data.fragment = True
