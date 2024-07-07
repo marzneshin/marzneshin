0778280ff2b64a76b785341eebfcb03fb192dcb9
Krr0ptioN <mardin-pop@protonmail.com>
2024-07-07 11:54:32 +0300

    fix: export path

--- dashboard/src/features/users/components/pills/index.ts
+++ dashboard/src/features/users/components/pills/index.ts
@@ -2,4 +2,4 @@ export * from "./user-enabled-pill";
 export * from "./user-activated-pill";
 export * from "./user-data-limit-reached-pill";
 export * from "./user-expire-strategy-pill";
-export * from "./user-enabled-pill";
+export * from "./user-expired-pill";
