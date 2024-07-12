fc5603f123c39eddae1a62f1e74a5610904fbf0c
Krr0ptioN <mardin-pop@protonmail.com>
2024-07-12 12:16:04 +0300

    refact(settings-dialog): hosts dialog adapt to new settings dialog

--- dashboard/src/features/hosts/dialogs/settings/index.tsx
+++ dashboard/src/features/hosts/dialogs/settings/index.tsx
@@ -1,9 +1,7 @@
 import { type FC, useEffect } from "react";
 import {
-    Sheet,
-    SheetContent,
-    SheetHeader,
-    SheetTitle,
+    SettingsDialogProps,
+    SettingsDialog,
     Table,
     TableBody,
     TableRowWithCell,
@@ -11,7 +9,7 @@ import {
 import { useTranslation } from "react-i18next"
 import { HostType } from "../.."
 
-interface HostsSettingsDialogProps {
+interface HostsSettingsDialogProps extends SettingsDialogProps {
     onOpenChange: (state: boolean) => void
     onClose: () => void
     open: boolean
@@ -26,25 +24,18 @@ export const HostSettingsDialog: FC<HostsSettingsDialogProps> = ({ onOpenChange,
     }, [open, onClose]);
 
     return (
-        <Sheet open={open} onOpenChange={onOpenChange}>
-            <SheetContent className="sm:min-w-full md:min-w-[700px]" >
-                <SheetHeader>
-                    <SheetTitle>
-                        {t('page.hosts.settings.title')}
-                    </SheetTitle>
-                </SheetHeader>
-                <Table>
-                    <TableBody>
-                        <TableRowWithCell label={t('remark')} value={entity.remark} />
-                        <TableRowWithCell label={t('address')} value={entity.address} />
-                        <TableRowWithCell label={t('port')} value={entity.port} />
-                        <TableRowWithCell label={t('host')} value={entity.host} />
-                        <TableRowWithCell label={t('path')} value={entity.path} />
-                        <TableRowWithCell label={t('sni')} value={entity.sni} />
-                        <TableRowWithCell label={t('alpn')} value={entity.alpn} />
-                    </TableBody>
-                </Table>
-            </SheetContent>
-        </Sheet>
+        <SettingsDialog open={open} onOpenChange={onOpenChange}>
+            <Table>
+                <TableBody>
+                    <TableRowWithCell label={t('remark')} value={entity.remark} />
+                    <TableRowWithCell label={t('address')} value={entity.address} />
+                    <TableRowWithCell label={t('port')} value={entity.port} />
+                    <TableRowWithCell label={t('host')} value={entity.host} />
+                    <TableRowWithCell label={t('path')} value={entity.path} />
+                    <TableRowWithCell label={t('sni')} value={entity.sni} />
+                    <TableRowWithCell label={t('alpn')} value={entity.alpn} />
+                </TableBody>
+            </Table>
+        </SettingsDialog>
     )
 }
