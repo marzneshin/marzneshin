577ea02b929c018a52918c8e0f45463c54b0da04
Krr0ptioN <mardin-pop@protonmail.com>
2024-07-18 14:34:29 +0300

    refact(github-repo): separating view from service

--- dashboard/src/features/github-repo/github-repo.tsx
+++ dashboard/src/features/github-repo/github-repo.tsx
@@ -5,9 +5,6 @@ import {
     CardDescription,
     Button,
 } from "@marzneshin/components"
-import {
-    useGithubRepoStatsQuery
-} from "./services";
 import { type FC } from "react";
 import {
     GithubIcon,
@@ -17,32 +14,35 @@ import { projectInfo } from "@marzneshin/utils";
 
 interface GithubRepoProps {
     variant?: "full" | "mini"
+    stargazers_count: number;
+    full_name?: string;
+    description?: string;
 }
 
-export const GithubRepo: FC<GithubRepoProps> = ({ variant = "full" }) => {
-    const { data: stats } = useGithubRepoStatsQuery()
+export const GithubRepo: FC<GithubRepoProps> = ({ variant = "full", stargazers_count, full_name, description }) => {
     return (
         <Button variant="secondary" className="bg-gray-800 border-0 dark:text-primary text-secondary dark:hover:text-secondary dark:hover:bg-primary hover:bg-secondary hover:text-primary p-2" asChild>
             <Card>
                 <a href={projectInfo.github} target="_blank">
                     <CardContent className="hstack size-fit p-0 gap-2 items-center">
                         <GithubIcon className="size-fit" />
-                        {variant === "full" ? (<div className="vstack items-start">
-                            <CardTitle className="font-mono text-xs hstack justify-between w-full">
-                                {stats.full_name}
-                                <div className="hstack gap-1 font-bold items-center text-xs">
-                                    <StarIcon className="size-3" />
-                                    {stats.stargazers_count}
-                                </div>
-                            </CardTitle>
-                            <CardDescription className="text-xs">
-                                {stats.description}
-                            </CardDescription>
-                        </div>
+                        {variant === "full" ? (
+                            <div className="vstack items-start">
+                                <CardTitle className="font-mono text-xs hstack justify-between w-full">
+                                    {full_name}
+                                    <div className="hstack gap-1 font-bold items-center text-xs">
+                                        <StarIcon className="size-3" />
+                                        {stargazers_count}
+                                    </div>
+                                </CardTitle>
+                                <CardDescription className="text-xs">
+                                    {description}
+                                </CardDescription>
+                            </div>
                         ) : (
                             <CardDescription className="hstack gap-1 font-bold items-center text-xs">
                                 <StarIcon className="size-3" />
-                                {stats.stargazers_count}
+                                {stargazers_count}
                             </CardDescription>
                         )}
                     </CardContent>
