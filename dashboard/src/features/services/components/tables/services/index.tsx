babc43f77b06b7f277adfcde434d01ff5d4e6cf6
Krr0ptioN <mardin-pop@protonmail.com>
2024-07-23 00:39:31 +0300

    refact(entity-table): adapt services table to new actions lists

--- dashboard/src/features/services/components/tables/services/index.tsx
+++ dashboard/src/features/services/components/tables/services/index.tsx
@@ -4,12 +4,35 @@ import {
   fetchServices,
   ServiceType
 } from "@marzneshin/features/services";
-import { columns } from "./columns";
+import { columns as columnsFn } from "./columns";
 import { EntityTable } from "@marzneshin/features/entity-table";
 import { useNavigate } from "@tanstack/react-router";
 
 export const ServicesTable: FC = () => {
   const navigate = useNavigate({ from: "/services" });
+  const onEdit = (entity: ServiceType) => {
+    navigate({
+      to: "/services/$serviceId/edit",
+      params: { serviceId: String(entity.id) },
+    })
+  }
+
+  const onDelete = (entity: ServiceType) => {
+    navigate({
+      to: "/services/$serviceId/delete",
+      params: { serviceId: String(entity.id) },
+    })
+  }
+
+  const onOpen = (entity: ServiceType) => {
+    navigate({
+      to: "/services/$serviceId",
+      params: { serviceId: String(entity.id) },
+    })
+  }
+
+  const columns = columnsFn({ onEdit, onDelete, onOpen });
+
   return (
     <EntityTable
       fetchEntity={fetchServices}
@@ -17,24 +40,7 @@ export const ServicesTable: FC = () => {
       filteredColumn="name"
       entityKey={ServicesQueryFetchKey}
       onCreate={() => navigate({ to: "/services/create" })}
-      onOpen={(entity: ServiceType) =>
-        navigate({
-          to: "/services/$serviceId",
-          params: { serviceId: String(entity.id) },
-        })
-      }
-      onEdit={(entity: ServiceType) =>
-        navigate({
-          to: "/services/$serviceId/edit",
-          params: { serviceId: String(entity.id) },
-        })
-      }
-      onDelete={(entity: ServiceType) =>
-        navigate({
-          to: "/services/$serviceId/delete",
-          params: { serviceId: String(entity.id) },
-        })
-      }
+      onOpen={onOpen}
     />
   );
 };
