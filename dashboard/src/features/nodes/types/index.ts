b7bebe887af7bee430a68cce4b484f20679ec18f
Krr0ptioN <mardin-pop@protonmail.com>
2024-07-22 20:23:12 +0300

    refact(entity-table): adapt nodes table to new actions lists

--- dashboard/src/features/nodes/types/index.ts
+++ dashboard/src/features/nodes/types/index.ts
@@ -3,43 +3,43 @@ import { PowerOff, Zap, ZapOff } from "lucide-react";
 import { z } from "zod";
 
 export const NodesStatus = {
-  healthy: {
-    label: "healthy",
-    icon: Zap,
-  },
-  unhealthy: {
-    label: "unhealthy",
-    icon: ZapOff,
-  },
-  disabled: {
-    label: "disabled",
-    icon: PowerOff,
-  },
-  none: {
-    label: "none",
-    icon: null,
-  },
+    healthy: {
+        label: "healthy",
+        icon: Zap,
+    },
+    unhealthy: {
+        label: "unhealthy",
+        icon: ZapOff,
+    },
+    disabled: {
+        label: "disabled",
+        icon: PowerOff,
+    },
+    none: {
+        label: "none",
+        icon: null,
+    },
 } as Record<string, StatusType>;
 
 export const NodeSchema = z.object({
-  name: z.string().min(1),
-  address: z.string().min(1),
-  port: z
-    .number()
-    .min(1)
-    .or(z.string().transform((v) => Number.parseFloat(v))),
-  id: z.number().nullable().optional(),
-  status: z.enum([
-    NodesStatus.healthy.label,
-    NodesStatus.unhealthy.label,
-    "none",
-    NodesStatus.disabled.label,
-  ]),
-  usage_coefficient: z
-    .number()
-    .default(1.0)
-    .or(z.string().transform((v) => Number.parseFloat(v))),
-  connection_backend: z.enum(["grpclib", "grpcio"]).default("grpclib"),
+    name: z.string().min(1),
+    address: z.string().min(1),
+    port: z
+        .number()
+        .min(1)
+        .or(z.string().transform((v) => Number.parseFloat(v))),
+    id: z.number().nullable().optional(),
+    status: z.enum([
+        NodesStatus.healthy.label,
+        NodesStatus.unhealthy.label,
+        "none",
+        NodesStatus.disabled.label,
+    ]),
+    usage_coefficient: z
+        .number()
+        .default(1.0)
+        .or(z.string().transform((v) => Number.parseFloat(v))),
+    connection_backend: z.enum(["grpclib", "grpcio"]).default("grpclib"),
 });
 
-export type NodeType = z.infer<typeof NodeSchema>;
+export type NodeType = z.infer<typeof NodeSchema> & { id: number };
