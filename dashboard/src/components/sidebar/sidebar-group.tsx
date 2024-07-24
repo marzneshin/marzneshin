1345beda822322a7b93f0d18c46caac070c78871
Mardin <54506351+Krr0ptioN@users.noreply.github.com>
2024-07-24 15:19:11 +0300

    style(dashboard-sidebar): uppercase sidebar group items (#461)

--- dashboard/src/components/sidebar/sidebar-group.tsx
+++ dashboard/src/components/sidebar/sidebar-group.tsx
@@ -1,19 +1,20 @@
 
 import { FC, PropsWithChildren } from "react"
 import { Label } from "@marzneshin/components"
+import { cn } from "@marzneshin/utils"
 import { useSidebarContext } from "./sidebar-provider";
 
 export interface SidebarGroupProps
     extends PropsWithChildren {
-
+    className: string
 }
 
-export const SidebarGroup: FC<SidebarGroupProps> = ({ children }) => {
+export const SidebarGroup: FC<SidebarGroupProps> = ({ children, className }) => {
     const { collapsed } = useSidebarContext();
 
     if (!collapsed)
         return (
-            <Label className="text-gray-500 font-header">
+            <Label className={cn(className, "text-gray-500 font-header")}>
                 {children}
             </Label>
         )
