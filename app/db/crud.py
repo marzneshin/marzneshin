c0dcf6e7426986cb164cb138ec7cc75b7033fb2b
dawsh <dawsh@duck.com>
2024-07-30 09:14:04 +0330

    fix(hosts): update host allow_insecure

--- app/db/crud.py
+++ app/db/crud.py
@@ -186,6 +186,7 @@ def update_host(db: Session, db_host: InboundHost, host: InboundHostModify):
     db_host.fragment = host.fragment.model_dump() if host.fragment else None
     db_host.mux = host.mux
     db_host.is_disabled = host.is_disabled
+    db_host.allowinsecure = host.allowinsecure
     db.commit()
     db.refresh(db_host)
     return db_host
