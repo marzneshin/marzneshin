0828dafe09156fb960906a5183d37bba5623f0c7
dawsh <dawsh@duck.com>
2024-08-01 15:24:16 +0330

    refactor: remove other psutils usages

--- app/tasks/__init__.py
+++ app/tasks/__init__.py
@@ -1,10 +1,8 @@
-import logging
 from .nodes import nodes_startup
 from .record_usages import record_user_usages
 from .reset_user_data_usage import reset_user_data_usage
 from .review_users import review_users
 from .send_notifications import delete_expired_reminders, send_notifications
-from .store_bandwidth import record_realtime_bandwidth
 
 __all__ = [
     "nodes_startup",
@@ -13,6 +11,4 @@ __all__ = [
     "review_users",
     "delete_expired_reminders",
     "send_notifications",
-    "store_bandwidth",
-    "record_realtime_bandwidth",
 ]
