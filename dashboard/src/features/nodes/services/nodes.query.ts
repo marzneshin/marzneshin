c41a9c40d4fdfd67d80c5941ed7a0932fb1c1c3c
Krr0ptioN <mardin-pop@protonmail.com>
2024-08-01 16:06:31 +0300

    feat(multi-backend-node): node backend specification for nodes service

--- dashboard/src/features/nodes/services/nodes.query.ts
+++ dashboard/src/features/nodes/services/nodes.query.ts
@@ -1,13 +1,17 @@
-import { NodeType } from "@marzneshin/features/nodes";
+import type { NodeType } from "@marzneshin/features/nodes";
 import { useQuery } from "@tanstack/react-query";
 import { fetch } from "@marzneshin/utils";
-import {
+import type {
     EntityQueryKeyType,
-    UseEntityQueryProps
+    UseEntityQueryProps,
 } from "@marzneshin/features/entity-table";
 
-export async function fetchNodes({ queryKey }: EntityQueryKeyType): Promise<{ entity: NodeType[], pageCount: number }> {
-    return fetch(`/nodes?page=${queryKey[1]}&size=${queryKey[2]}&name=${queryKey[3]}`).then((result) => {
+export async function fetchNodes({
+    queryKey,
+}: EntityQueryKeyType): Promise<{ entity: NodeType[]; pageCount: number }> {
+    return fetch(
+        `/nodes?page=${queryKey[1]}&size=${queryKey[2]}&name=${queryKey[3]}`,
+    ).then((result) => {
         return {
             entity: result.items,
             pageCount: result.pages,
@@ -17,11 +21,10 @@ export async function fetchNodes({ queryKey }: EntityQueryKeyType): Promise<{ en
 
 export const NodesQueryFetchKey = "nodes";
 
-
 export const useNodesQuery = ({ page, size, search }: UseEntityQueryProps) => {
     return useQuery({
         queryKey: [NodesQueryFetchKey, page, size, search ? search : ""],
         queryFn: fetchNodes,
         initialData: { entity: [], pageCount: 0 },
-    })
-}
+    });
+};
