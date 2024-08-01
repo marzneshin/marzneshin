90830015d2918923cf3a63faea83ae95707de717
Krr0ptioN <mardin-pop@protonmail.com>
2024-08-01 16:10:27 +0300

    feat(multi-backend-node): node backend specification for nodes config mutation

--- dashboard/src/features/nodes/services/settings.mutate.ts
+++ dashboard/src/features/nodes/services/settings.mutate.ts
@@ -1,4 +1,4 @@
-import { NodeType } from "@marzneshin/features/nodes";
+import type { NodeType } from "@marzneshin/features/nodes";
 import { useMutation } from "@tanstack/react-query";
 import { fetch, queryClient } from "@marzneshin/utils";
 import { toast } from "sonner";
@@ -6,41 +6,44 @@ import i18n from "@marzneshin/features/i18n";
 import { NodesSettingsQueryFetchKey } from "./settings.query";
 
 interface UpdateNodesSettings {
-    node: NodeType
-    config: string
+  node: NodeType;
+  backend: string;
+  config: string;
 }
 
-export async function fetchUpdateNodesSettings({ node, config }: UpdateNodesSettings): Promise<NodeType> {
-    return fetch(`/nodes/${node.id}/xray_config`, { method: 'put', body: config }).then((node) => {
-        return node;
-    });
+export async function fetchUpdateNodesSettings({
+  node,
+  backend,
+  config,
+}: UpdateNodesSettings): Promise<NodeType> {
+  return fetch(`/nodes/${node.id}/${backend}/config`, {
+    method: "put",
+    body: config,
+  }).then((node) => {
+    return node;
+  });
 }
 
 const handleError = (error: Error, value: UpdateNodesSettings) => {
-    toast.error(
-        i18n.t('events.update.error', { name: value.node.name }),
-        {
-            description: error.message
-        })
-}
+  toast.error(i18n.t("events.update.error", { name: value.node.name }), {
+    description: error.message,
+  });
+};
 
 const handleSuccess = (value: NodeType) => {
-    toast.success(
-        i18n.t('events.update.success.title', { name: value.name }),
-        {
-            description: i18n.t('events.update.success.desc')
-        })
-    queryClient.invalidateQueries({ queryKey: [NodesSettingsQueryFetchKey] })
-}
+  toast.success(i18n.t("events.update.success.title", { name: value.name }), {
+    description: i18n.t("events.update.success.desc"),
+  });
+  queryClient.invalidateQueries({ queryKey: [NodesSettingsQueryFetchKey] });
+};
 
-
-const NodesSettingsUpdateFetchKey = ["nodes", "xray_config"];
+const NodesSettingsUpdateFetchKey = ["nodes", "config"];
 
 export const useNodesSettingsMutation = () => {
-    return useMutation({
-        mutationKey: NodesSettingsUpdateFetchKey,
-        mutationFn: fetchUpdateNodesSettings,
-        onError: handleError,
-        onSuccess: handleSuccess,
-    })
-}
+  return useMutation({
+    mutationKey: NodesSettingsUpdateFetchKey,
+    mutationFn: fetchUpdateNodesSettings,
+    onError: handleError,
+    onSuccess: handleSuccess,
+  });
+};
