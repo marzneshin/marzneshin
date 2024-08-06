45f3625c9472ecf05735ff906b6cf7c9e60badfb
Krr0ptioN <mardin-pop@protonmail.com>
2024-08-06 23:56:52 +0300

    fix(node-backends): 5 second backend stats refetching

--- dashboard/src/features/nodes/services/backend-stats.query.ts
+++ dashboard/src/features/nodes/services/backend-stats.query.ts
@@ -2,28 +2,34 @@ import type { NodeType } from "@marzneshin/features/nodes";
 import { useQuery } from "@tanstack/react-query";
 import { fetch } from "@marzneshin/utils";
 
-type NodesSettingsQueryKey = [string, number, string, string];
+type NodeBackendStatsQueryKey = [string, number, string, string];
 
-interface NodesSettingsQuery {
-    queryKey: NodesSettingsQueryKey;
+interface NodeBackendStatsQuery {
+  queryKey: NodeBackendStatsQueryKey;
 }
 
 export async function fetchNodeBackendStats({
-    queryKey,
-}: NodesSettingsQuery): Promise<{
-    running: boolean;
+  queryKey,
+}: NodeBackendStatsQuery): Promise<{
+  running: boolean;
 } | null> {
-    return fetch(`/nodes/${Number(queryKey[1])}/${queryKey[2]}/stats`)
-        .then((stats) => stats)
-        .catch(() => null);
+  return fetch(`/nodes/${Number(queryKey[1])}/${queryKey[2]}/stats`)
+    .then((stats) => stats)
+    .catch(() => null);
 }
 
 export const useBackendStatsQuery = (node: NodeType, backend: string) => {
-    const queryKey: NodesSettingsQueryKey = ["nodes", node.id, backend, "stats"];
+  const queryKey: NodeBackendStatsQueryKey = [
+    "nodes",
+    node.id,
+    backend,
+    "stats",
+  ];
 
-    return useQuery({
-        queryKey,
-        queryFn: fetchNodeBackendStats,
-        initialData: null,
-    });
+  return useQuery({
+    queryKey,
+    queryFn: fetchNodeBackendStats,
+    refetchInterval: 1000 * 5,
+    initialData: null,
+  });
 };
