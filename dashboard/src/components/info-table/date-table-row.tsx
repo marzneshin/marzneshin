187b0592fc7c7a866930a0c48413816919b64fa4
Krr0ptioN <mardin-pop@protonmail.com>
2024-08-07 22:59:55 +0300

    feat(ui-lib): date table row support time

--- dashboard/src/components/info-table/date-table-row.tsx
+++ dashboard/src/components/info-table/date-table-row.tsx
@@ -1,14 +1,15 @@
-import { FC } from "react";
+import type { FC } from "react";
 import { TableRowWithCell } from "./table-row-with-cell";
 import { format, isValid } from "date-fns";
 
-export const DateTableRow: FC<{ label: string; date?: Date | null | string }> = ({
-    label,
-    date,
-}) => {
+export const DateTableRow: FC<{
+    label: string;
+    withTime?: boolean;
+    date?: Date | null | string;
+}> = ({ label, date, withTime = false }) => {
     let formattedDate = "";
     if (date && isValid(new Date(date))) {
-        formattedDate = format(date, "PPP");
+        formattedDate = format(date, withTime ? "Ppp" : "P");
     }
     return <TableRowWithCell label={label} value={formattedDate} />;
 };
