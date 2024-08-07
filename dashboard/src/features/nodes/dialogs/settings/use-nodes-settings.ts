be62cd594fac3e9b33bb1d987e40871383b26098
Krr0ptioN <mardin-pop@protonmail.com>
2024-08-07 13:12:30 +0300

    refact(backend-config-language): a function to the detect the language

--- dashboard/src/features/nodes/dialogs/settings/use-nodes-settings.ts
+++ dashboard/src/features/nodes/dialogs/settings/use-nodes-settings.ts
@@ -1,62 +1,65 @@
 import { useState } from "react";
 import YAML from "yaml";
 import {
-  type NodeType,
-  NodeBackendSettingConfigFormat,
-  useNodesSettingsMutation,
-  useNodesSettingsQuery,
+    type NodeType,
+    NodeBackendSettingConfigFormat,
+    useNodesSettingsMutation,
+    useNodesSettingsQuery,
 } from "../..";
 
 const parseConfig = (data: {
-  config: string;
-  format: NodeBackendSettingConfigFormat;
+    config: string;
+    format: NodeBackendSettingConfigFormat;
 }) => {
-  return {
-    [NodeBackendSettingConfigFormat.PLAIN]: () => data.config,
-    [NodeBackendSettingConfigFormat.JSON]: () =>
-      JSON.stringify(JSON.parse(data.config), null, "\t"),
-    [NodeBackendSettingConfigFormat.YAML]: () =>
-      YAML.stringify(YAML.parse(data.config), null, "\t"),
-  }[data.format]();
+    return {
+        [NodeBackendSettingConfigFormat.PLAIN]: () => data.config,
+        [NodeBackendSettingConfigFormat.JSON]: () =>
+            JSON.stringify(JSON.parse(data.config), null, "    "),
+        [NodeBackendSettingConfigFormat.YAML]: () =>
+            YAML.stringify(YAML.parse(data.config), null, "    "),
+    }[data.format]();
 };
 
+const parseLanguage = (format: NodeBackendSettingConfigFormat) =>
+    ({
+        [NodeBackendSettingConfigFormat.PLAIN]: "text",
+        [NodeBackendSettingConfigFormat.JSON]: "json",
+        [NodeBackendSettingConfigFormat.YAML]: "yaml",
+    })[format];
+
 export const useNodesSettings = (entity: NodeType, backend: string) => {
-  const { data, isFetching } = useNodesSettingsQuery(entity, backend);
-  const [config, setConfig] = useState<string>(data.config);
-  const [payloadValidity, setPayloadValidity] = useState<boolean>(true);
-  const mutate = useNodesSettingsMutation();
-  const language = {
-    [NodeBackendSettingConfigFormat.PLAIN]: "text",
-    [NodeBackendSettingConfigFormat.JSON]: "json",
-    [NodeBackendSettingConfigFormat.YAML]: "yaml",
-  }[data.format];
+    const { data, isFetching } = useNodesSettingsQuery(entity, backend);
+    const [config, setConfig] = useState<string>(data.config);
+    const [payloadValidity, setPayloadValidity] = useState<boolean>(true);
+    const mutate = useNodesSettingsMutation();
+    const language = parseLanguage(data.format);
 
-  const handleEditorValidation = (markers: any[]) => {
-    setPayloadValidity(!markers.length);
-  };
+    const handleEditorValidation = (markers: any[]) => {
+        setPayloadValidity(!markers.length);
+    };
 
-  const handleConfigSave = () => {
-    mutate.mutate({ node: entity, backend, config, format: data.format });
-  };
+    const handleConfigSave = () => {
+        mutate.mutate({ node: entity, backend, config, format: data.format });
+    };
 
-  const handleConfigChange = (newConfig: string | undefined) => {
-    if (newConfig) {
-      try {
-        setConfig(parseConfig({ config: newConfig, format: data.format }));
-        setPayloadValidity(true);
-      } catch (error) {
-        setPayloadValidity(false);
-      }
-    }
-  };
+    const handleConfigChange = (newConfig: string | undefined) => {
+        if (newConfig) {
+            try {
+                setConfig(parseConfig({ config: newConfig, format: data.format }));
+                setPayloadValidity(true);
+            } catch (error) {
+                setPayloadValidity(false);
+            }
+        }
+    };
 
-  return {
-    payloadValidity,
-    language,
-    isFetching,
-    config: parseConfig(data),
-    handleConfigSave,
-    handleConfigChange,
-    handleEditorValidation,
-  };
+    return {
+        payloadValidity,
+        language,
+        isFetching,
+        config: parseConfig(data),
+        handleConfigSave,
+        handleConfigChange,
+        handleEditorValidation,
+    };
 };
