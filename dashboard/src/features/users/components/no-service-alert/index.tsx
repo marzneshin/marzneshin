65bc4ac77fc8ba488f2e4456ac5e6563b8ffb5f9
Krr0ptioN <mardin-pop@protonmail.com>
2024-08-12 14:47:39 +0300

    refact(users-page-service-alert): using alert card for users page no service alert

--- dashboard/src/features/users/components/no-service-alert/index.tsx
+++ dashboard/src/features/users/components/no-service-alert/index.tsx
@@ -1,25 +1,23 @@
 import { useServicesQuery } from '@marzneshin/features/services';
-import { ExclamationTriangleIcon } from '@radix-ui/react-icons';
 import { Link } from '@tanstack/react-router'
 import { useTranslation } from 'react-i18next';
 import {
-    Alert,
-    AlertDescription,
-    AlertTitle,
+    AlertCard,
 } from '@marzneshin/components';
 
 export const UsersNoServiceAlert = () => {
     const { t } = useTranslation();
     const { data } = useServicesQuery({ page: 1, size: 10 });
     return (data && data.pageCount === 0) && (
-        <Alert>
-            <ExclamationTriangleIcon className="mr-2" />
-            <AlertTitle className="font-semibold text-primary">{t('page.users.services-alert.title')}</AlertTitle>
-            <AlertDescription>
-                {t('page.users.services-alert.desc')}
-                <Link className="m-1 font-semibold text-secondary-foreground" to="/services">{t('page.nodes.certificate-alert.click')}</Link>
-            </AlertDescription>
-        </Alert>
+        <AlertCard
+            title={t('page.users.services-alert.title')}
+            desc={
+                <>
+                    {t('page.users.services-alert.desc')}
+                    <Link className="m-1 font-semibold text-secondary-foreground" to="/services">{t('page.nodes.certificate-alert.click')}</Link>
+                </>
+            }
+        />
     )
 }
 
