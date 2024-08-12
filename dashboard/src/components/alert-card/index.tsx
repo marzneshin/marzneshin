56b916ea88dc5da1067f13c4ec53bd2a9ac1622e
Mardin <54506351+Krr0ptioN@users.noreply.github.com>
2024-08-12 15:59:48 +0300

    improve(nodes-settings): show an alert when there is no backend configured for the node (#499)
    * feat(ui-lib): alert card
    
    * improve(nodes-settings): show an alert when there is no backend configured for the node
    
    * chore: lint
    
    * improve(nodes-settings): english translation for backend alert
    
    * fix: title i18n token missing 'settings'
    
    * improve(alert-card): accepting elements and components
    
    * refact(users-page-service-alert): using alert card for users page no service alert
    
    * improve(alert-card): variants for card
    
    * refact(nodes-page-certificate-alert): using alert card
    
    * chore(translations): add no backend alert in farsi
    
    ---------
    
    Co-authored-by: dawsh <dawsh@duck.com>

---
+++ dashboard/src/components/alert-card/index.tsx
@@ -0,0 +1,20 @@
+import { Info, TriangleAlert, OctagonAlert } from 'lucide-react';
+import {
+    Alert,
+    AlertDescription,
+    AlertTitle,
+} from '@marzneshin/components';
+
+export const AlertCard = ({ title, desc, variant = "default" }: { variant?: "warning" | "default" | "destructive", title: string | JSX.Element, desc: string | JSX.Element }) => {
+    const Icon = variant === "warning" ? TriangleAlert : (variant === "destructive" ? OctagonAlert : Info)
+    return (
+        <Alert variant={variant}>
+            <Icon className="mr-2" />
+            <AlertTitle className="font-semibold">{title}</AlertTitle>
+            <AlertDescription>
+                {desc}
+            </AlertDescription>
+        </Alert>
+    )
+}
+
