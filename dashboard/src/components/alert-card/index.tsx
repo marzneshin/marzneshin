b6af91f98b80a7bdff7cb6187ba893ccd03a76f9
Krr0ptioN <mardin-pop@protonmail.com>
2024-08-12 15:26:23 +0300

    improve(alert-card): variants for card

--- dashboard/src/components/alert-card/index.tsx
+++ dashboard/src/components/alert-card/index.tsx
@@ -1,15 +1,16 @@
-import { ExclamationTriangleIcon } from '@radix-ui/react-icons';
+import { Info, TriangleAlert, OctagonAlert } from 'lucide-react';
 import {
     Alert,
     AlertDescription,
     AlertTitle,
 } from '@marzneshin/components';
 
-export const AlertCard = ({ title, desc }: { title: string | JSX.Element, desc: string | JSX.Element }) => {
+export const AlertCard = ({ title, desc, variant = "default" }: { variant?: "warning" | "default" | "destructive", title: string | JSX.Element, desc: string | JSX.Element }) => {
+    const Icon = variant === "warning" ? TriangleAlert : (variant === "destructive" ? OctagonAlert : Info)
     return (
-        <Alert>
-            <ExclamationTriangleIcon className="mr-2" />
-            <AlertTitle className="font-semibold text-primary">{title}</AlertTitle>
+        <Alert variant={variant}>
+            <Icon className="mr-2" />
+            <AlertTitle className="font-semibold">{title}</AlertTitle>
             <AlertDescription>
                 {desc}
             </AlertDescription>
--- dashboard/src/features/nodes/dialogs/settings/index.tsx
+++ dashboard/src/features/nodes/dialogs/settings/index.tsx
@@ -1,11 +1,11 @@
 import {
-  type SettingsDialogProps,
-  SettingsDialog,
-  Tabs,
-  TabsContent,
-  TabsList,
-  TabsTrigger,
-  AlertCard,
+    type SettingsDialogProps,
+    SettingsDialog,
+    Tabs,
+    TabsContent,
+    TabsList,
+    TabsTrigger,
+    AlertCard,
 } from "@marzneshin/components";
 import type { FC } from "react";
 import { useTranslation } from "react-i18next";
@@ -14,58 +14,59 @@ import { NodesDetailTable } from "../../tables/detail-table";
 import { NodeBackendSetting } from "./node-backend-setting";
 
 interface NodesSettingsDialogProps extends SettingsDialogProps {
-  entity: NodeType;
-  onClose: () => void;
+    entity: NodeType;
+    onClose: () => void;
 }
 
 export const NodesSettingsDialog: FC<NodesSettingsDialogProps> = ({
-  onOpenChange,
-  open,
-  entity,
-  onClose,
+    onOpenChange,
+    open,
+    entity,
+    onClose,
 }) => {
-  const { t } = useTranslation();
+    const { t } = useTranslation();
 
-  return (
-    <SettingsDialog open={open} onClose={onClose} onOpenChange={onOpenChange}>
-      <div className="my-4">
-        <h1 className="font-medium font-header">
-          {t("page.nodes.settings.detail")}
-        </h1>
-        <NodesDetailTable node={entity} />
-      </div>
-      {entity.backends.length === 0 ? (
-        <AlertCard
-          desc={t('page.nodes.settings.no-backend-alert.desc')}
-          title={t('page.nodes.settings.no-backend-alert.title')}
-        />
-      ) : (
-        <Tabs
-          className="my-3 w-full h-full"
-          defaultValue={entity.backends[0].name}
-        >
-          <TabsList className="w-full">
-            {...entity.backends.map((backend: NodeBackendType) => (
-              <TabsTrigger
-                className="capitalize w-full"
-                value={backend.name}
-                key={backend.name}
-              >
-                {backend.name}
-              </TabsTrigger>
-            ))}
-          </TabsList>
-          {...entity.backends.map((backend: NodeBackendType) => (
-            <TabsContent
-              className="w-full"
-              value={backend.name}
-              key={backend.name}
-            >
-              <NodeBackendSetting node={entity} backend={backend.name} />
-            </TabsContent>
-          ))}
-        </Tabs>
-      )}
-    </SettingsDialog>
-  );
+    return (
+        <SettingsDialog open={open} onClose={onClose} onOpenChange={onOpenChange}>
+            <div className="my-4">
+                <h1 className="font-medium font-header">
+                    {t("page.nodes.settings.detail")}
+                </h1>
+                <NodesDetailTable node={entity} />
+            </div>
+            {entity.backends.length === 0 ? (
+                <AlertCard
+                    variant="warning"
+                    desc={t('page.nodes.settings.no-backend-alert.desc')}
+                    title={t('page.nodes.settings.no-backend-alert.title')}
+                />
+            ) : (
+                <Tabs
+                    className="my-3 w-full h-full"
+                    defaultValue={entity.backends[0].name}
+                >
+                    <TabsList className="w-full">
+                        {...entity.backends.map((backend: NodeBackendType) => (
+                            <TabsTrigger
+                                className="capitalize w-full"
+                                value={backend.name}
+                                key={backend.name}
+                            >
+                                {backend.name}
+                            </TabsTrigger>
+                        ))}
+                    </TabsList>
+                    {...entity.backends.map((backend: NodeBackendType) => (
+                        <TabsContent
+                            className="w-full"
+                            value={backend.name}
+                            key={backend.name}
+                        >
+                            <NodeBackendSetting node={entity} backend={backend.name} />
+                        </TabsContent>
+                    ))}
+                </Tabs>
+            )}
+        </SettingsDialog>
+    );
 };
