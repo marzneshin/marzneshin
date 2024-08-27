b0ee9ce14f04ac9af0c0ac8c09f9756eba46818c
Krr0ptioN <mardin-pop@protonmail.com>
2024-08-27 04:44:47 +0300

    fix: undefined filters

--- dashboard/src/features/entity-table/hooks/use-filtering.ts
+++ dashboard/src/features/entity-table/hooks/use-filtering.ts
@@ -2,11 +2,11 @@ import { OnChangeFn } from "@tanstack/react-table"
 import { useState } from "react"
 
 export interface UseFiltersReturn {
-    columnsFilter: Record<string, string>
-    setColumnsFilter: OnChangeFn<Record<string, string>>
+    columnsFilter: Record<string, string | undefined>
+    setColumnsFilter: OnChangeFn<Record<string, string | undefined>>
 }
 
 export const useFilters = (): UseFiltersReturn => {
-    const [columnsFilter, setColumnsFilter] = useState<Record<string, string>>({})
+    const [columnsFilter, setColumnsFilter] = useState<Record<string, string | undefined>>({})
     return { columnsFilter, setColumnsFilter }
 }
