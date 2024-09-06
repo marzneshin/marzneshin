015292f59b5c0f9ed57ed0fc009bfcae4a189ed8
Krr0ptioN <mardin-pop@protonmail.com>
2024-09-06 08:56:53 +0300

    refact(selectable-tables): user-services and service inbounds table

--- dashboard/src/features/services/components/tables/inbounds/index.tsx
+++ dashboard/src/features/services/components/tables/inbounds/index.tsx
@@ -1,11 +1,10 @@
-import { type FC, useCallback, useEffect, useState } from "react";
-import { useInboundsQuery } from "@marzneshin/features/inbounds";
+import { type FC, useCallback, useState } from "react";
+import { fetchInbounds } from "@marzneshin/features/inbounds";
 import { Button } from "@marzneshin/components";
-import { DataTable } from "@marzneshin/features/entity-table";
+import { SelectableEntityTable, useRowSelection } from "@marzneshin/features/entity-table";
 import { columns } from "./columns";
 import { type ServiceType, useServicesUpdateMutation } from "@marzneshin/features/services";
 import { useTranslation } from "react-i18next";
-import type { RowSelectionState } from "@tanstack/react-table";
 
 interface ServiceInboundsTableProps {
     service: ServiceType;
@@ -15,45 +14,29 @@ export const ServiceInboundsTable: FC<ServiceInboundsTableProps> = ({
     service,
 }) => {
     const { mutate: updateService } = useServicesUpdateMutation();
-    const { data } = useInboundsQuery();
-    const [selectedInbound, setSelectedInbound] = useState<{
-        [key: number]: boolean;
-    }>({});
+    const rowSelection = useRowSelection({});
+    const [selectedInbound, setSelectedInbound] = useState<number[]>([]);
     const { t } = useTranslation();
 
-    useEffect(() => {
-        setSelectedInbound((prevSelected) => {
-            const updatedSelected: RowSelectionState = { ...prevSelected };
-            for (const inboundId of service.inbound_ids) {
-                for (const [i, fetchedInbound] of data.entries()) {
-                    if (fetchedInbound.id === inboundId) {
-                        updatedSelected[i] = true;
-                    }
-                }
-            }
-            return updatedSelected;
-        });
-    }, [data, service.inbound_ids]);
-
     const handleApply = useCallback(() => {
-        const selectedInboundIds = Object.keys(selectedInbound)
-            .filter((key) => selectedInbound[Number.parseInt(key)])
-            .map((key) => data[Number.parseInt(key)].id);
-        updateService({ ...service, inbound_ids: selectedInboundIds });
-    }, [data, selectedInbound, service, updateService]);
+        updateService({ ...service, inbound_ids: selectedInbound });
+    }, [selectedInbound, service, updateService]);
 
     const disabled = Object.keys(selectedInbound).length < 1;
 
     return (
         <div className="flex flex-col gap-4">
-            <DataTable
+            <SelectableEntityTable
+                fetchEntity={fetchInbounds}
                 columns={columns}
-                data={data}
                 filteredColumn="tag"
-                selectedRow={selectedInbound}
-                setSelectedRow={setSelectedInbound}
+                parentEntity={service}
+                rowIdentifier="id"
+                rowSelection={rowSelection}
+                parentEntity={service}
+                parentEntityRelationName="inbound_ids"
+                setSelectedEntities={setSelectedInbound}
             />
-
             <Button onClick={handleApply} disabled={disabled}>
                 {t("apply")}
             </Button>
--- dashboard/src/features/users/components/tables/services/index.tsx
+++ dashboard/src/features/users/components/tables/services/index.tsx
@@ -1,59 +1,41 @@
-import { type FC, useCallback, useEffect, useState } from "react";
+import { type FC, useCallback, useState } from "react";
 import { Button } from "@marzneshin/components";
-import { DataTable } from "@marzneshin/features/entity-table";
+import { SelectableEntityTable, useRowSelection } from "@marzneshin/features/entity-table";
 import { columns } from "./columns";
 import {
     type UserType,
     useUsersUpdateMutation,
 } from "@marzneshin/features/users";
 import { useTranslation } from "react-i18next";
-import type { RowSelectionState } from "@tanstack/react-table";
-import { useServicesQuery } from "@marzneshin/features/services";
+import { fetchServices } from "@marzneshin/features/services";
 
 interface UserServicesTableProps {
     user: UserType;
 }
 
-// TODO: #322 Implement pagination for row selectable with appliedable action table
 export const UserServicesTable: FC<UserServicesTableProps> = ({ user }) => {
     const { mutate: updateUser } = useUsersUpdateMutation();
-    const { data } = useServicesQuery({ page: 1, size: 100 });
-    const [selectedService, setSelectedService] = useState<{
-        [key: number]: boolean;
-    }>({});
+    const rowSelection = useRowSelection({});
+    const [selectedService, setSelectedService] = useState<number[]>([]);
     const { t } = useTranslation();
 
-    useEffect(() => {
-        setSelectedService((prevSelected) => {
-            const updatedSelected: RowSelectionState = { ...prevSelected };
-            for (const serviceId of user.service_ids) {
-                for (const [i, fetchedService] of data.entity.entries()) {
-                    if (fetchedService.id === serviceId) {
-                        updatedSelected[i] = true;
-                    }
-                }
-            }
-            return updatedSelected;
-        });
-    }, [data, user.service_ids]);
-
     const handleApply = useCallback(() => {
-        const selectedServiceIds = Object.keys(selectedService)
-            .filter((key) => selectedService[Number.parseInt(key)])
-            .map((key) => data.entity[Number.parseInt(key)].id);
-        updateUser({ ...user, service_ids: selectedServiceIds });
-    }, [data, selectedService, user, updateUser]);
+        updateUser({ ...user, service_ids: selectedService });
+    }, [selectedService, user, updateUser]);
 
-    const disabled = Object.keys(selectedService).length < 1;
+    const disabled = Object.keys(rowSelection.selectedRow).length < 1;
 
     return (
         <div className="flex flex-col gap-4">
-            <DataTable
+            <SelectableEntityTable
+                fetchEntity={fetchServices}
                 columns={columns}
-                data={data.entity}
                 filteredColumn="name"
-                selectedRow={selectedService}
-                setSelectedRow={setSelectedService}
+                parentEntity={user}
+                rowIdentifier="id"
+                rowSelection={rowSelection}
+                parentEntityRelationName="service_ids"
+                setSelectedEntities={setSelectedService}
             />
 
             <Button onClick={handleApply} disabled={disabled}>
