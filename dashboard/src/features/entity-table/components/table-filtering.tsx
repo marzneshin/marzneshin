7e6e9d7a10ad09abc23d004e5c049d609c1d7db8
Krr0ptioN <mardin-pop@protonmail.com>
2024-09-30 23:01:24 +0300

    perf(entity-table/filtering): debouncing the column filtering

--- dashboard/src/features/entity-table/components/table-filtering.tsx
+++ dashboard/src/features/entity-table/components/table-filtering.tsx
@@ -5,17 +5,22 @@ import {
     useEntityTableContext
 } from "@marzneshin/features/entity-table/contexts";
 import { useTranslation } from "react-i18next";
+import { useDebouncedCallback } from 'use-debounce';
 
 interface TableSearchProps { }
 
 export const TableSearch: FC<TableSearchProps> = () => {
     const { primaryFilter } = useEntityTableContext()
+    const setColumnFilters = useDebouncedCallback(
+        primaryFilter.setColumnFilters,
+        5000
+    );
     const { t } = useTranslation()
     return (
         <Input
             placeholder={t('table.filter-placeholder', { name: primaryFilter.column })}
             value={primaryFilter.columnFilters}
-            onChange={(e) => primaryFilter.setColumnFilters(e.target.value)}
+            onChange={(e) => setColumnFilters(e.target.value)}
             className=" w-full md:w-[20rem]"
         />
     );
