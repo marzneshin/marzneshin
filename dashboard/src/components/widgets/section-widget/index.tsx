713aa35edd317e82e69243e3244266bffba8284e
Krr0ptioN <mardin-pop@protonmail.com>
2024-10-10 16:28:45 +0300

    feat(ui-lib/widgets): adding description and header line to section widget

--- dashboard/src/components/widgets/section-widget/index.tsx
+++ dashboard/src/components/widgets/section-widget/index.tsx
@@ -1,6 +1,7 @@
 import {
     Card,
     CardContent,
+    CardDescription,
     CardFooter,
     CardHeader,
     CardTitle,
@@ -10,17 +11,31 @@ import type { FC, PropsWithChildren } from 'react';
 
 export interface SectionWidgetProps extends PropsWithChildren {
     title: JSX.Element | string;
+    description: JSX.Element | string;
     content?: JSX.Element | string;
     footer?: JSX.Element | string;
+    options?: JSX.Element | string;
 }
 
-export const SectionWidget: FC<SectionWidgetProps> = ({ footer, content, children, title }) => {
+export const SectionWidget: FC<SectionWidgetProps> = ({
+    options, footer, content, children, title, description
+}) => {
     return (
         <Card>
-            <CardHeader className="p-4">
-                <CardTitle className="flex flex-row justify-start items-center gap-3 text-xl">
-                    {title}
-                </CardTitle>
+            <CardHeader className="flex flex-col items-stretch space-y-0 border-b p-0 sm:flex-row">
+                <div className="flex flex-1 flex-col justify-center gap-1 px-6 py-5 sm:py-6">
+                    <CardTitle className="flex flex-row justify-start items-center gap-3 text-xl">
+                        {title}
+                    </CardTitle>
+                    <CardDescription>
+                        {description}
+                    </CardDescription>
+                </div>
+                {options &&
+                    <div className="flex items-center flex-col justify-center gap-1 px-6 py-5 sm:py-6">
+                        {options}
+                    </div>
+                }
             </CardHeader>
             <Separator />
             <CardContent className="p-4">
