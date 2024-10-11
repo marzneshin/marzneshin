5e14894bd6f644f36a9432f9926249b8863c1883
Mehdi <m.mahdim10@yahoo.com>
2024-10-11 12:31:29 +0330

    feat(notification): add functionality for ReachedUsagePercent and ReachedDaysLeft notifications

--- app/tasks/__init__.py
+++ app/tasks/__init__.py
@@ -2,10 +2,12 @@ from .nodes import nodes_startup
 from .record_usages import record_user_usages
 from .reset_user_data_usage import reset_user_data_usage
 from .review_users import review_users
+from .expire_days_reached import expire_days_reached
 
 __all__ = [
     "nodes_startup",
     "record_user_usages",
     "reset_user_data_usage",
     "review_users",
+    "expire_days_reached",
 ]
