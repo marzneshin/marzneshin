4c55563a4ab2b811c004880abbc0dea2c91f9050
Krr0ptioN <mardin-pop@protonmail.com>
2024-10-15 14:28:21 +0300

    fix(user-nodes-stats): skeleton loading and fixing over fetching

--- dashboard/src/modules/users/components/widgets/user-nodes-usage-widget/hooks/use-from-now-interval.ts
+++ dashboard/src/modules/users/components/widgets/user-nodes-usage-widget/hooks/use-from-now-interval.ts
@@ -3,8 +3,9 @@ import { ChartDateInterval } from "../types";
 export function useFromNowInterval(interval: ChartDateInterval) {
     const start = new Date();
     const end = new Date();
-
     start.setDate(end.getDate() - parseInt(interval.replace("d", "")));
+    end.setMinutes(0, 0, 0);
+    start.setMinutes(0, 0, 0);
     return {
         start: start.toISOString(),
         end: end.toISOString()
--- dashboard/src/modules/users/components/widgets/user-nodes-usage-widget/index.tsx
+++ dashboard/src/modules/users/components/widgets/user-nodes-usage-widget/index.tsx
@@ -2,114 +2,134 @@ import { FC } from 'react';
 import { useTranslation } from 'react-i18next';
 import {
     SectionWidget,
+    ChartLegendContent,
     ChartContainer,
+    Awaiting,
+    ChartLegend,
     ChartTooltip,
     ChartTooltipContent,
 } from "@marzneshin/components";
-import { UserNodeUsagesResponse, useUserNodeUsagesQuery } from "@marzneshin/modules/users";
-import mockData from "./mock-data.json";
 import { Area, AreaChart, CartesianGrid, YAxis, XAxis } from "recharts"
 import { useState } from "react";
 import { SelectDateView } from "./select-date-view";
-import { UserNodesUsageWidgetProps } from "./types";
-import { useChartConfig, useTransformData } from "./hooks";
+import { ChartDateInterval, UserNodesUsageWidgetProps } from "./types";
+import { useChartConfig, useTransformData, useFromNowInterval } from "./hooks";
 import { format as formatByte } from '@chbphone55/pretty-bytes';
-
+import { useUserNodeUsagesQuery } from "@marzneshin/modules/users";
+import { Tooltip } from "./tooltip"
+import { UsageGraphSkeleton } from "./skeleton"
 
 export const UserNodesUsageWidget: FC<UserNodesUsageWidgetProps> = ({
     user,
 }) => {
     const { t } = useTranslation();
     const [timeRange, setTimeRange] = useState("90d")
-    // const { start, end } = useStartEndDateFromNow(timeRange as ChartDateInterval);
-    // const { data, isLoading } = useUserNodeUsagesQuery({ username: user.username, start, end })
-    const chartData = useTransformData(mockData);
-    const config = useChartConfig(mockData);
+    const { start, end } = useFromNowInterval(timeRange as ChartDateInterval);
+    const { data, isPending } = useUserNodeUsagesQuery({ username: user.username, start, end })
+    const chartData = useTransformData(data);
+    const config = useChartConfig(data);
 
     return (
-        <SectionWidget
-            title={t("page.users.settings.nodes-usage.title")}
-            description={t("page.users.settings.nodes-usage.desc")}
-            footer={<SelectDateView timeRange={timeRange} setTimeRange={setTimeRange} />}
-        >
-            <ChartContainer
-                className="aspect-auto h-[250px] w-full"
-                config={config}>
-                <AreaChart
-                    accessibilityLayer
-                    data={chartData}
-                    margin={{
-                        left: 12,
-                        right: 12,
-                    }}
+        <Awaiting
+            Component={
+                <SectionWidget
+                    title={t("page.users.settings.nodes-usage.title")}
+                    description={t("page.users.settings.nodes-usage.desc")}
+                    footer={<SelectDateView timeRange={timeRange} setTimeRange={setTimeRange} />}
                 >
-                    <CartesianGrid vertical={false} />
-                    <YAxis
-                        tickLine={false}
-                        axisLine={false}
-                        tickMargin={8}
-                        tickFormatter={(value) => {
-                            const [amount, metric] = formatByte(value)
-                            return `${amount} ${metric}`
-                        }}
-                    />
-                    <XAxis
-                        dataKey="datetime"
-                        tickLine={false}
-                        axisLine={false}
-                        tickMargin={8}
-                        tickFormatter={(value) => {
-                            const date = new Date(value)
-                            const format = timeRange === "90d" ? {
-                                day: "numeric",
-                                month: "short"
-                            } : ((timeRange === "30d") ? { day: "numeric" } : { day: "numeric", hour: "numeric" })
-                            return date.toLocaleDateString("en-US", format)
-                        }}
-                    />
-                    <ChartTooltip
-                        cursor={false}
-                        content={
-                            <ChartTooltipContent
-                                indicator='line'
-                                labelFormatter={(value) => {
-                                    return new Date(value).toLocaleDateString("en-US", {
-                                        month: "short",
-                                        day: "numeric",
-                                        year: "numeric",
-                                    })
+                    <ChartContainer
+                        className="aspect-auto h-[250px] w-full"
+                        config={config}>
+                        <AreaChart
+                            accessibilityLayer
+                            data={chartData}
+                            margin={{
+                                left: 12,
+                                right: 12,
+                            }}
+                        >
+                            <CartesianGrid vertical={false} />
+                            <YAxis
+                                tickLine={false}
+                                axisLine={false}
+                                tickMargin={8}
+                                tickFormatter={(value) => {
+                                    const [amount, metric] = formatByte(value)
+                                    return `${amount} ${metric}`
                                 }}
                             />
-                        }
-                    />
-                    <defs>
-                        {mockData.node_usages.map(node =>
-                            <linearGradient id={node.node_name} x1="0" y1="0" x2="0" y2="1">
-                                <stop
-                                    offset="5%"
-                                    stopColor={config[node.node_name].color}
-                                    stopOpacity={0.9}
+
+                            <XAxis
+                                dataKey="datetime"
+                                tickLine={false}
+                                axisLine={false}
+                                tickMargin={8}
+                                tickFormatter={(value) => {
+                                    const date = new Date(value);
+                                    const format: Intl.DateTimeFormatOptions = timeRange === "90d" ? {
+                                        day: "numeric",
+                                        month: "short",
+                                    } : (timeRange === "30d" ? {
+                                        day: "numeric",
+                                    } : {
+                                        day: "numeric",
+                                        hour: "numeric",
+                                    });
+                                    return date.toLocaleDateString("en-US", format);
+                                }}
+                            />
+                            <ChartTooltip
+                                cursor={false}
+                                content={
+                                    <ChartTooltipContent
+                                        indicator='line'
+                                        labelFormatter={(value) => {
+                                            return new Date(value).toLocaleDateString("en-US", {
+                                                month: "short",
+                                                day: "numeric",
+                                                year: "numeric",
+                                            })
+                                        }}
+                                        valueFormatter={(value) => {
+                                            const [amount, metric] = formatByte(value as number)
+                                            return `${amount} ${metric}`
+                                        }}
+                                    />
+                                }
+                            />
+                            <defs>
+                                {data.node_usages.map(node =>
+                                    <linearGradient id={node.node_name} x1="0" y1="0" x2="0" y2="1">
+                                        <stop
+                                            offset="5%"
+                                            stopColor={config[node.node_name].color}
+                                            stopOpacity={0.1}
+                                        />
+                                        <stop
+                                            offset="95%"
+                                            stopColor={config[node.node_name].color}
+                                            stopOpacity={0.8}
+                                        />
+                                    </linearGradient>
+                                )}
+                            </defs>
+                            {data.node_usages.map(node =>
+                                <Area
+                                    dataKey={node.node_name}
+                                    type="natural"
+                                    fill={config[node.node_name].color}
+                                    fillOpacity={0.4}
+                                    stackId={node.node_id}
+                                    stroke={config[node.node_name].color}
                                 />
-                                <stop
-                                    offset="95%"
-                                    stopColor={config[node.node_name].color}
-                                    stopOpacity={0.1}
-                                />
-                            </linearGradient>
-                        )}
-                    </defs>
-                    {mockData.node_usages.map(node =>
-                        <Area
-                            dataKey={node.node_name}
-                            type="natural"
-                            fill={config[node.node_name].color}
-                            fillOpacity={0.4}
-                            stackId={node.node_id}
-                            stroke={config[node.node_name].color}
-                        />
-                    )}
-                </AreaChart>
-            </ChartContainer>
-        </SectionWidget>
+                            )}
+                        </AreaChart>
+                        <ChartLegend content={<ChartLegendContent />} />
+                    </ChartContainer>
+                </SectionWidget>
+            }
+            Skeleton={<UsageGraphSkeleton />}
+            isFetching={isPending}
+        />
     );
 };
