b41515779546363ffa6a1340c309daed69699bf8
Krr0ptioN <mardin-pop@protonmail.com>
2024-10-15 14:27:50 +0300

    improve(user-nodes-stats): coloring

--- dashboard/src/modules/users/components/widgets/user-nodes-usage-widget/hooks/use-chart-config.ts
+++ dashboard/src/modules/users/components/widgets/user-nodes-usage-widget/hooks/use-chart-config.ts
@@ -3,7 +3,7 @@ import {
 } from "@marzneshin/components";
 import { UserNodeUsagesResponse } from "@marzneshin/modules/users";
 import { interpolateColors } from "@marzneshin/utils";
-import { interpolateRainbow } from "d3";
+import { interpolateRdBu } from "d3";
 
 export const useChartConfig = (nodesUsage: UserNodeUsagesResponse) => {
     const numberOfNodes = nodesUsage.node_usages.length;
@@ -17,7 +17,7 @@ export const useChartConfig = (nodesUsage: UserNodeUsagesResponse) => {
         colorEnd: 1,
         useEndAsStart: false,
     };
-    const colors = interpolateColors(numberOfNodes, interpolateRainbow, colorRangeInfo);
+    const colors = interpolateColors(numberOfNodes, interpolateRdBu, colorRangeInfo);
     nodesUsage.node_usages.forEach((node, i) => {
         config[node.node_name] = { label: node.node_name, color: colors[i] };
     })
