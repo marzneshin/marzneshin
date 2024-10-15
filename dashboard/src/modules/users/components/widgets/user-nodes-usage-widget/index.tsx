c0cdf577044ad7bcf792cea7e2229638e940aac7
Krr0ptioN <mardin-pop@protonmail.com>
2024-10-15 15:24:49 +0300

    fix: misplacement of legend

--- dashboard/src/modules/users/components/widgets/user-nodes-usage-widget/index.tsx
+++ dashboard/src/modules/users/components/widgets/user-nodes-usage-widget/index.tsx
@@ -39,94 +39,92 @@ export const UserNodesUsageWidget: FC<UserNodesUsageWidgetProps> = ({
                     <ChartContainer
                         className="aspect-auto h-[250px] w-full"
                         config={config}>
-                        <>
-                            <AreaChart
-                                accessibilityLayer
-                                data={chartData}
-                                margin={{
-                                    left: 12,
-                                    right: 12,
+                        <AreaChart
+                            accessibilityLayer
+                            data={chartData}
+                            margin={{
+                                left: 12,
+                                right: 12,
+                            }}
+                        >
+                            <CartesianGrid vertical={false} />
+                            <YAxis
+                                tickLine={false}
+                                axisLine={false}
+                                tickMargin={8}
+                                tickFormatter={(value) => {
+                                    const [amount, metric] = formatByte(value)
+                                    return `${amount} ${metric}`
                                 }}
-                            >
-                                <CartesianGrid vertical={false} />
-                                <YAxis
-                                    tickLine={false}
-                                    axisLine={false}
-                                    tickMargin={8}
-                                    tickFormatter={(value) => {
-                                        const [amount, metric] = formatByte(value)
-                                        return `${amount} ${metric}`
-                                    }}
-                                />
+                            />
 
-                                <XAxis
-                                    dataKey="datetime"
-                                    tickLine={false}
-                                    axisLine={false}
-                                    tickMargin={8}
-                                    tickFormatter={(value) => {
-                                        const date = new Date(value);
-                                        const format: Intl.DateTimeFormatOptions = timeRange === "90d" ? {
-                                            day: "numeric",
-                                            month: "short",
-                                        } : (timeRange === "30d" ? {
-                                            day: "numeric",
-                                        } : {
-                                            day: "numeric",
-                                            hour: "numeric",
-                                        });
-                                        return date.toLocaleDateString("en-US", format);
-                                    }}
-                                />
-                                <ChartTooltip
-                                    cursor={false}
-                                    content={
-                                        <ChartTooltipContent
-                                            indicator='line'
-                                            labelFormatter={(value) => {
-                                                return new Date(value).toLocaleDateString("en-US", {
-                                                    month: "short",
-                                                    day: "numeric",
-                                                    year: "numeric",
-                                                })
-                                            }}
-                                            valueFormatter={(value) => {
-                                                const [amount, metric] = formatByte(value as number)
-                                                return `${amount} ${metric}`
-                                            }}
-                                        />
-                                    }
-                                />
-                                <defs>
-                                    {data.node_usages.map(node =>
-                                        <linearGradient key={node.node_name} id={node.node_name} x1="0" y1="0" x2="0" y2="1">
-                                            <stop
-                                                offset="5%"
-                                                stopColor={config[node.node_name].color}
-                                                stopOpacity={0.1}
-                                            />
-                                            <stop
-                                                offset="95%"
-                                                stopColor={config[node.node_name].color}
-                                                stopOpacity={0.8}
-                                            />
-                                        </linearGradient>
-                                    )}
-                                </defs>
-                                {data.node_usages.map(node =>
-                                    <Area
-                                        dataKey={node.node_name}
-                                        key={node.node_name}
-                                        type="natural"
-                                        fill={config[node.node_name].color}
-                                        fillOpacity={0.4}
-                                        stackId={node.node_id}
-                                        stroke={config[node.node_name].color}
+                            <XAxis
+                                dataKey="datetime"
+                                tickLine={false}
+                                axisLine={false}
+                                tickMargin={8}
+                                tickFormatter={(value) => {
+                                    const date = new Date(value);
+                                    const format: Intl.DateTimeFormatOptions = timeRange === "90d" ? {
+                                        day: "numeric",
+                                        month: "short",
+                                    } : (timeRange === "30d" ? {
+                                        day: "numeric",
+                                    } : {
+                                        day: "numeric",
+                                        hour: "numeric",
+                                    });
+                                    return date.toLocaleDateString("en-US", format);
+                                }}
+                            />
+                            <ChartTooltip
+                                cursor={false}
+                                content={
+                                    <ChartTooltipContent
+                                        indicator='line'
+                                        labelFormatter={(value) => {
+                                            return new Date(value).toLocaleDateString("en-US", {
+                                                month: "short",
+                                                day: "numeric",
+                                                year: "numeric",
+                                            })
+                                        }}
+                                        valueFormatter={(value) => {
+                                            const [amount, metric] = formatByte(value as number)
+                                            return `${amount} ${metric}`
+                                        }}
                                     />
+                                }
+                            />
+                            <defs>
+                                {data.node_usages.map(node =>
+                                    <linearGradient key={node.node_name} id={node.node_name} x1="0" y1="0" x2="0" y2="1">
+                                        <stop
+                                            offset="5%"
+                                            stopColor={config[node.node_name].color}
+                                            stopOpacity={0.1}
+                                        />
+                                        <stop
+                                            offset="95%"
+                                            stopColor={config[node.node_name].color}
+                                            stopOpacity={0.8}
+                                        />
+                                    </linearGradient>
                                 )}
-                            </AreaChart>
+                            </defs>
+                            {data.node_usages.map(node =>
+                                <Area
+                                    dataKey={node.node_name}
+                                    key={node.node_name}
+                                    type="natural"
+                                    fill={config[node.node_name].color}
+                                    fillOpacity={0.4}
+                                    stackId={node.node_id}
+                                    stroke={config[node.node_name].color}
+                                />
+                            )}
                             <ChartLegend content={<ChartLegendContent />} />
-                        </>
+                        </AreaChart>
                     </ChartContainer>
                 </SectionWidget>
             }
