9a04d6fa9c06f95588e1919be588f07ac99195d5
Krr0ptioN <mardin-pop@protonmail.com>
2024-11-02 08:12:58 +0300

    fix(hosts/settings): type checking for the new settings

--- dashboard/src/modules/hosts/dialogs/settings/index.tsx
+++ dashboard/src/modules/hosts/dialogs/settings/index.tsx
@@ -7,9 +7,17 @@ import {
     TableRowWithCell,
 } from "@marzneshin/components";
 import { useTranslation } from "react-i18next";
-import {
-    type HostWithProfileSchemaType
-} from "@marzneshin/modules/hosts";
+
+interface HostWithProfileSchemaType {
+    remark: string;
+    address: string;
+    weight?: number | null;
+    port?: string | number | null;
+    host?: string;
+    path?: string;
+    sni?: string;
+    alpn?: string;
+}
 
 interface HostsSettingsDialogProps extends SettingsDialogProps {
     onOpenChange: (state: boolean) => void;
@@ -32,11 +40,26 @@ export const HostSettingsDialog: FC<HostsSettingsDialogProps> = ({
                 <TableBody>
                     <TableRowWithCell label={t("remark")} value={entity.remark} />
                     <TableRowWithCell label={t("address")} value={entity.address} />
-                    <TableRowWithCell label={t("port")} value={entity.port} />
-                    <TableRowWithCell label={t("host")} value={entity.host ?? ""} />
-                    <TableRowWithCell label={t("path")} value={entity.path ?? ""} />
-                    <TableRowWithCell label={t("sni")} value={entity.sni ?? ""} />
-                    <TableRowWithCell label={t("alpn")} value={entity.alpn ?? ""} />
+                    <TableRowWithCell 
+                        label={t("port")} 
+                        value={entity.port?.toString() ?? ""} 
+                    />
+                    <TableRowWithCell 
+                        label={t("host")} 
+                        value={entity.host ?? ""} 
+                    />
+                    <TableRowWithCell 
+                        label={t("path")} 
+                        value={entity.path ?? ""} 
+                    />
+                    <TableRowWithCell 
+                        label={t("sni")} 
+                        value={entity.sni ?? ""} 
+                    />
+                    <TableRowWithCell 
+                        label={t("alpn")} 
+                        value={entity.alpn ?? ""} 
+                    />
                 </TableBody>
             </Table>
         </SettingsDialog>
