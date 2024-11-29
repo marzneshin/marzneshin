a6ea1582f155b17bedbe68afe97a57fac533539f
dawsh <dawsh@duck.com>
2024-11-29 22:23:39 +0330

    set and pass shadowtls version from inbound config

--- app/utils/share.py
+++ app/utils/share.py
@@ -317,6 +317,7 @@ def generate_user_configs(
                     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.3"
                 },
                 shadowsocks_method="chacha20-ietf-poly1305",
+                shadowtls_version=inbound.get("shadowtls_version"),
                 weight=host.weight,
             )
             if host.fragment:
