a99ce80e783d41017915aba96e78aa58c679e020
dawsh <dawsh@duck.com>
2024-11-29 22:10:05 +0330

    feat(inbounds): add shadowtls

---
+++ app/db/migrations/versions/20241129_a015ed8ce227_add_shadowtls_protocol.py
@@ -0,0 +1,39 @@
+"""add shadowtls protocol
+
+Revision ID: a015ed8ce227
+Revises: be0032100c07
+Create Date: 2024-11-29 21:41:06.896405
+
+"""
+
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = "a015ed8ce227"
+down_revision = "be0032100c07"
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    bind = op.get_bind()
+    dialect = bind.dialect.name
+
+    if dialect == "postgresql":
+        op.execute("ALTER TYPE proxytypes ADD VALUE 'Shadowsocks2022'")
+        op.execute("ALTER TYPE proxytypes ADD VALUE 'WireGuard'")
+        op.execute("ALTER TYPE proxytypes ADD VALUE 'TUIC'")
+    elif dialect in ("mariadb", "mysql"):
+        op.execute(
+            "ALTER TABLE inbounds MODIFY protocol ENUM('VMess', 'VLESS', 'Trojan', 'Shadowsocks', 'Shadowsocks2022', 'Hysteria2', 'WireGuard', 'TUIC', 'ShadowTLS')"
+        )
+
+
+def downgrade() -> None:
+    bind = op.get_bind()
+    dialect = bind.dialect.name
+
+    if dialect in ("mysql", "mariadb"):
+        op.execute(
+            "ALTER TABLE inbounds MODIFY protocol ENUM('VMess', 'VLESS', 'Trojan', 'Shadowsocks', 'Shadowsocks2022', 'Hysteria2', 'WireGuard', 'TUIC')"
+        )
