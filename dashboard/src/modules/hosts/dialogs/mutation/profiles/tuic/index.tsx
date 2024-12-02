5cf55b3f6d1707d43c8410e07a2a5c6dd5ba08b6
Mardin <54506351+Krr0ptioN@users.noreply.github.com>
2024-12-02 11:34:35 +0300

    fix(tuic-hosts): wrapping the tuic profile fields in accordian (#686)
    * chore: upgrading packages
    
    * fix(hosts/tuic-profile): wrapping the tls fields in accordian

--- dashboard/src/modules/hosts/dialogs/mutation/profiles/tuic/index.tsx
+++ dashboard/src/modules/hosts/dialogs/mutation/profiles/tuic/index.tsx
@@ -4,13 +4,18 @@ import {
 import {
     TlsFields
 } from "../../sections";
+import {
+    Accordion,
+} from "@marzneshin/common/components";
 
 export const TuicProfileFields = () => {
     return (
-        <>
+        <div className="space-y-2">
             <CommonFields />
-            <TlsFields />
-        </>
+            <Accordion className="space-y-2" type="single" collapsible>
+                <TlsFields />
+            </Accordion>
+        </div>
     )
 }
 
