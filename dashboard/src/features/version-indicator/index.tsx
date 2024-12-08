e2b77be79532a59b6c725b41a32b1e5412c31042
Krr0ptioN <mardin-pop@protonmail.com>
2024-12-08 18:57:58 +0300

    improve(version-indicator): removing the marzneshin name from version indicator

--- dashboard/src/features/version-indicator/index.tsx
+++ dashboard/src/features/version-indicator/index.tsx
@@ -4,7 +4,7 @@ export const VersionIndicator = () => {
     return (
         <div className="size-full flex justify-center items-center dark:text-neutral-300 text-neutral-800">
             <Badge variant="outline" className="border-none">
-                Marzneshin {import.meta.env.VITE_LATEST_APP_VERSION}
+                {import.meta.env.VITE_LATEST_APP_VERSION}
             </Badge>
         </div>
     )
