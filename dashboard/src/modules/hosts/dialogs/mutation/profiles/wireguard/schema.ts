4d0417bff9cebce6f65e1e35c6fbdcd421d86c7b
Mardin <54506351+Krr0ptioN@users.noreply.github.com>
2024-12-09 12:13:11 +0300

    fix(hosts/schema): correcting hosts schema for port (#687)
    * fix(hosts/schema): correcting hosts schema for port
    
    * fix(hosts/is-disabled): using the common host schema for is disabled
    
    * fix(hosts/schema): union with optional and int hosts schema for port
    
    * fix: changing to merge
    
    * Squash merge master into fix/hysteria-profile-port-type
    
    * fix(schema): missing default values for profiles
    
    * fix(hosts/port-field): preprocess the value of the field and check for emptiness

--- dashboard/src/modules/hosts/dialogs/mutation/profiles/wireguard/schema.ts
+++ dashboard/src/modules/hosts/dialogs/mutation/profiles/wireguard/schema.ts
@@ -1,9 +1,8 @@
 import { z } from "zod";
 import { HostSchema } from "@marzneshin/modules/hosts";
 
-export const WireguardSchema = HostSchema.extend({
+export const WireguardSchema = HostSchema.merge(z.object({
     path: z.string().nullable().optional(),
-    is_disabled: z.boolean(),
     allowed_ips: z.string().nullable().optional(),
     mtu: z.coerce
         .number()
@@ -14,6 +13,6 @@ export const WireguardSchema = HostSchema.extend({
         .string()
         .nullable()
         .optional(),
-});
+}));
 
 export type WireguardSchemaType = z.infer<typeof WireguardSchema>;
