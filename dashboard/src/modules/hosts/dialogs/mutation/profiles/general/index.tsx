f11eb74b30a7c7d7853b8c2a7d6b4e539145ffe2
dawsh <dawsh@duck.com>
2024-12-19 21:25:41 +0330

    allow user to explicitly set path, host and sni to null; and add splithttp fields

--- dashboard/src/modules/hosts/dialogs/mutation/profiles/general/index.tsx
+++ dashboard/src/modules/hosts/dialogs/mutation/profiles/general/index.tsx
@@ -1,36 +1,35 @@
 import {
     CommonFields,
+    FragmentField,
     HostField,
     MuxField,
-    FragmentField,
     PathField,
     SecurityFields,
+    SplitHttpField,
 } from "../../fields";
-import {
-    Accordion,
-    HStack
-} from "@marzneshin/common/components";
-import { SettingSection } from "@marzneshin/modules/hosts"
-import { useTranslation } from "react-i18next";
+import {Accordion, HStack} from "@marzneshin/common/components";
+import {SettingSection} from "@marzneshin/modules/hosts"
+import {useTranslation} from "react-i18next";
 
 export const GeneralProfileFields = () => {
-    const { t } = useTranslation();
+    const {t} = useTranslation();
     return (
         <div className="space-y-2">
-            <CommonFields />
+            <CommonFields/>
             <Accordion className="space-y-2" type="single" collapsible>
                 <SettingSection value="network" triggerText={t("page.hosts.network-settings")}>
                     <HStack>
-                        <HostField />
-                        <PathField />
+                        <HostField/>
+                        <PathField/>
                     </HStack>
+                    <SplitHttpField/>
                 </SettingSection>
                 <SettingSection value="camouflage" triggerText={t("page.hosts.camouflage-settings")}>
-                    <FragmentField />
-                    <MuxField />
+                    <FragmentField/>
+                    <MuxField/>
                 </SettingSection>
                 <SettingSection value="security" triggerText={t("page.hosts.security-settings")}>
-                    <SecurityFields />
+                    <SecurityFields/>
                 </SettingSection>
             </Accordion>
         </div>
