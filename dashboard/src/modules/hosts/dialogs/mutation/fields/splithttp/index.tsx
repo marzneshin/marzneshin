88e8a52bc5b80fe06b354d8193e6d3c7a7a322a4
Krr0ptioN <mardin-pop@protonmail.com>
2024-12-22 14:47:27 +0300

    refact(hosts/split-http): moving xmux to its own component

--- dashboard/src/modules/hosts/dialogs/mutation/fields/splithttp/index.tsx
+++ dashboard/src/modules/hosts/dialogs/mutation/fields/splithttp/index.tsx
@@ -1,24 +1,21 @@
 import {
     CheckboxField,
-    FormControl,
     FormField,
     FormItem,
     FormLabel,
-    Input,
     Switch,
 } from "@marzneshin/common/components";
 import { useFormContext, useWatch } from "react-hook-form";
 import { useTranslation } from "react-i18next";
 import { SplitHttpModeField } from "./mode";
 import { SplitHttpPaddingBytesField } from "./padding-bytes";
+import { SplitHttpXmuxFields } from "./xmux";
 
 export const SplitHttpFields = () => {
     const { t } = useTranslation();
     const form = useFormContext();
     const split_http = useWatch({ name: "splithttp_settings" });
-    const xmux = useWatch({ name: "splithttp_settings.xmux" });
     const isSplitHttpDisabled = split_http === null;
-    const isXMuxDisabled = xmux === null;
 
     const updateFieldValue = (path: string, value: any) => {
         form.setValue(path, value === "" ? null : value, {
@@ -60,58 +57,7 @@ export const SplitHttpFields = () => {
                                     label={t("No GRPC Header")}
                                 />
                             </div>
-
-                            <FormLabel className="flex items-center justify-between">
-                                {t("page.hosts.xmux-settings")}
-                                <Switch
-                                    checked={!isSplitHttpDisabled}
-                                    onCheckedChange={(checked) =>
-                                        form.setValue(
-                                            "splithttp_settings.xmux",
-                                            checked ? { xmux: {} } : null,
-                                        )
-                                    }
-                                />
-                            </FormLabel>
-                            {!isXMuxDisabled && (
-                                <div className="grid grid-cols-2 gap-3">
-                                    {[
-                                        "max_concurrency",
-                                        "max_connections",
-                                        "max_reuse_times",
-                                        "max_lifetime",
-                                        "max_request_times",
-                                        "keep_alive_period",
-                                    ].map((fieldName) => (
-                                        <FormField
-                                            key={fieldName}
-                                            name={`splithttp_settings.xmux.${fieldName}`}
-                                            render={({ field }) => (
-                                                <FormItem>
-                                                    <FormLabel className="flex items-center justify-between capitalize">
-                                                        {t(
-                                                            `${fieldName.replace(/_/g, " ")}`,
-                                                        )}
-                                                    </FormLabel>
-                                                    <FormControl>
-                                                        <Input
-                                                            value={field.value}
-                                                            onChange={(e) =>
-                                                                updateFieldValue(
-                                                                    `splithttp_settings.xmux.${fieldName}`,
-                                                                    e.target
-                                                                        .value,
-                                                                )
-                                                            }
-                                                            className="w-full"
-                                                        />
-                                                    </FormControl>
-                                                </FormItem>
-                                            )}
-                                        />
-                                    ))}
-                                </div>
-                            )}
+                            <SplitHttpXmuxFields />
                         </>
                     )}
                 </FormItem>
---
+++ dashboard/src/modules/hosts/dialogs/mutation/fields/splithttp/xmux.tsx
@@ -0,0 +1,77 @@
+import {
+    FormField,
+    FormItem,
+    FormLabel,
+    FormControl,
+    Switch,
+    Input,
+} from "@marzneshin/common/components";
+import { useFormContext, useWatch } from "react-hook-form";
+import { useTranslation } from "react-i18next";
+
+export const SplitHttpXmuxFields = () => {
+    const { t } = useTranslation();
+    const form = useFormContext();
+    const xmux = useWatch({ name: "splithttp_settings.xmux" });
+    const isXMuxDisabled = xmux === null;
+
+    const updateFieldValue = (path: string, value: any) => {
+        form.setValue(path, value === "" ? null : value, {
+            shouldDirty: true,
+            shouldValidate: true,
+        });
+    };
+
+    return (
+        <div>
+            <FormLabel className="flex items-center justify-between">
+                {t("page.hosts.xmux-settings")}
+                <Switch
+                    checked={!isXMuxDisabled}
+                    onCheckedChange={(checked) =>
+                        form.setValue(
+                            "splithttp_settings.xmux",
+                            checked ? { xmux: {} } : null,
+                        )
+                    }
+                />
+            </FormLabel>
+            {!isXMuxDisabled && (
+                <div className="grid grid-cols-2 gap-3">
+                    {[
+                        "max_concurrency",
+                        "max_connections",
+                        "max_reuse_times",
+                        "max_lifetime",
+                        "max_request_times",
+                        "keep_alive_period",
+                    ].map((fieldName) => (
+                        <FormField
+                            key={fieldName}
+                            name={`splithttp_settings.xmux.${fieldName}`}
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel className="flex items-center justify-between capitalize">
+                                        {t(`${fieldName.replace(/_/g, " ")}`)}
+                                    </FormLabel>
+                                    <FormControl>
+                                        <Input
+                                            value={field.value}
+                                            onChange={(e) =>
+                                                updateFieldValue(
+                                                    `splithttp_settings.xmux.${fieldName}`,
+                                                    e.target.value,
+                                                )
+                                            }
+                                            className="w-full"
+                                        />
+                                    </FormControl>
+                                </FormItem>
+                            )}
+                        />
+                    ))}
+                </div>
+            )}
+        </div>
+    );
+};
