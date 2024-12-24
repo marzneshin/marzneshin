d9479268eb03267774cd5cc089d93db28fab03a6
erfjab <erfjab@gmail.com>
2024-12-24 04:45:08 +0330

    Style: formated text

--- tests/test_migrations.py
+++ tests/test_migrations.py
@@ -6,9 +6,11 @@ from pytest_alembic.tests import (
     test_upgrade,
 )
 
+
 @pytest.mark.tryfirst
 def test_model_definitions_match_ddl(alembic_engine, alembic_config):
-    if 'sqlite' in alembic_engine.url.drivername:
-        pytest.skip('Skipping DDL test for SQLite database')
+    if "sqlite" in alembic_engine.url.drivername:
+        pytest.skip("Skipping DDL test for SQLite database")
     from pytest_alembic.tests import test_model_definitions_match_ddl
+
     test_model_definitions_match_ddl(alembic_engine, alembic_config)
