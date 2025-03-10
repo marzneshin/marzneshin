deda4f747971f15f6121fa4ae8b8e5c01de22f1e
Mardin <mardin-pop@protonmail.com>
2025-03-10 22:54:48 +0300

    feat: use mobile

--- dashboard/src/common/hooks/index.ts
+++ dashboard/src/common/hooks/index.ts
@@ -1,4 +1,5 @@
 export * from './use-screen-breakpoint'
 export * from './use-dialog'
 export * from './use-mutation-dialog'
-export * from "./use-is-current-route"
+export * from './use-is-current-route'
+export * from './use-mobile'
---
+++ dashboard/src/common/hooks/use-mobile.ts
@@ -0,0 +1,17 @@
+import { useLayoutEffect, useState } from "react";
+import debounce from "lodash.debounce";
+
+export const useIsMobile = (): boolean => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useLayoutEffect(() => {
+    const updateSize = (): void => {
+      setIsMobile(window.innerWidth < 768);
+    };
+    window.addEventListener("resize", debounce(updateSize, 250));
+    // updateSize();
+    return (): void => window.removeEventListener("resize", updateSize);
+  }, []);
+
+  return isMobile;
+};
