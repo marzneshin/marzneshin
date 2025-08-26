200ad05e54ec339005e71736e5e995f489a46c83
ben-aslan <kiziltahaaslan@gmail.com>
2025-08-27 00:19:37 +0330

    feat(user): add remaining time and fix expire_date format for all locales

--- dashboard/src/features/i18n/index.ts
+++ dashboard/src/features/i18n/index.ts
@@ -1,4 +1,8 @@
 import dayjs from 'dayjs';
+import "dayjs/locale/fa";
+import "dayjs/locale/ru";
+import "dayjs/locale/zh-cn";
+import "dayjs/locale/ar";
 import i18n from 'i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import HttpApi from 'i18next-http-backend';
