2f0f26bf476b1f2e8a94b27c9794a4ad4f5ba478
erfjab <erfjab@gmail.com>
2025-10-03 15:26:34 +0330

    improve(endpoints)!: add api version

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -4,12 +4,12 @@ from . import admin, node, service, inbounds, subscription, system, user
 
 api_router = APIRouter()
 
-api_router.include_router(admin.router, prefix="/api")
-api_router.include_router(node.router, prefix="/api")
-api_router.include_router(service.router, prefix="/api")
-api_router.include_router(inbounds.router, prefix="/api")
+api_router.include_router(admin.router, prefix="/api/v0")
+api_router.include_router(node.router, prefix="/api/v0")
+api_router.include_router(service.router, prefix="/api/v0")
+api_router.include_router(inbounds.router, prefix="/api/v0")
 api_router.include_router(subscription.router)
-api_router.include_router(system.router, prefix="/api")
-api_router.include_router(user.router, prefix="/api")
+api_router.include_router(system.router, prefix="/api/v0")
+api_router.include_router(user.router, prefix="/api/v0")
 
 __all__ = ["api_router"]
