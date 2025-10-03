8731723a4d31cb47cb71629f7ecb93ee3ae8ff46
danial <danialhooshangi41@gmail.com>
2025-10-03 16:54:14 +0330

    feat: integrate Persian calendar in user date fields
    - Replace DateField with PersianDateField in user forms
    - Support Persian calendar for activation deadline selection
    - Support Persian calendar for user expire date selection
    - Automatic language-aware calendar switching (Persian/Gregorian)

--- dashboard/src/modules/users/components/dialogs/mutation/fields/activation-deadline.tsx
+++ dashboard/src/modules/users/components/dialogs/mutation/fields/activation-deadline.tsx
@@ -1,7 +1,7 @@
 
-import { DateField } from "@marzneshin/common/components";
+import { PersianDateField } from "@marzneshin/common/components";
 import { FC } from "react";
 
 export const ActivationDeadlineField: FC = () => {
-    return <DateField name="activation_deadline" label="page.users.activation_deadline" />;
+    return <PersianDateField name="activation_deadline" label="page.users.activation_deadline" />;
 };
--- dashboard/src/modules/users/components/dialogs/mutation/fields/expire-date.tsx
+++ dashboard/src/modules/users/components/dialogs/mutation/fields/expire-date.tsx
@@ -1,6 +1,6 @@
-import { DateField } from "@marzneshin/common/components";
+import { PersianDateField } from "@marzneshin/common/components";
 import { FC } from "react";
 
 export const ExpireDateField: FC = () => {
-    return <DateField name="expire_date" label="page.users.expire_date" />;
+    return <PersianDateField name="expire_date" label="page.users.expire_date" />;
 };
